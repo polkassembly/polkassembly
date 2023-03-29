8e1bba36a8563950e4568d4d3a0f3a549dfa2cc8
alphainfinitus <kumar.mridul000@gmail.com>
2023-03-29 17:22:06 +0530

    address Link fix and post edit condition fix

--- pages/api/v1/auth/actions/addressLinkConfirm.ts
+++ pages/api/v1/auth/actions/addressLinkConfirm.ts
@@ -9,18 +9,21 @@ import authServiceInstance from '~src/auth/auth';
 import { ChangeResponseType, MessageType } from '~src/auth/types';
 import getTokenFromReq from '~src/auth/utils/getTokenFromReq';
 import messages from '~src/auth/utils/messages';
+import getSubstrateAddress from '~src/util/getSubstrateAddress';
 
 async function handler(req: NextApiRequest, res: NextApiResponse<ChangeResponseType | MessageType>) {
 	if (req.method !== 'POST') return res.status(405).json({ message: 'Invalid request method, POST required.' });
 
 	const { address, signature } = req.body;
-
 	if(!address || !signature) return res.status(400).json({ message: 'Missing parameters in request body' });
 
 	const token = getTokenFromReq(req);
 	if(!token) return res.status(400).json({ message: 'Invalid token' });
 
-	const updatedJWT = await authServiceInstance.AddressLinkConfirm(token, address, signature);
+	const substrateAddress = getSubstrateAddress(address);
+	if(!substrateAddress) return res.status(400).json({ message: messages.INVALID_ADDRESS });
+
+	const updatedJWT = await authServiceInstance.AddressLinkConfirm(token, substrateAddress, signature);
 
 	return res.status(200).json({ message: messages.ADDRESS_LINKING_SUCCESSFUL, token: updatedJWT });
 }

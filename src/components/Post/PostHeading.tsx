5cc3f55bd3631727708096fa511d97e714379f07
This-Is-Prince <this.is.kumar.prince@gmail.com>
2023-04-13 11:01:21 +0530

    fix: trackName typo

--- src/components/Post/PostHeading.tsx
+++ src/components/Post/PostHeading.tsx
@@ -29,7 +29,7 @@ const PostHeading: FC<IPostHeadingProps> = (props) => {
 	const router= useRouter();
 	const { className } = props;
 	const { postData: {
-		created_at, status, postType: proposalType, postIndex: onchainId, title, description, proposer, curator, username, topic, last_edited_at, requested, reward,tags,trackName
+		created_at, status, postType: proposalType, postIndex: onchainId, title, description, proposer, curator, username, topic, last_edited_at, requested, reward,tags, track_name
 	} } = usePostDataContext();
 
 	const { network } = useNetworkContext();
@@ -69,7 +69,7 @@ const PostHeading: FC<IPostHeadingProps> = (props) => {
 				</>
 			</div>
 			{tags && tags.length>0 &&<div className='flex mt-6 gap-[8px]'>
-				{tags?.map((tag,index ) => (<div onClick={() => handleTagClick(onTagClickFilter(proposalType, trackName || ''),tag)} className='rounded-full px-[16px] py-[4px] border-navBlue border-solid border-[1px] text-navBlue text-xs traking-2 cursor-pointer hover:border-pink_primary hover:text-pink_primary' key={index} >{tag.charAt(0).toUpperCase()+tag.slice(1)}</div>))}
+				{tags?.map((tag,index ) => (<div onClick={() => handleTagClick(onTagClickFilter(proposalType, track_name || ''),tag)} className='rounded-full px-[16px] py-[4px] border-navBlue border-solid border-[1px] text-navBlue text-xs traking-2 cursor-pointer hover:border-pink_primary hover:text-pink_primary' key={index} >{tag.charAt(0).toUpperCase()+tag.slice(1)}</div>))}
 			</div> }
 		</div>
 	);

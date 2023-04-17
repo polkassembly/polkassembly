7cbf85188d4d9d2d5dae125810b7ee0da65f8a0b
KanishkaRajputd <kanishka6393rajput@gmail.com>
2023-04-17 22:25:55 +0530

    .

--- pages/delegation-dashboard/[track].tsx
+++ pages/delegation-dashboard/[track].tsx
@@ -3,18 +3,23 @@
 // of the Apache-2.0 license. See the LICENSE file for details.
 
 import { GetServerSideProps } from 'next';
-import { getOnChainPosts } from 'pages/api/v1/listing/on-chain-posts';
+// import { getOnChainPosts } from 'pages/api/v1/listing/on-chain-posts';
 import { useEffect } from 'react';
 import { getNetworkFromReqHeaders } from '~src/api-utils';
 import DashboardTrackListing from '~src/components/DelegationDashboardComponents/dashboardTrack';
+import { getTrackData } from '~src/components/Listing/Tracks/AboutTrackCard';
+// import { CustomStatus } from '~src/components/Listing/Tracks/TrackListingCard';
 import { useNetworkContext } from '~src/context';
 import SEOHead from '~src/global/SEOHead';
-import { ProposalType } from '~src/global/proposalType';
+// import { ProposalType } from '~src/global/proposalType';
+// import { sortValues } from '~src/global/sortOptions';
 
 export const getServerSideProps: GetServerSideProps = async ({ req, query }) => {
-  const { track } = query;
+	const { track } = query;
 	const network = getNetworkFromReqHeaders(req.headers);
-  const fetch = getOnChainPosts({ network ,trackStatus: ProposalType.OPEN_GOV,  })
+	const data= getTrackData(String(track));
+	console.log(data);
+	// const fetch = getOnChainPosts({ network , proposalType: ProposalType.OPEN_GOV, trackStatus: CustomStatus.Active , sortBy,page })
 	return { props: { network } };
 
 };

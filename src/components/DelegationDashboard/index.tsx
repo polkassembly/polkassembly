3e827069de0748697a93f80183b7ab74e8907b40
KanishkaRajputd <kanishka6393rajput@gmail.com>
2023-05-08 17:33:18 +0530

    fixes

--- src/components/DelegationDashboard/index.tsx
+++ src/components/DelegationDashboard/index.tsx
@@ -79,6 +79,7 @@ const DelegationDashboardHome = ({ className } : Props) => {
 	useEffect(() => {
 
 		userDetails.isLoggedOut() && setOpenLoginModal(true);
+		!userDetails.isLoggedOut() && setOpenLoginModal(false);
 
 	// eslint-disable-next-line react-hooks/exhaustive-deps
 	}, [userDetails?.isLoggedOut]);

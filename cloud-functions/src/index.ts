5b4cc627c435bb3cb0d771bc29352c72893db82f
alphainfinitus <kumar.mridul000@gmail.com>
2023-05-24 21:51:41 +0530

    polkassembly_posts index 'on write' function

--- cloud-functions/src/index.ts
+++ cloud-functions/src/index.ts
@@ -1,19 +1,38 @@
-/**
- * Import function triggers from their respective submodules:
- *
- * import {onCall} from "firebase-functions/v2/https";
- * import {onDocumentWritten} from "firebase-functions/v2/firestore";
- *
- * See a full list of supported triggers at https://firebase.google.com/docs/functions
- */
+import { onDocumentWritten } from 'firebase-functions/v2/firestore';
+import * as logger from 'firebase-functions/logger';
+import algoliasearch from 'algoliasearch';
 
-import {onRequest} from "firebase-functions/v2/https";
-import * as logger from "firebase-functions/logger";
+// posts
+exports.myfunction = onDocumentWritten('networks/{network}/post_types/{postType}/posts/{postId}', (event) => {
+	const ALGOLIA_APP_ID = process.env.ALGOLIA_APP_ID;
+	const ALGOLIA_WRITE_API_KEY = process.env.ALGOLIA_WRITE_API_KEY;
 
-// Start writing functions
-// https://firebase.google.com/docs/functions/typescript
+	if (!ALGOLIA_APP_ID || !ALGOLIA_WRITE_API_KEY) return;
 
-// export const helloWorld = onRequest((request, response) => {
-//   logger.info("Hello logs!", {structuredData: true});
-//   response.send("Hello from Firebase!");
-// });
+	const algoliaClient = algoliasearch(ALGOLIA_APP_ID, ALGOLIA_WRITE_API_KEY);
+	const index = algoliaClient.initIndex('polkassembly_posts');
+
+	const { network, postType, postId } = event.params;
+	logger.info('Document written: ', { network, postType, postId });
+
+	// Retrieve the data from the Firestore event
+	const post = event.data?.after.data();
+
+	// Create an object to be indexed by Algolia
+	const postRecord = {
+		objectID: postId, // Unique identifier for the object
+		network,
+		postType,
+		...post
+	};
+
+	// Update the Algolia index
+	index
+		.saveObject(postRecord)
+		.then(() => {
+			logger.info('Post indexed successfully:', { postId });
+		})
+		.catch((error) => {
+			logger.error('Error indexing post:', { postId, error });
+		});
+});

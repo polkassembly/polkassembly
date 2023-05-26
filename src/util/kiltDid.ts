b4b7880536e857f5c69caa9d850ba00b52df5a12
Yash Sharma <yash@polkassembly.com>
2023-05-26 12:59:55 +0530

    added kily rpc ednpoint

--- src/util/kiltDid.ts
+++ src/util/kiltDid.ts
@@ -4,12 +4,13 @@ import { ApiPromise, WsProvider } from '@polkadot/api'
 
 import '@kiltprotocol/augment-api'
 import { typesBundle } from '@kiltprotocol/type-definitions'
+import { chainProperties, network } from '~src/global/networkConstants'
 
 export async function getKiltDidName(
   lookupAccountAddress: KeyringPair['address']
 ): Promise<string | null> {
   const api = await ApiPromise.create({
-    provider: new WsProvider("wss://spiritnet.api.onfinality.io/public-ws"),
+    provider: new WsProvider(chainProperties[network.KILT].rpcEndpoint),
     typesBundle
   })
 

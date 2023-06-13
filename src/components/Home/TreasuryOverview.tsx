6c804201bdf8a05224484759840aa2df1c30346a
This-Is-Prince <this.is.kumar.prince@gmail.com>
2023-06-13 12:52:10 +0530

    fix: NaN issue of Treasury Overview weekly change

--- src/components/Home/TreasuryOverview.tsx
+++ src/components/Home/TreasuryOverview.tsx
@@ -308,7 +308,7 @@ const TreasuryOverview: FC<ITreasuryOverviewProps> = (props) => {
 				);
 				const responseJSON = await response.json();
 				if (responseJSON['message'] == 'Success') {
-					const weekAgoPrice = responseJSON['data']['average'];
+					const weekAgoPrice = responseJSON['data']['ema7_average'];
 					const currentTokenPriceNum : number = parseFloat(currentTokenPrice.value);
 					const weekAgoPriceNum : number = parseFloat(weekAgoPrice);
 					if(weekAgoPriceNum == 0) {

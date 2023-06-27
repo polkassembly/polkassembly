fad432d0f00478921821c72618bf8a66834ea5a5
KanishkaRajputd <kanishka6393rajput@gmail.com>
2023-06-27 12:32:07 +0530

    fixed

--- src/components/Post/LinkCard.tsx
+++ src/components/Post/LinkCard.tsx
@@ -11,23 +11,23 @@ interface Props{
 }
 const LinkCard = ({ timeline, proposalType }: Props) => {
 
-	return  <div className='bg-white flex flex-wrap drop-shadow-md min-h-[69px] rounded-md w-full mb-6 items-center px-4'>
+	return  <div className='bg-white flex flex-wrap drop-shadow-md min-h-[69px] max-sm:py-2 rounded-md w-full mb-6 items-center px-4'>
 		{
 			timeline?.map((item: any, index: number) => {
 				const proposal_type = getFirestoreProposalType(item?.type);
 				return (
 					proposalType === proposal_type ?
 						<div key={index}
-							className='flex gap-2 text-lg font-medium text-[#243A57] cursor-default'>
-							<span>{item?.type === 'ReferendumV2' ? 'Opengov Referenda' : item?.type?.split(/(?=[A-Z])/).join(' ')}</span>
+							className='flex gap-2 text-lg font-medium text-[#243A57] cursor-default max-md:text-sm max-md:gap-1'>
+							<span className='flex flex-wrap'>{item?.type === 'ReferendumV2' ? 'Open Gov Referendum' : item?.type?.split(/(?=[A-Z])/).join(' ')}</span>
 							<span className='text-[#243A57]'>#{item?.index}</span>
 							<span className='mr-2'>{ index !== timeline.length - 1 && ' >> '}</span>
 						</div>:
 						<Link
 							key={index}
-							className='flex gap-2 text-lg font-medium text-[#243A57] cursor-pointer'
+							className='flex gap-2 text-lg font-medium text-[#243A57] cursor-pointer max-md:text-sm max-md:gap-1'
 							href={`/${getSinglePostLinkFromProposalType(proposal_type as any)}/${item?.type === 'Tip' ? item?.hash : item?.index}`}>
-							<span>{item?.type === 'ReferendumV2' ? 'Opengov Referenda' : item?.type?.split(/(?=[A-Z])/).join(' ')}</span>
+							<span  className='flex flex-wrap'>{item?.type === 'ReferendumV2' ? 'Open Gov Referendum' : item?.type?.split(/(?=[A-Z])/).join(' ')}</span>
 							<span className='text-pink_primary'>#{item?.index}</span>
 							<span className='mr-2'>{ index !== timeline.length - 1 && ' >> '}</span>
 						</Link>

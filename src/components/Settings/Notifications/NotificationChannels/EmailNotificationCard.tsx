2f68c3c23d6faac90855a33b4cedba69fa1bce69
AleemAlam <aleem.alam@outlook.com>
2023-06-27 15:19:54 +0530

    remove email verification flow from notification

--- src/components/Settings/Notifications/NotificationChannels/EmailNotificationCard.tsx
+++ src/components/Settings/Notifications/NotificationChannels/EmailNotificationCard.tsx
@@ -3,12 +3,7 @@
 // of the Apache-2.0 license. See the LICENSE file for details.
 import React, { useState } from 'react';
 import MailFilled from '~assets/icons/email-notification.svg';
-import { Button, Form, Input, Switch } from 'antd';
-import { Rule } from 'antd/es/form';
-import styled from 'styled-components';
-import nextApiClientFetch from '~src/util/nextApiClientFetch';
-import queueNotification from '~src/ui-components/QueueNotification';
-import { NotificationStatus } from '~src/types';
+import { Switch } from 'antd';
 import DisabledConfirmation from './Modals/Confirmation';
 import { CHANNEL } from '.';
 type Props = {
@@ -18,76 +13,10 @@ type Props = {
 	notificationEnabled:boolean
 };
 
-const Container = styled.div`
-	// #email_help {
-    //     position: absolute;
-    // }
-`;
-
-const validationRules: Rule[] = [
-	{ message: 'Email is required, Please enter an email', required: true }
-];
-
 export default function EmailNotificationCard({ verifiedEmail, handleEnableDisabled, verified, notificationEnabled }: Props) {
-	const [form] = Form.useForm();
-	const [loading, setLoading] = useState<boolean>(false);
 	const [showModal, setShowModal] = useState<boolean>(false);
 	const handleToggleClick = () => {
 		setShowModal(true);
-	};
-	const validateEmailFormat = (
-		_: Rule,
-		value: string,
-		callback: (error?: string) => void
-	) => {
-		const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
-		if (!value || emailRegex.test(value)) {
-			callback();
-			return;
-		} else if (verifiedEmail === value) {
-			callback('This email is already verified.');
-			return;
-		} else {
-			callback('Please enter a valid email address.'); // Validation failed
-			return;
-		}
-	};
-
-	const handleClick = async () => {
-		try {
-			const values = await form.validateFields();
-			const { email } = values;
-			setLoading(true);
-			const { data, error } = await nextApiClientFetch<any>(
-				'api/v1/auth/actions/sendVerificationEmail',
-				{
-					email
-				}
-			);
-			if (error) {
-				queueNotification({
-					header: 'Failed!',
-					message: error,
-					status: NotificationStatus.ERROR
-				});
-			}
-			if (data) {
-				queueNotification({
-					header: 'Success!',
-					message: 'Verification Email Sent.',
-					status: NotificationStatus.SUCCESS
-				});
-			}
-			setLoading(false);
-		} catch (error) {
-			console.log('Validation error:', error);
-			setLoading(false);
-			queueNotification({
-				header: 'Failed!',
-				message: error,
-				status: NotificationStatus.ERROR
-			});
-		}
 	};
 
 	return (
@@ -95,7 +24,7 @@ export default function EmailNotificationCard({ verifiedEmail, handleEnableDisab
 			<h3 className='flex gap-2 items-center text-base text-[#243A57] font-medium m-0 gap-1'>
 				<span>
 					<MailFilled /> Email Notifications{' '}
-					{!verified && <span className='text-[10px] px-[4px] py-[2px] bg-[red] border-[#5A46FF] border-2 text-[#FFFFFF] rounded-tr-lg rounded-bl-lg'>Not Verified</span>}
+					{!verified && <span className='text-[10px] px-[4px] py-[2px] bg-[red] border-[#5A46FF] border-2 text-[#FFFFFF] rounded-tr-lg rounded-bl-lg'>  {verifiedEmail ? 'Not Verified' : 'Not Added'}</span>}
 
 				</span>
 				{(!!verifiedEmail && verified) &&
@@ -111,39 +40,9 @@ export default function EmailNotificationCard({ verifiedEmail, handleEnableDisab
 					</span>
 				}
 			</h3>
-			<Container>
-				<Form
-					onFinish={handleClick}
-					form={form}
-					className='flex gap-2 w-2/3 flex-wrap lg:flex-nowrap'
-				>
-					<Form.Item
-						name={'email'}
-						className='m-0 w-full min-w-[250px]'
-						rules={[
-							...validationRules,
-							{ validator: validateEmailFormat }
-						]}
-						initialValue={verifiedEmail}
-					>
-						<Input
-							className='p-2 text-sm leading-[21px]'
-							placeholder='Account Address'
-							disabled={verified || loading}
-						/>
-					</Form.Item>
-					{
-						!verified &&
-						<Button
-							htmlType='submit'
-							loading={loading}
-							className='h-10 rounded-[6px] bg-[#E5007A] flex items-center justify-center border border-solid border-pink_primary px-[22px] py-[4px] text-white font-medium text-sm leading-[21px] tracking-[0.0125em] capitalize'
-						>
-						Verify
-						</Button>
-					}
-				</Form>
-			</Container>
+			<div className='flex items-center gap-2'>
+				<h3 className='m-0 text-[#243A57] text-[14px]'>{verifiedEmail ? verifiedEmail: 'Please add your email on account page.'}</h3>
+			</div>
 			<DisabledConfirmation
 				open={showModal}
 				onConfirm={() => {

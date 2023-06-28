a260448b520d6d572a49ac2b80b0c31e903fa301
pathak-arpita <pathak.arpita6497.com>
2023-06-28 16:39:29 +0530

    delegation-dashboard-empty-state

--- pages/delegation/index.tsx
+++ pages/delegation/index.tsx
@@ -6,18 +6,40 @@ import { GetServerSideProps } from 'next';
 import React, { useEffect } from 'react';
 import { getNetworkFromReqHeaders } from '~src/api-utils';
 import DelegationDashboard from '~src/components/DelegationDashboard';
-
+import DelegationDashboardEmptyState from '~assets/icons/delegation-empty-state.svg';
+import CopyContentIcon from '~assets/icons/content-copy.svg';
+import copyToClipboard from 'src/util/copyToClipboard';
+import queueNotification from 'src/ui-components/QueueNotification';
+import { NotificationStatus } from 'src/types';
+import { message } from 'antd';
 import { useNetworkContext } from '~src/context';
 import SEOHead from '~src/global/SEOHead';
+import { useRouter } from 'next/router';
 
 export const getServerSideProps: GetServerSideProps = async ({ req }) => {
 	const network = getNetworkFromReqHeaders(req.headers);
 	return { props: { network } };
 };
 
-const Delegation = ( props : { network: string} ) => {
+const Delegation = ( props : { network: string } ) => {
 
 	const { setNetwork } = useNetworkContext();
+	const { asPath } = useRouter();
+	const handleClick = () => {
+		message.success('Request submitted successfully');
+	};
+
+	const handleCopylink = () => {
+		const url = `https://${props.network}.polkassembly.io${asPath.split('#')[0]}`;
+
+		copyToClipboard(url);
+
+		queueNotification({
+			header: 'Copied!',
+			message: 'Comment link copied to clipboard.',
+			status: NotificationStatus.INFO
+		});
+	};
 
 	useEffect(() => {
 		setNetwork(props.network);
@@ -26,7 +48,12 @@ const Delegation = ( props : { network: string} ) => {
 
 	return <>
 		<SEOHead title='Delegation Dashboard' network={props.network} />
-		<DelegationDashboard/>
+		<div className='hidden sm:flex'><DelegationDashboard/></div>
+		<div className='flex sm:hidden items-center justify-center flex-col mt-10 w-full'>
+			<DelegationDashboardEmptyState className='w-[100%] sm:w-64 mr-[-14px]' viewBox='0 0 350 350'/>
+			<button className='text-[#E5007A] outline-none border border-pink_primary border-solid rounded-md px-6 py-3 mt-[-15px] flex items-center justify-center' onClick={handleClick}>Request Mobile View</button>
+			<div className='mt-[64px] px-4 py-1 rounded-full border border-[#D2D8E0] border-solid flex justify-center items-center' onClick={() => {handleCopylink();}}>Copy Page Link <CopyContentIcon className='ml-1'/></div>
+		</div>
 	</>;
 };
 

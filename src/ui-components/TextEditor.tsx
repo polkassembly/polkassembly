c9ca0a029dc1967088f301a43eb586096043da41
alphainfinitus <kumar.mridul000@gmail.com>
2023-07-04 14:07:42 +0530

    refactor

--- src/ui-components/TextEditor.tsx
+++ src/ui-components/TextEditor.tsx
@@ -28,8 +28,7 @@ interface ITextEditorProps {
 const editorContentStyle = `
 @import url("https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,200;0,300;0,400;0,500;0,600;0,700;1,200;1,300;1,400;1,500;1,600;1,700&display=swap");
 body {
-	font-family: "Poppins",
-	sans-serif;
+	font-family: "Poppins", sans-serif;
 	font-size: 14px;
 	line-height: 1.5;
 }

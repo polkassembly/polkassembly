03c62d9713b1e917d1d00c39c7e5584690a1c1e2
Rajdeep Singh <42423438+rajdeep7Singh@users.noreply.github.com>
2023-07-05 21:09:18 +0530

    Revert "logos-on-username"

--- src/ui-components/IdentityBadge.tsx
+++ src/ui-components/IdentityBadge.tsx
@@ -5,12 +5,28 @@
 import { CheckCircleFilled, MinusCircleFilled } from '@ant-design/icons';
 import { DeriveAccountFlags, DeriveAccountRegistration } from '@polkadot/api-derive/types';
 import { Tooltip } from 'antd';
-import EmailIcon from '~assets/icons/mail-icon.svg';
-import JudgementIcon from '~assets/icons/account-box-icon.svg';
-import TwitterIcon from '~assets/icons/twitter-icon.svg';
-import ShareScreenIcon from '~assets/icons/screen-share-icon.svg';
 import React from 'react';
 import styled from 'styled-components';
+
+const StyledPopup = styled.div`
+font-size: sm;
+color: var(--grey_primary);
+list-style: none;
+padding: 1rem;
+
+li {
+	margin-bottom: 0.3rem;
+}
+
+.desc {
+	font-weight: 500;
+	margin-right: 0.3rem;
+}
+
+.judgments {
+	display: inline list-item;
+}
+`;
 
 interface Props {
 	className?: string,
@@ -35,70 +51,18 @@ const IdentityBadge = ({ className, address, identity, flags, web3Name }: Props)
 
 	const displayJudgements = JSON.stringify(judgements?.map(([,jud]) => jud.toString()));
 
-	const popupContent =
-	<div className='pl-1 pr-5 py-1'>
-		{identity?.legal &&
-		<li className='my-1'>
-			<span className='desc text-bodyBlue font-medium'>legal:&nbsp;</span>
-			<span className='text-xs text-bodyBlue font-normal'>{identity.legal}</span>
-		</li>
-		}
-		{identity?.email &&
-		<li className='flex items-center my-1'>
-			<span className='desc text-bodyBlue font-medium flex items-center text-sm'>
-				<EmailIcon className='mr-0.5 pt-0.5'/>Email:&nbsp;
-			</span>
-			<span className='text-xs text-bodyBlue font-normal pt-0.5'>{identity.email}</span>
-		</li>
-		}
-		{(identity?.judgements?.length || 0) > 0 &&
-		<li className='flex items-center my-1'>
-			<span className='desc flex items-center text-sm text-bodyBlue font-medium'><JudgementIcon className='mr-1'/>Judgements:&nbsp;</span>
-			<span className='text-xs text-bodyBlue'>{displayJudgements}</span>
-		</li>
-		}
-		{identity?.pgp &&
-		<li className='flex items-center my-1'>
-			<span className='desc text-bodyblue text-sm font-medium'>pgp:&nbsp;</span>
-			<span className='text-xs text-bodyblue font-normal'>{identity.pgp}</span>
-		</li>
-		}
-		{identity?.riot &&
-		<li className='flex items-center my-1'>
-			<span className='desc text-bodyblue text-sm font-medium'>riot:&nbsp;</span>
-			<span className='text-xs text-bodyBlue font-normal'>{identity.riot}</span>
-		</li>
-		}
-		{identity?.twitter &&
-		<li className='flex items-center my-1'>
-			<span className='desc text-bodyBlue font-medium flex text-sm'><TwitterIcon className='mr-1 mt-1'/>Twitter:&nbsp;</span>
-			<span className='text-xs font-normal text-bodyBlue'>{identity.twitter}</span>
-		</li>
-		}
-		{identity?.web &&
-		<li className='flex items-center my-1'>
-			<span className='desc text-bodyBlue font-medium text-sm'>Web:&nbsp;</span>
-			<span className='text-xs text-bodyBlue font-normal'>{identity.web}</span>
-		</li>
-		}
-		{flags?.isCouncil &&
-		<li className='flex items-center my-1'>
-			<span className='desc text-bodyBlue font-medium text-sm'>Council member:&nbsp;</span><CouncilEmoji/>
-		</li>
-		}
-		{
-			<li className='flex items-center my-1'>
-				<span className='desc'><a href={`https://polkaverse.com/accounts/${address}`} target='_blank' rel='noreferrer' className='flex text-pink-500 underline items-center'><ShareScreenIcon className='mr-2'/>Polkaverse Profile</a>
-				</span>
-			</li>
-		}
-		{web3Name &&
-		<li className='flex items-center my-1'>
-			<span className='desc flex items-center'><a href={`https://w3n.id/${web3Name}`} target='_blank' rel='noreferrer' className='flex text-pink-500'><ShareScreenIcon className='mr-2'/>Web3 Name Profile</a>
-			</span>
-		</li>
-		}
-	</div>;
+	const popupContent = <StyledPopup>
+		{identity?.legal && <li><span className='desc'>legal:</span>{identity.legal}</li>}
+		{identity?.email && <li><span className='desc'>email:</span>{identity.email}</li>}
+		{(identity?.judgements?.length || 0) > 0 && <li><span className='desc'>judgements:</span><span className='judgments'>{displayJudgements}</span></li>}
+		{identity?.pgp && <li><span className='desc'>pgp:</span>{identity.pgp}</li>}
+		{identity?.riot && <li><span className='desc'>riot:</span>{identity.riot}</li>}
+		{identity?.twitter && <li><span className='desc'>twitter:</span>{identity.twitter}</li>}
+		{identity?.web && <li><span className='desc'>web:</span>{identity.web}</li>}
+		{flags?.isCouncil && <li><span className='desc'>Council member</span><CouncilEmoji/></li>}
+		{<li><span className='desc'><a href={`https://polkaverse.com/accounts/${address}`} target='_blank' rel='noreferrer'>Polkaverse Profile</a></span></li>}
+		{web3Name && <li><span className='desc'><a href={`https://w3n.id/${web3Name}`} target='_blank' rel='noreferrer'>Web3 Name Profile</a></span></li>}
+	</StyledPopup>;
 
 	return <div className={className}>
 		<Tooltip color='#fff' title={popupContent}>
@@ -108,10 +72,8 @@ const IdentityBadge = ({ className, address, identity, flags, web3Name }: Props)
 };
 
 export default styled(IdentityBadge)`
-	li{
-		line-height: 21px;
-		list-style: none;
-	}
+	display: inline;
+
 	i.green.circle.icon {
 		color: green_primary !important;
 	}

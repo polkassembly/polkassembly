fba8c100c0108f5176ff58a94faf7c31731da431
KanishkaRajputd <kanishka6393rajput@gmail.com>
2023-07-08 19:46:26 +0530

    fix

--- src/components/Post/Post.tsx
+++ src/components/Post/Post.tsx
@@ -347,8 +347,8 @@ const Post: FC<IPostProps> = (props) => {
 			subscribers: post?.subscribers || [],
 			tags: post?.tags || [],
 			timeline: post?.timeline,
-			title: post?.title,
-			topic: post?.topic,
+			title: post?.title || '',
+			topic: post?.topic || '',
 			track_name: trackName,
 			track_number: post?.track_number,
 			username: post?.username

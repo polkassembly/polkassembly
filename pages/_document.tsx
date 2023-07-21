4358c42bc97d7388df8eb38b50b37a049310c896
This-Is-Prince <this.is.kumar.prince@gmail.com>
2023-07-21 13:36:19 +0530

    feat: pwa initial setup

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -7,7 +7,9 @@ import { Head, Html, Main, NextScript } from 'next/document';
 export default function Document() {
 	return (
 		<Html lang="en">
-			<Head />
+			<Head>
+				<link rel="manifest" href="/manifest.json" />
+			</Head>
 			<body>
 				<Main />
 				<NextScript />

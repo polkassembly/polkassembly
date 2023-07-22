fd6783f2ae9399d73bd54e4cd71fee23cd2c2b43
This-Is-Prince <this.is.kumar.prince@gmail.com>
2023-07-22 08:35:53 +0530

    feat: offline file added

---
+++ pages/_offline.tsx
@@ -0,0 +1,12 @@
+// Copyright 2019-2025 @polkassembly/polkassembly authors & contributors
+// This software may be modified and distributed under the terms
+// of the Apache-2.0 license. See the LICENSE file for details.
+import React from 'react';
+
+const Offline = () => {
+	return (
+		<div>You are Offline</div>
+	);
+};
+
+export default Offline;

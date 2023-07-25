f8fbbfbb1a8964b31cf861b80e7524c157220247
This-Is-Prince <this.is.kumar.prince@gmail.com>
2023-07-25 12:15:54 +0530

    feat: fallback ui updated

--- src/ui-components/ErrorBoundary/Fallback.tsx
+++ src/ui-components/ErrorBoundary/Fallback.tsx
@@ -1,15 +1,28 @@
 // Copyright 2019-2025 @polkassembly/polkassembly authors & contributors
 // This software may be modified and distributed under the terms
 // of the Apache-2.0 license. See the LICENSE file for details.
+
+import { Result } from 'antd';
+import Link from 'next/link';
 import React from 'react';
 
+import NothingFoundSVG from '~assets/nothing-found.svg';
 const Fallback = () => {
 	return (
 		<section className='flex flex-col items-center justify-center h-screen w-screen'>
-			<h2>Something went wrong</h2>
-			<a href='https://polkassembly.hellonext.co/' target='_blank' className='text-pink_primary underline underline-offset-4' rel='noreferrer'>
-                Report an Issue
-			</a>
+			<Result
+				icon={
+					<div className='w-1/2 h-auto mx-auto max-w-[900px]'>
+						<NothingFoundSVG />
+					</div>
+				}
+				title="Unfortunately something has gone wrong. Please try again."
+				extra={
+					<Link href='/' className='py-2 px-6 bg-pink_primary text-white border-white hover:bg-pink_secondary rounded-md text-lg h-[50px] w-[215px]'>
+						Go To Home
+					</Link>
+				}
+			/>
 		</section>
 	);
 };

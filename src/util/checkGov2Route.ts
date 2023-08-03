a7a4fccea1f8ce29bc3a42d24c861729768b132a
Lt-Kartik-9PARA-SF <kartiktyagi22@gmail.com>
2023-08-03 14:09:07 +0530

    changes

--- src/util/checkGov2Route.ts
+++ src/util/checkGov2Route.ts
@@ -20,7 +20,22 @@ for (const trackName of Object.keys(networkTrackInfo.kusama)) {
 	gov2Routes.push(trackName.split(/(?=[A-Z])/).join('-').toLowerCase());
 }
 
+function isOtherRouteGov2Route(pathname: string): boolean{
+	const wasGov2RouteString = localStorage.getItem('wasGov2Route');
+	if(wasGov2RouteString){
+		const wasGov2Route = JSON.parse(wasGov2RouteString);
+		return ['bounties','child_bounties'].includes(pathname.split('/')[1]) && wasGov2Route;
+	}
+	return gov2Routes.includes(pathname.split('/')[1]);
+}
+
 export default function checkGov2Route(pathname: string, query?: ParsedUrlQuery, prevRoute?: string, network?: string): boolean {
+
+	const isGov2Route = checkGov2RouteUtility(pathname,query,prevRoute,network);
+	localStorage.setItem('wasGov2Route',JSON.stringify(isGov2Route));
+	return isGov2Route;
+}
+function checkGov2RouteUtility(pathname: string, query?: ParsedUrlQuery, prevRoute?: string, network?: string): boolean{
 	if (network === 'collectives') {
 		return false;
 	}
@@ -36,7 +51,13 @@ export default function checkGov2Route(pathname: string, query?: ParsedUrlQuery,
 	else if(prevRoute && gov2Routes.includes(prevRoute.split('/')[1]) && pathname.split('/')[1] === 'post'){
 		return true;
 	}
+	else if(prevRoute && gov2Routes.includes(prevRoute.split('/')[1]) && pathname.split('/')[1] === 'bounties'){
+		return true;
+	}
+	else if(prevRoute && gov2Routes.includes(prevRoute.split('/')[1]) && pathname.split('/')[1] === 'child_bounties'){
+		return true;
+	}
 	else if(!prevRoute && pathname.split('/')[1] === 'discussions'){ return false; }
 
-	return gov2Routes.includes(pathname.split('/')[1]);
+	return isOtherRouteGov2Route(pathname);
 }

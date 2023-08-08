8d9f008cefcc3aba3e3c6a99f8600f0197b94c91
Nikhil Ranjan <niklabh811@gmail.com>
2023-08-07 17:06:53 -0700

    removed unused

--- src/ui-components/ErrorBoundary/index.tsx
+++ src/ui-components/ErrorBoundary/index.tsx
@@ -2,7 +2,7 @@
 // This software may be modified and distributed under the terms
 // of the Apache-2.0 license. See the LICENSE file for details.
 
-import React, { useState, useEffect, FC, PropsWithChildren } from 'react';
+import React, { FC, PropsWithChildren } from 'react';
 
 interface IErrorBoundaryProps extends PropsWithChildren {}
 

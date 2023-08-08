3e38c1cf409eb512875c085c1c3514a282dbd153
Nikhil Ranjan <niklabh811@gmail.com>
2023-08-07 17:02:49 -0700

    fix

--- src/ui-components/ErrorBoundary/index.tsx
+++ src/ui-components/ErrorBoundary/index.tsx
@@ -3,32 +3,14 @@
 // of the Apache-2.0 license. See the LICENSE file for details.
 
 import React, { useState, useEffect, FC, PropsWithChildren } from 'react';
-import Fallback from './Fallback';
 
 interface IErrorBoundaryProps extends PropsWithChildren {}
 
 const ErrorBoundary: FC<IErrorBoundaryProps> = (props) => {
 	const { children } = props;
-	const [hasError, setHasError] = useState(false);
-
-	useEffect(() => {
-		const handleGlobalError = (error: ErrorEvent) => {
-			console.error('Error caught by ErrorBoundary:', error);
-			setHasError(true);
-		};
-
-		window.addEventListener('error', handleGlobalError);
-
-		return () => {
-			window.removeEventListener('error', handleGlobalError);
-		};
-	}, []);
 
 	return (
 		<div className='relative'>
-			{/* {
-				hasError? <Fallback onReset={() => setHasError(false)}  />: null
-			} */}
 			{children}
 		</div>
 	);

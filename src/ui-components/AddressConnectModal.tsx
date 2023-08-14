0a60a9d9a8881e5106c5745df509bf8ad0fc80a6
KanishkaRajputd <kanishka6393rajput@gmail.com>
2023-08-15 00:30:32 +0530

    boolean check

--- src/ui-components/AddressConnectModal.tsx
+++ src/ui-components/AddressConnectModal.tsx
@@ -435,8 +435,8 @@ const AddressConnectModal = ({ className, open, setOpen, closable, localStorageW
 			{isUnlinkedAddress ? 'Link Address' : linkAddressNeeded ? 'Next' : 'Confirm'}
 		</Button>
 		}
-		closable = {Boolean(closable)}
-		onCancel={() => setOpen(Boolean(closable))}
+		closable = {closable}
+		onCancel={() => setOpen(!closable)}
 		closeIcon={<CloseIcon/>}
 	>
 		<Spin spinning={loading} indicator={<LoadingOutlined />}>

4901ccd1d066af332534b62ceb37752fbb9250e6
Vaibhav Singhvi <vaibhavsinghvi254@gmail.com>
2023-08-20 11:27:26 +0530

    fix: Timeline UI Changes

--- src/components/Post/Tabs/PostTimeline/TimelineContainer.tsx
+++ src/components/Post/Tabs/PostTimeline/TimelineContainer.tsx
@@ -4,13 +4,13 @@
 
 import { dayjs } from 'dayjs-init';
 import Link from 'next/link';
-import * as React from 'react';
 import StatusTag from 'src/ui-components/StatusTag';
-
+import React, { useState } from 'react';
 import { getStatus } from '~src/components/Post/Comment/CommentsContainer';
 import { useNetworkContext } from '~src/context';
 import { getFirestoreProposalType, getSinglePostLinkFromProposalType } from '~src/global/proposalType';
 import { getBlockLink } from '~src/util/subscanCheck';
+import { ExportOutlined, PlusSquareOutlined, UpOutlined } from '@ant-design/icons';
 
 interface BlockStatus {
 	block: number;
@@ -33,19 +33,24 @@ function sortfunc(a: BlockStatus, b: BlockStatus) {
 }
 
 const TimelineContainer: React.FC<ITimelineContainerProps> = (props) => {
+	const [isCollapsed, setIsCollapsed] = useState(false);
 	const { timeline } = props;
 	const { network } = useNetworkContext();
 	if (!timeline) return null;
 	const { statuses, type } = timeline;
 
 	if (statuses.length === 0) return null;
-
+	const minHeight = statuses.length * 50;
 	const StatusDiv = ({ status } : { status: string }) => {
 		return (
 			<div className='flex items-center absolute -top-3.5 justify-center'>
 				<StatusTag colorInverted={false} status={status} type={type} />
 			</div>
 		);
+	};
+
+	const toggleCollapse = () => {
+		setIsCollapsed(!isCollapsed);
 	};
 
 	const url = getBlockLink(network);
@@ -87,19 +92,69 @@ const TimelineContainer: React.FC<ITimelineContainerProps> = (props) => {
 			</section>
 		);
 	};
+
+	const Timeline = (isMobile:boolean) => {
+		return(
+			<section>
+				{
+					statuses.sort(sortfunc).map(({ block, status, timestamp }, index) => {
+						const blockDate = dayjs(timestamp);
+						let color;
+						if(status === 'DecisionDepositePlaced'){
+							status = 'Decision deposite placed';
+							color = 'FF67000';
+						}
+						else if(status === 'Executed' || status === 'Submitted'){
+							color = '#5BC044';
+						}
+						else{
+							color = '#407AFC';
+						}
+
+						return (
+							<div key={status} className={'border-t border-black-300'} style={{ borderTop: '1px solid #D2D8E0' }}>
+								<div className={'w-[680px]'}>
+									<article className="py-[8px]">
+										<div className="ml-[24px] flex items-center">
+											<div className="flex items-center space-x-[12px]"> {/* Add flex-grow here */}
+												<p className="text-xs text-sidebarBlue mb-0">
+													{blockDate.format("Do MMM 'YY, h:mm a")}
+												</p>
+												<a className="font-medium" href={`${url}${block}`} target="_blank" rel="noreferrer">
+													<ExportOutlined style={{ color: '#e5007a' }}/>
+												</a>
+											</div>
+											<div className="text-right ml-auto">
+												<p style={{ backgroundColor: color }} className={'text-white my-1 px-[15px] text-xs py-[5px] rounded-[50px] items-center'}>
+													{status}
+												</p>
+											</div>
+										</div>
+									</article>
+								</div>
+							</div>
+						);
+					})
+				}
+			</section>
+		);
+	};
+
 	return (
-		<section className='flex'>
-			<div className='min-h-[300px] bg-pink_primary w-[2px] relative'>
+		<section className='flex my-12 mx-7'>
+			<div className={`min-h-${minHeight} flex -mb-[2px] bg-pink_primary w-[2px] relative -ml-2`}>
 				<Link href={`/${getSinglePostLinkFromProposalType(getFirestoreProposalType(type as any) as any)}/${type === 'Tip'? timeline.hash: timeline.index}`}>
-					<span className='bg-pink_primary rounded-2xl font-medium text-base text-white whitespace-nowrap min-w-[100px] px-5 h-[33px] flex items-center justify-center absolute -left-5 -top-5 shadow-lg shadow-grey_secondary border-.15 border-solid border-pink_primary transition-all ease-in-out  hover:bg-white hover:text-pink_primary'>
-						{getStatus(String(type))}
-					</span>
+					<p className='font-medium text-base leading-6 text-pink_primary whitespace-nowrap px-2 h-[33px] flex items-center justify-center absolute -left-[15px] -top-7'>
+						<PlusSquareOutlined className="mr-3"/>
+						<span className='font-semibold text-base'>{getStatus(String(type))}</span>
+					</p>
 				</Link>
-				<span className='bg-pink_primary rounded-full absolute -bottom-1 -left-1 w-[10px] h-[10px]'>
+				<span className="flex justify-end mb-[200]px ml-[665px] -mt-[33px]">
+					<UpOutlined onClick={toggleCollapse}/>
 				</span>
 			</div>
-			<div className="hidden md:flex flex-1 overflow-x-scroll scroll-hidden cursor-ew-resize">
-				{TimelineItems(false)}
+			<div className={`${isCollapsed ? 'hidden' : ''} mt-3`}>
+				{Timeline(false)}
 			</div>
 			<div className="flex md:hidden flex-1 overflow-x-scroll scroll-hidden cursor-ew-resize">
 				{TimelineItems(true)}
@@ -107,5 +162,4 @@ const TimelineContainer: React.FC<ITimelineContainerProps> = (props) => {
 		</section>
 	);
 };
-
 export default TimelineContainer;

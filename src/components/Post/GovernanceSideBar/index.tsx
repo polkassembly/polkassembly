f00e4303e9bf580c10324542b402388dde13c015
rajdeep7Singh <sdrajdeep96@gmail.com>
2023-08-28 23:08:02 +0530

    lint fix

--- src/components/Post/GovernanceSideBar/index.tsx
+++ src/components/Post/GovernanceSideBar/index.tsx
@@ -167,7 +167,7 @@ const GovernanceSideBar: FC<IGovernanceSidebarProps> = (props) => {
 		gov2ReferendumStatus.DECIDING,
 		gov2ReferendumStatus.CONFIRM_STARTED,
 		gov2ReferendumStatus.DECISION_DEPOSIT_PLACED,
-		gov2ReferendumStatus.CONFIRM_ABORTED,
+		gov2ReferendumStatus.CONFIRM_ABORTED
 	].includes(post.status);
 
 	const unit =`${chainProperties[network]?.tokenSymbol}`;

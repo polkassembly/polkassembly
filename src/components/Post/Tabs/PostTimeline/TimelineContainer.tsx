be8850bf0f65bfb75b61f9a895bed1579d70543f
VAIBHAV-25 <vaibhavsinghvi254@gmail.com>
2023-09-05 21:24:51 +0530

    fix: removed space

--- src/components/Post/Tabs/PostTimeline/TimelineContainer.tsx
+++ src/components/Post/Tabs/PostTimeline/TimelineContainer.tsx
@@ -1,7 +1,6 @@
 // Copyright 2019-2025 @polkassembly/polkassembly authors & contributors
 // This software may be modified and distributed under the terms
 // of the Apache-2.0 license. See the LICENSE file for details.
-
 import { dayjs } from 'dayjs-init';
 import Link from 'next/link';
 import React, { useState } from 'react';

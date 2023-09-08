11f62f78821d6d04b2403b3bd58730ddf4582b7b
alphainfinitus <kumar.mridul000@gmail.com>
2023-09-08 23:09:42 +0530

    fix: :bug: where clause for isDeleted in missing queries

--- pages/api/v1/posts/off-chain-post.ts
+++ pages/api/v1/posts/off-chain-post.ts
@@ -212,7 +212,7 @@ export async function getOffChainPost(params: IGetOffChainPostParams) : Promise<
 			if (post.timeline && Array.isArray(post.timeline) && post.timeline.length > 0) {
 				const commentPromises = post.timeline.map(async (timeline: any) => {
 					const postDocRef = postsByTypeRef(network, getFirestoreProposalType(timeline.type) as ProposalType).doc(String(timeline.type === 'Tips'? timeline.hash: timeline.index));
-					const commentsCount = (await postDocRef.collection('comments').get()).size;
+					const commentsCount = (await postDocRef.collection('comments').where('isDeleted', '==', false).count().get())?.data()?.count;
 					return { ...timeline, commentsCount };
 				});
 				const timelines:Array<any>  = await Promise.allSettled(commentPromises);
--- pages/api/v1/posts/on-chain-post.ts
+++ pages/api/v1/posts/on-chain-post.ts
@@ -985,7 +985,7 @@ export async function getOnChainPost(params: IGetOnChainPostParams) : Promise<IA
 			if (post.timeline && Array.isArray(post.timeline) && post.timeline.length > 0) {
 				const commentPromises = post.timeline.map(async (timeline: any) => {
 					const postDocRef = postsByTypeRef(network, getFirestoreProposalType(timeline.type) as ProposalType).doc(String(timeline.type === 'Tip'? timeline.hash: timeline.index));
-					const commentsCount = (await postDocRef.collection('comments').count().get()).data().count;
+					const commentsCount = (await postDocRef.collection('comments').where('isDeleted', '==', false).count().get()).data().count;
 					return { ...timeline, commentsCount };
 				});
 				const timelines:Array<any> = await Promise.allSettled(commentPromises);

8be0842eccb943bf84b761e1825f95be83c1b88d
VAIBHAV-25 <vaibhavsinghvi254@gmail.com>
2023-10-11 17:02:21 +0530

    fix: minor fix

--- src/ui-components/CreationLabel.tsx
+++ src/ui-components/CreationLabel.tsx
@@ -151,9 +151,11 @@ const CreationLabel: FC<ICreationLabelProps> = (props) => {
 	};
 
 	const renderVoteContent = (vote: any, network: any, idx: number) => {
-		const lockPeriod = vote.lockPeriod === 0 ? '0.1' : vote.lockPeriod;
-		const conviction = vote?.decision === 'abstain' ? '0.1' : lockPeriod;
-		const balance = parseBalance((vote?.decision === 'abstain' ? vote?.balance?.abstain || 0 : vote?.balance?.value || 0).toString(), 2, true, network);
+		// const lockPeriod = vote.lockPeriod === 0 ? '0.1' : vote.lockPeriod;
+		// const conviction = vote?.decision === 'abstain' ? '0.1' : lockPeriod;
+		const conviction = 0.5;
+		// const balance = parseBalance((vote?.decision === 'abstain' ? vote?.balance?.abstain || 0 : vote?.balance?.value || 0).toString(), 2, true, network);
+		const balance = '2.43K KSM';
 		const balanceUnit = balance.slice(-3);
 		const balanceMatch = balance ? balance.match(/[\d.]+/) : null;
 		if (balance.slice(-5, -4) == 'K' && balanceMatch) {
@@ -168,6 +170,8 @@ const CreationLabel: FC<ICreationLabelProps> = (props) => {
 		} else if (balance.slice(-5, -4) == 'M' && balanceMatch) {
 			power = (parseFloat(power) / 1000000).toString();
 			power = `${power}M ${balanceUnit}`;
+		} else {
+			power = `${power} ${balanceUnit}`;
 		}
 
 		return (

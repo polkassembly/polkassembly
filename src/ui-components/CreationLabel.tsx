7cd9318ef840e04dcf8215c5b3d9eb70d7d9d8a2
VAIBHAV-25 <vaibhavsinghvi254@gmail.com>
2023-10-11 18:20:23 +0530

    fix: conv fixes

--- src/ui-components/CreationLabel.tsx
+++ src/ui-components/CreationLabel.tsx
@@ -151,7 +151,8 @@ const CreationLabel: FC<ICreationLabelProps> = (props) => {
 	};
 
 	const renderVoteContent = (vote: any, network: any, idx: number) => {
-		const conviction = 0.5;
+		const lockPeriod = vote.lockPeriod === 0 ? '0.1' : vote.lockPeriod;
+		const conviction = vote?.decision === 'abstain' ? '0.1' : lockPeriod;
 		const balance = parseBalance((vote?.decision === 'abstain' ? vote?.balance?.abstain || 0 : vote?.balance?.value || 0).toString(), 2, true, network);
 		const balanceUnit = balance.slice(-3);
 		const balanceMatch = balance ? balance.match(/[\d.]+/) : null;

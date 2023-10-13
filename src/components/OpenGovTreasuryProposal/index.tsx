d28d75ef6ec1f5c024d11a1de45f98dad66b4832
KanishkaRajputd <kanishka6393rajput@gmail.com>
2023-10-13 15:18:13 +0530

    feat(resolve build conflicts): :bug: build conflicts

--- src/components/OpenGovTreasuryProposal/index.tsx
+++ src/components/OpenGovTreasuryProposal/index.tsx
@@ -1,7 +1,7 @@
 // Copyright 2019-2025 @polkassembly/polkassembly authors & contributors
 // This software may be modified and distributed under the terms
 // of the Apache-2.0 license. See the LICENSE file for details.
-import React, { useContext, useEffect, useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import BN from 'bn.js';
 import { poppins } from 'pages/_app';
 import styled from 'styled-components';

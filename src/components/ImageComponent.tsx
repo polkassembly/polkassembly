99f1bdb4f2f0bbaf6a821e68e5f8852e4dcea234
KanishkaRajputd <kanishka6393rajput@gmail.com>
2023-10-31 17:59:17 +0530

    fix

--- src/components/ImageComponent.tsx
+++ src/components/ImageComponent.tsx
@@ -3,7 +3,7 @@
 // of the Apache-2.0 license. See the LICENSE file for details.
 
 import { Avatar } from 'antd';
-import React, { FC } from 'react';
+import React, { FC, useEffect, useState } from 'react';
 import DefaultProfile from '~assets/icons/dashboard-profile.svg';
 
 interface IImageComponentProps {
@@ -15,11 +15,22 @@ interface IImageComponentProps {
 
 const ImageComponent: FC<IImageComponentProps> = (props) => {
 	const { alt, className, src = '', iconClassName } = props;
+	const [isValid, setIsValid] = useState(false);
 	const regex = /\.(jpg|jpeg|png|gif|bmp|svg|tiff|ico)$/;
+
+	useEffect(() => {
+		if (!regex.test(src)) return;
+		(async () => {
+			const res = await fetch(src);
+			setIsValid(res.ok);
+		})();
+		// eslint-disable-next-line react-hooks/exhaustive-deps
+	}, []);
+
 	return (
 		<Avatar
 			className={className}
-			src={regex.test(src) ? src : '/assets/icons/user-profile.png'}
+			src={isValid ? src : '/assets/icons/user-profile.png'}
 			alt={alt}
 			icon={
 				<span className={iconClassName}>

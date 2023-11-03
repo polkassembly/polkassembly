32e06e20c9d50771b36c8793137b6799ce6224d5
VAIBHAV-25 <vaibhavsinghvi254@gmail.com>
2023-11-03 14:50:53 +0530

    fix: minor fixes

--- src/components/Login/Web3Login.tsx
+++ src/components/Login/Web3Login.tsx
@@ -351,8 +351,6 @@ const Web3Login: FC<Props> = ({
 						setLoginOpen?.(false);
 						setShowOptionalFields(false);
 					}
-					setLoginOpen?.(true);
-					setShowOptionalFields(true);
 					setIsClosable?.(false);
 					setLoading(false);
 					return;

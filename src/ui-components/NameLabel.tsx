cd0074df6960300138382946acc122c59fafdfb9
KanishkaRajputd <kanishka6393rajput@gmail.com>
2023-11-08 13:59:06 +0530

    fix

--- src/ui-components/NameLabel.tsx
+++ src/ui-components/NameLabel.tsx
@@ -6,10 +6,16 @@ import React, { useEffect, useState } from 'react';
 
 import Address from './Address';
 import { Tooltip } from 'antd';
-import QuickView from './QuickView';
+import QuickView, { TippingUnavailableNetworks } from './QuickView';
 import nextApiClientFetch from '~src/util/nextApiClientFetch';
 import { ISocial } from '~src/auth/types';
 import styled from 'styled-components';
+import { useNetworkSelector } from '~src/redux/selectors';
+import dynamic from 'next/dynamic';
+
+const Tipping = dynamic(() => import('~src/components/Tipping'), {
+	ssr: false
+});
 
 interface Props {
 	className?: string;
@@ -31,15 +37,22 @@ const NameLabel = ({
 	truncateUsername,
 	usernameMaxLength
 }: Props) => {
+	const { network } = useNetworkSelector();
 	const [open, setOpen] = useState<boolean>(false);
 	const [socials, setSocials] = useState<ISocial[]>([]);
 	const [profileCreatedAt, setProfileCreatedAt] = useState<Date | null>(null);
+	const [openTipping, setOpenTipping] = useState<boolean>(false);
+	const [openAddressChangeModal, setOpenAddressChangeModal] = useState<boolean>(false);
+	const [address, setAddress] = useState<string>('');
 
 	const getUserProfile = async () => {
 		const { data } = await nextApiClientFetch<any>(`api/v1/auth/data/userProfileWithUsername?username=${username}`);
 		if (data) {
 			setSocials(data?.social_links || []);
 			setProfileCreatedAt(data?.created_at || null);
+			if (data?.addresses) {
+				setAddress(data?.addresses[0]);
+			}
 		}
 	};
 	useEffect(() => {
@@ -50,59 +63,72 @@ const NameLabel = ({
 	}, [username]);
 
 	return (
-		<div
-			className={`${className}`}
-			title={username}
-		>
-			{!defaultAddress ? (
-				<Tooltip
-					arrow
-					color='#fff'
-					overlayClassName={className}
-					title={
-						<QuickView
-							socials={socials}
-							setOpen={setOpen}
-							profileCreatedAt={profileCreatedAt}
-							username={username || ''}
-							polkassemblyUsername={username}
-							enableTipping={false}
-						/>
-					}
-					open={!defaultAddress ? open : false}
-					onOpenChange={(e) => {
-						setOpen(e);
-					}}
-				>
-					<span
-						className={`username mr-1.5 font-semibold text-bodyBlue dark:text-blue-dark-high ${!disableAddressClick ? 'cursor-pointer hover:underline' : 'cursor-not-allowed'}`}
-						onClick={(e) => {
-							e.stopPropagation();
-							e.preventDefault();
-							if (!disableAddressClick) {
-								const routePath = `/user/${username}`;
-								window.open(routePath, '_blank');
-							}
+		<>
+			<div
+				className={`${className}`}
+				title={username}
+			>
+				{!defaultAddress ? (
+					<Tooltip
+						arrow
+						color='#fff'
+						overlayClassName={className}
+						title={
+							<QuickView
+								socials={socials}
+								setOpen={setOpen}
+								profileCreatedAt={profileCreatedAt}
+								username={username || ''}
+								polkassemblyUsername={username}
+								enableTipping={!!address}
+							/>
+						}
+						open={!defaultAddress ? open : false}
+						onOpenChange={(e) => {
+							setOpen(e);
 						}}
 					>
-						{username}
-					</span>
-				</Tooltip>
-			) : (
-				<Address
-					passedUsername={username}
-					address={defaultAddress}
-					className='text-sm'
-					displayInline
-					usernameClassName={usernameClassName}
-					disableIdenticon={disableIdenticon}
-					disableAddressClick={disableAddressClick}
-					isTruncateUsername={truncateUsername || false}
-					isSubVisible={false}
-					usernameMaxLength={usernameMaxLength}
+						<span
+							className={`username mr-1.5 font-semibold text-bodyBlue dark:text-blue-dark-high ${!disableAddressClick ? 'cursor-pointer hover:underline' : 'cursor-not-allowed'}`}
+							onClick={(e) => {
+								e.stopPropagation();
+								e.preventDefault();
+								if (!disableAddressClick) {
+									const routePath = `/user/${username}`;
+									window.open(routePath, '_blank');
+								}
+							}}
+						>
+							{username}
+						</span>
+					</Tooltip>
+				) : (
+					<Address
+						passedUsername={username}
+						address={defaultAddress}
+						className='text-sm'
+						displayInline
+						usernameClassName={usernameClassName}
+						disableIdenticon={disableIdenticon}
+						disableAddressClick={disableAddressClick}
+						isTruncateUsername={truncateUsername || false}
+						isSubVisible={false}
+						usernameMaxLength={usernameMaxLength}
+					/>
+				)}
+			</div>
+			{!TippingUnavailableNetworks.includes(network) && !!address && !defaultAddress && (
+				<Tipping
+					destinationAddress={address}
+					username={username || ''}
+					open={openTipping}
+					setOpen={setOpenTipping}
+					key={address}
+					setOpenAddressChangeModal={setOpenAddressChangeModal}
+					openAddressChangeModal={openAddressChangeModal}
 				/>
 			)}
-		</div>
+		</>
 	);
 };
 

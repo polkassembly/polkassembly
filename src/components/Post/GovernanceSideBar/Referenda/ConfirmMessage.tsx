817ad87eb7cbc6680d91fe48ac9bf7b977f5582b
Prince <this.is.kumar.prince@gmail.com>
2023-11-10 19:10:52 +0530

    fix: confirm message conditions

--- src/components/Post/GovernanceSideBar/Referenda/ConfirmMessage.tsx
+++ src/components/Post/GovernanceSideBar/Referenda/ConfirmMessage.tsx
@@ -1,7 +1,9 @@
 // Copyright 2019-2025 @polkassembly/polkassembly authors & contributors
 // This software may be modified and distributed under the terms
 // of the Apache-2.0 license. See the LICENSE file for details.
-import React, { useEffect, useState } from 'react';
+import dayjs from 'dayjs';
+import React, { memo, useEffect, useState } from 'react';
+import { usePostDataContext } from '~src/context';
 import { useCurvesInformationSelector } from '~src/redux/selectors';
 
 const getDays = (hrs: number) => {
@@ -14,41 +16,64 @@ const getExtraHrs = (hrs: number) => {
 	return hrs - days * 24;
 };
 
+const futurePoint = (data: any[], value: number) => {
+	const futureIdx = data.findIndex((data) => data && data?.y <= value);
+	if (futureIdx < 0) {
+		return;
+	}
+	const currentFuture = data[futureIdx];
+	const prevFuture = data[futureIdx - 1];
+	let fP = currentFuture;
+	const a = Math.abs(value - (currentFuture?.y || 0));
+	const b = Math.abs(value - (prevFuture?.y || 0));
+	if (a > b) {
+		fP = prevFuture;
+	} else {
+		fP = currentFuture;
+	}
+	return fP;
+};
+
 const ConfirmMessage = () => {
 	const curvesInformation = useCurvesInformationSelector();
+	const {
+		postData: { statusHistory }
+	} = usePostDataContext();
+	const { approval, approvalData, supportData, support, supportThreshold, approvalThreshold } = curvesInformation;
 	const [estimateHour, setEstimateHour] = useState(0);
 
 	useEffect(() => {
-		if (!curvesInformation) return;
-		const { currentSupportData, currentApprovalData, supportData, approvalData, support, approval, supportThreshold, approvalThreshold } = curvesInformation;
-		const currentSupport = currentSupportData?.[currentSupportData.length - 1];
-		const currentApproval = currentApprovalData?.[currentApprovalData.length - 1];
-		// If the current support and approval is already above the threshold, no need to estimate
-		if (!(support >= supportThreshold && approval >= approvalThreshold)) {
-			const futureSupport = supportData.find((data) => data && data?.y <= support);
-			const approvalPoint = approvalData.find((s) => s && futureSupport?.x && s.x >= futureSupport.x);
-			if (approvalPoint) {
-				if (approvalPoint.y <= 50) {
-					return setEstimateHour(0);
-				}
-			}
-			const futureApproval = approvalData.find((data) => data && data?.y <= approval);
-			const estimateSupportHour = (futureSupport?.x || 0) - currentSupport.x;
-			const estimateApprovalHour = (futureApproval?.x || 0) - currentApproval.x;
-			let estimateHour = 0;
-			if (support < supportThreshold && approval < approvalThreshold) {
-				estimateHour = Math.max(estimateSupportHour, estimateApprovalHour);
-			} else if (support < supportThreshold) {
-				estimateHour = (futureSupport?.x || 0) - currentSupport.x;
-			} else {
-				estimateHour = (futureApproval?.x || 0) - currentApproval.x;
-			}
-			setEstimateHour(estimateHour);
+		const statusObj = statusHistory?.find((s) => s?.status === 'Deciding');
+		if (!statusObj) {
+			return;
 		}
-	}, [curvesInformation]);
+		if (support >= supportThreshold && approval >= approvalThreshold) {
+			return;
+		}
+		const futureSupport = futurePoint(supportData, support);
+		const futureApproval = futurePoint(approvalData, approval);
+		if (!futureSupport || !futureApproval) {
+			return;
+		}
+
+		const hour = dayjs().diff(statusObj?.timestamp, 'hour');
+		const estimateSupportHour = (futureSupport?.x || 0) - hour;
+		const estimateApprovalHour = (futureApproval?.x || 0) - hour;
+		let estimateHour = 0;
+		if (support < supportThreshold && approval < approvalThreshold) {
+			estimateHour = Math.max(estimateSupportHour, estimateApprovalHour);
+		} else if (support < supportThreshold) {
+			estimateHour = estimateSupportHour;
+		} else {
+			estimateHour = estimateApprovalHour;
+		}
+		setEstimateHour(estimateHour);
+	}, [approval, approvalData, approvalThreshold, statusHistory, support, supportData, supportThreshold]);
+
 	if (!estimateHour) {
 		return null;
 	}
+
 	return (
 		<p className='dark:text-white'>
 			Confirm in {getDays(estimateHour) ? `${getDays(estimateHour)} days` : ''} {getExtraHrs(estimateHour) ? `${getExtraHrs(estimateHour)} hrs` : ''}
@@ -56,4 +81,4 @@ const ConfirmMessage = () => {
 	);
 };
 
-export default ConfirmMessage;
+export default memo(ConfirmMessage);

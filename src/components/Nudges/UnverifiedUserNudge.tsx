672510d0b1780903b5a2e03072c85ad0aab4480f
Adetoye Adewoye <71382301+adetoye-dev@users.noreply.github.com>
2023-11-22 07:46:59 +0100

    Implement view once per visit on nudge

--- src/components/Nudges/UnverifiedUserNudge.tsx
+++ src/components/Nudges/UnverifiedUserNudge.tsx
@@ -1,38 +1,48 @@
 // Copyright 2019-2025 @polkassembly/polkassembly authors & contributors
 // This software may be modified and distributed under the terms
 // of the Apache-2.0 license. See the LICENSE file for details.
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { Layout } from 'antd';
 import { CloseIcon } from '~src/ui-components/CustomIcons';
 import ShieldUserIcon from '~assets/icons/shield-user-icon.svg';
+import { setCookie, getCookie } from 'cookies-next';
 
 const UnverifiedUserNudge = () => {
 	const [isOpen, setIsOpen] = useState<boolean>(true);
 
+	useEffect(() => {
+		const viewedNudge = getCookie('nudge_viewed');
+
+		if (viewedNudge) {
+			setIsOpen(false);
+		}
+	}, []);
+
+	function handleNudgeClose() {
+		setCookie('nudge_viewed', true);
+		setIsOpen(false);
+	}
+
+	if (!isOpen) return null;
+
 	return (
-		<>
-			{isOpen ? (
-				<Layout className='flex flex-row'>
-					<div className='hidden w-[72px] lg:block '></div>
-					<div className='flex w-full flex-row items-center justify-between gap-8 bg-purple-600 px-4 py-2 md:px-8 lg:px-20'>
-						<div className='flex flex-col gap-2 sm:inline-flex sm:flex-row sm:items-center'>
-							Identity has not been verified yet
-							<span className='inline-flex items-center gap-2 rounded-md bg-purple-900 px-2 py-1'>
-								<ShieldUserIcon /> Set on-chain identity
-							</span>
-						</div>
-						<span
-							className='ml-auto'
-							onClick={() => setIsOpen(false)}
-						>
-							<CloseIcon className='text-white' />
-						</span>
-					</div>
-				</Layout>
-			) : (
-				''
-			)}
-		</>
+		<Layout className='flex flex-row'>
+			<div className='hidden w-[72px] lg:block '></div>
+			<div className='flex w-full flex-row items-center justify-between gap-8 bg-purple-600 px-4 py-2 md:px-8 lg:px-20'>
+				<div className='flex flex-col gap-2 sm:inline-flex sm:flex-row sm:items-center'>
+					Identity has not been verified yet
+					<span className='inline-flex items-center gap-2 rounded-md bg-purple-900 px-2 py-1'>
+						<ShieldUserIcon /> Set on-chain identity
+					</span>
+				</div>
+				<span
+					className='ml-auto'
+					onClick={handleNudgeClose}
+				>
+					<CloseIcon className='text-white' />
+				</span>
+			</div>
+		</Layout>
 	);
 };
 

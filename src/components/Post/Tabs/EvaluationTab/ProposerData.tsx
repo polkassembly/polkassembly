61728bfb704ac0e1522d4cd524cdcb5ec6321621
VAIBHAV-25 <vaibhavsinghvi254@gmail.com>
2023-12-06 21:55:46 +0530

    fix: added voting power

--- src/components/Post/Tabs/EvaluationTab/ProposerData.tsx
+++ src/components/Post/Tabs/EvaluationTab/ProposerData.tsx
@@ -6,7 +6,7 @@
 // of the Apache-2.0 license. See the LICENSE file for details.
 import { Divider, message } from 'antd';
 
-import React, { FC } from 'react';
+import React, { FC, useEffect, useState } from 'react';
 import ImageComponent from '~src/components/ImageComponent';
 import CopyIcon from '~assets/icons/content_copy_small.svg';
 import WhiteCopyIcon from '~assets/icons/content_copy_small_white.svg';
@@ -18,7 +18,14 @@ import CalenderIcon from '~assets/icons/Calendar.svg';
 import dayjs from 'dayjs';
 import { useTheme } from 'next-themes';
 import Address from '~src/ui-components/Address';
+import userProfileBalances from '~src/util/userProfieBalances';
+import { useApiContext } from '~src/context';
+import { useNetworkSelector } from '~src/redux/selectors';
+import BN from 'bn.js';
+import { formatedBalance } from '~src/util/formatedBalance';
+import { chainProperties } from '~src/global/networkConstants';
 
+const ZERO_BN = new BN(0);
 interface IProposerData {
 	className?: string;
 	address?: any;
@@ -28,8 +35,23 @@ interface IProposerData {
 
 const ProposerData: FC<IProposerData> = (props) => {
 	const { className, address, profileData } = props;
+	const [transferableBalance, setTransferableBalance] = useState<BN>(ZERO_BN);
+
 	const [messageApi, contextHolder] = message.useMessage();
 	const { resolvedTheme: theme } = useTheme();
+	const { api, apiReady } = useApiContext();
+	const { network } = useNetworkSelector();
+	const unit = chainProperties[network]?.tokenSymbol;
+
+	useEffect(() => {
+		if (!api || !apiReady) return;
+		(async () => {
+			const balances = await userProfileBalances({ address, api, apiReady, network });
+			setTransferableBalance(balances?.transferableBalance || ZERO_BN);
+			console.log(balances, transferableBalance);
+		})();
+		// eslint-disable-next-line react-hooks/exhaustive-deps
+	}, [address]);
 
 	const success = () => {
 		messageApi.open({
@@ -128,7 +150,9 @@ const ProposerData: FC<IProposerData> = (props) => {
 					<MessageIcon />
 					<div className='-mt-1'>
 						<p className='m-0 p-0 text-[10px] text-lightBlue opacity-70 dark:text-lightGreyTextColor'>Voting Power</p>
-						<span className='m-0 p-0 text-sm font-semibold text-bodyBlue dark:text-white'>{dayjs(profileData?.created_at as string).format('DD MMM YYYY')}</span>
+						<span className='m-0 p-0 text-sm font-semibold text-bodyBlue dark:text-white'>
+							{formatedBalance((transferableBalance.toString() || '0').toString(), unit, 2)} {unit}
+						</span>
 					</div>
 				</div>
 			</div>

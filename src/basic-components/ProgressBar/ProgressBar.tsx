8cef20594d56d12b645aa0de273aed5ac1434648
VAIBHAV-25 <vaibhavsinghvi254@gmail.com>
2023-12-18 13:27:05 +0530

    design system implementation for progress bar

---
+++ src/basic-components/ProgressBar/ProgressBar.tsx
@@ -0,0 +1,35 @@
+// Copyright 2019-2025 @polkassembly/polkassembly authors & contributors
+// This software may be modified and distributed under the terms
+// of the Apache-2.0 license. See the LICENSE file for details.
+import React, { FC, PropsWithChildren } from 'react';
+import { Progress as ANTDProgressBar } from 'antd';
+
+interface IProgressBar {
+	className?: string;
+	showInfo?: boolean;
+	percent?: any;
+	strokeColor?: string;
+	size?: any;
+	trailColor?: string;
+	strokeWidth?: number;
+	format?: any;
+	type?: any;
+}
+const ProgressBar: FC<PropsWithChildren<IProgressBar>> = (props) => {
+	const { strokeWidth, type, format, showInfo, percent, strokeColor, size, trailColor, className } = props;
+	return (
+		<ANTDProgressBar
+			className={`${className}`}
+			type={type}
+			format={format}
+			showInfo={showInfo}
+			percent={percent}
+			strokeColor={strokeColor}
+			size={size}
+			strokeWidth={strokeWidth}
+			trailColor={trailColor}
+		/>
+	);
+};
+
+export default ProgressBar;

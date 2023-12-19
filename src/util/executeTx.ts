4410fa4f24f8097e0c2896f68c1ec6c86d547aaf
alphainfinitus <kumar.mridul000@gmail.com>
2023-12-20 02:41:05 +0530

    feat: :construction: proxy tx scaffolding

--- src/util/executeTx.ts
+++ src/util/executeTx.ts
@@ -20,54 +20,64 @@ interface Props {
 }
 const executeTx = async ({ api, apiReady, network, tx, address, params = {}, errorMessageFallback, onSuccess, onFailed, onBroadcast, setStatus }: Props) => {
 	if (!api || !apiReady || !tx) return;
-	tx.signAndSend(address, params, async ({ status, events, txHash }: any) => {
-		if (status.isInvalid) {
-			console.log('Transaction invalid');
-			setStatus?.('Transaction invalid');
-		} else if (status.isReady) {
-			console.log('Transaction is ready');
-			setStatus?.('Transaction is ready');
-		} else if (status.isBroadcast) {
-			console.log('Transaction has been broadcasted');
-			setStatus?.('Transaction has been broadcasted');
-			onBroadcast && onBroadcast?.();
-		} else if (status.isInBlock) {
-			console.log('Transaction is in block');
-			setStatus?.('Transaction is in block');
 
-			for (const { event } of events) {
-				if (event.method === 'ExtrinsicSuccess') {
-					setStatus?.('Transaction Success');
-					await onSuccess(txHash);
-				} else if (event.method === 'ExtrinsicFailed') {
-					setStatus?.('Transaction failed');
-					console.log('Transaction failed');
-					setStatus?.('Transaction failed');
-					const dispatchError = (event.data as any)?.dispatchError;
+	const isProxyAddress = false;
+	const realAddress = '';
 
-					if (dispatchError?.isModule) {
-						const errorModule = (event.data as any)?.dispatchError?.asModule;
-						const { method, section, docs } = api.registry.findMetaError(errorModule);
-						errorMessageFallback = `${section}.${method} : ${docs.join(' ')}`;
-						console.log(errorMessageFallback, 'error module');
-						await onFailed(errorMessageFallback);
-					} else if (dispatchError?.isToken) {
-						console.log(`${dispatchError.type}.${dispatchError.asToken.type}`);
-						await onFailed(`${dispatchError.type}.${dispatchError.asToken.type}`);
-					} else {
-						await onFailed(`${dispatchError.type}` || errorMessageFallback);
+	// TODO: find a way to find if it is a proxy address and to get the real address from the proxy address
+
+	const extrinsic = isProxyAddress && realAddress ? api.tx.proxy.proxy(realAddress, null, tx) : tx;
+
+	extrinsic
+		.signAndSend(address, params, async ({ status, events, txHash }: any) => {
+			if (status.isInvalid) {
+				console.log('Transaction invalid');
+				setStatus?.('Transaction invalid');
+			} else if (status.isReady) {
+				console.log('Transaction is ready');
+				setStatus?.('Transaction is ready');
+			} else if (status.isBroadcast) {
+				console.log('Transaction has been broadcasted');
+				setStatus?.('Transaction has been broadcasted');
+				onBroadcast && onBroadcast?.();
+			} else if (status.isInBlock) {
+				console.log('Transaction is in block');
+				setStatus?.('Transaction is in block');
+
+				for (const { event } of events) {
+					if (event.method === 'ExtrinsicSuccess') {
+						setStatus?.('Transaction Success');
+						await onSuccess(txHash);
+					} else if (event.method === 'ExtrinsicFailed') {
+						setStatus?.('Transaction failed');
+						console.log('Transaction failed');
+						setStatus?.('Transaction failed');
+						const dispatchError = (event.data as any)?.dispatchError;
+
+						if (dispatchError?.isModule) {
+							const errorModule = (event.data as any)?.dispatchError?.asModule;
+							const { method, section, docs } = api.registry.findMetaError(errorModule);
+							errorMessageFallback = `${section}.${method} : ${docs.join(' ')}`;
+							console.log(errorMessageFallback, 'error module');
+							await onFailed(errorMessageFallback);
+						} else if (dispatchError?.isToken) {
+							console.log(`${dispatchError.type}.${dispatchError.asToken.type}`);
+							await onFailed(`${dispatchError.type}.${dispatchError.asToken.type}`);
+						} else {
+							await onFailed(`${dispatchError.type}` || errorMessageFallback);
+						}
 					}
 				}
+			} else if (status.isFinalized) {
+				console.log(`Transaction has been included in blockHash ${status.asFinalized.toHex()}`);
+				console.log(`tx: https://${network}.subscan.io/extrinsic/${txHash}`);
 			}
-		} else if (status.isFinalized) {
-			console.log(`Transaction has been included in blockHash ${status.asFinalized.toHex()}`);
-			console.log(`tx: https://${network}.subscan.io/extrinsic/${txHash}`);
-		}
-	}).catch((error: unknown) => {
-		console.log(':( transaction failed');
-		setStatus?.(':( transaction failed');
-		console.error('ERROR:', error);
-		onFailed(error?.toString?.() || errorMessageFallback);
-	});
+		})
+		.catch((error: unknown) => {
+			console.log(':( transaction failed');
+			setStatus?.(':( transaction failed');
+			console.error('ERROR:', error);
+			onFailed(error?.toString?.() || errorMessageFallback);
+		});
 };
 export default executeTx;

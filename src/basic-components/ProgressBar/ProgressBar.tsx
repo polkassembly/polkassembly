54b24bf9c87d71665c9e33ebd339d47e909076b4
VAIBHAV-25 <vaibhavsinghvi254@gmail.com>
2023-12-19 15:23:51 +0530

    fix: antd props

--- src/basic-components/ProgressBar/ProgressBar.tsx
+++ src/basic-components/ProgressBar/ProgressBar.tsx
@@ -2,37 +2,17 @@
 // This software may be modified and distributed under the terms
 // of the Apache-2.0 license. See the LICENSE file for details.
 import React, { FC, PropsWithChildren } from 'react';
-import { Progress as ANTDProgressBar } from 'antd';
+import { Progress as ANTDProgressBar, ProgressProps } from 'antd';
 
-interface IProgressBar {
+interface IProgressBar extends ProgressProps {
 	className?: string;
-	showInfo?: boolean;
-	percent?: any;
-	success?: any;
-	strokeColor?: string;
-	size?: any;
-	trailColor?: string;
-	strokeWidth?: number;
-	gapPosition?: 'top' | 'bottom' | 'left' | 'right' | undefined;
-	format?: any;
-	type?: any;
 }
 const ProgressBar: FC<PropsWithChildren<IProgressBar>> = (props) => {
-	const { gapPosition, success, strokeWidth, type, format, showInfo, percent, strokeColor, size, trailColor, className } = props;
+	const { className } = props;
 	return (
 		<ANTDProgressBar
 			className={`${className}`}
 			{...props}
-			type={type}
-			format={format}
-			showInfo={showInfo}
-			percent={percent}
-			strokeColor={strokeColor}
-			size={size}
-			success={success}
-			gapPosition={gapPosition}
-			strokeWidth={strokeWidth}
-			trailColor={trailColor}
 		/>
 	);
 };

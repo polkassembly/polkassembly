4853150ed041bd0508283b3163aee772836150df
Nikhil Ranjan <niklabh811@gmail.com>
2023-12-24 04:22:33 +0530

    prettier

--- src/components/AppLayout/index.tsx
+++ src/components/AppLayout/index.tsx
@@ -866,7 +866,7 @@ const AppLayout = ({ className, Component, pageProps }: Props) => {
 					<span className='dark:text-white'>Please use your desktop computer to verify on chain identity</span>
 				</div>
 			</Modal>
-			{ theme == 'dark' && process.browser && <Snowfall snowflakeCount={100} />}
+			{theme == 'dark' && process.browser && <Snowfall snowflakeCount={100} />}
 		</Layout>
 	);
 };

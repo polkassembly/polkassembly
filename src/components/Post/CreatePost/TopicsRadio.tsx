1f0661773ecc75be321d3cee25e83db76506b81d
KanishkaRajputd <kanishka6393rajput@gmail.com>
2023-12-26 18:47:08 +0530

    hot fix

--- src/components/Post/CreatePost/TopicsRadio.tsx
+++ src/components/Post/CreatePost/TopicsRadio.tsx
@@ -60,14 +60,22 @@ const TopicsRadio = ({ className, onTopicSelection, govType, topicId }: Props) =
 			]);
 		} else if (govType === EGovType.OPEN_GOV) {
 			if (
-				![post_topic.AUCTION_ADMIN, post_topic.FELLOWSHIP, post_topic.GOVERNANCE, post_topic.ROOT, post_topic.STAKING_ADMIN, post_topic.TREASURY, post_topic.WHITELIST].includes(
-					topicId
-				)
+				![
+					post_topic.AUCTION_ADMIN,
+					post_topic.FELLOWSHIP,
+					post_topic.GOVERNANCE,
+					post_topic.ROOT,
+					post_topic.STAKING_ADMIN,
+					post_topic.TREASURY,
+					post_topic.WHITELIST,
+					post_topic.GENERAL
+				].includes(topicId)
 			) {
-				onTopicSelection(8);
+				onTopicSelection(5);
 			}
 
 			setTopicOptions([
+				topicToOptionText('GENERAL'),
 				topicToOptionText('AUCTION_ADMIN'),
 				topicToOptionText('FELLOWSHIP'),
 				topicToOptionText('GOVERNANCE'),

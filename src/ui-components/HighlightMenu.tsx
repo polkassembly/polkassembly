981fefb747a22a48ae7b2d3d48e86373b7f04a12
Adetoye Adewoye <adetoyeadewoye@gmail.com>
2024-01-25 07:17:09 +0100

    implement quote comment function

--- src/ui-components/HighlightMenu.tsx
+++ src/ui-components/HighlightMenu.tsx
@@ -5,13 +5,16 @@
 import React, { useState, useEffect, useRef } from 'react';
 import QuoteIcon from '~assets/icons/quote-icon.svg';
 import TwitterIcon from '~assets/icons/twitter.svg';
+import { useQuoteCommentContext } from '~src/context';
 
 interface IHiglightMenuProps {
 	markdownRef: React.RefObject<HTMLDivElement>;
 }
 const HighlightMenu = ({ markdownRef }: IHiglightMenuProps) => {
+	const { setQuotedText } = useQuoteCommentContext();
 	const [selectedText, setSelectedText] = useState('');
 	const [menuPosition, setMenuPosition] = useState({ left: 0, top: 0 });
+
 	const menuRef = useRef<HTMLDivElement>(null);
 
 	useEffect(() => {
@@ -62,20 +65,30 @@ const HighlightMenu = ({ markdownRef }: IHiglightMenuProps) => {
 		setSelectedText('');
 	};
 
+	const handleQuote = () => {
+		setQuotedText(selectedText);
+		const commentForm = document.getElementById('comment-form');
+
+		commentForm?.scrollIntoView({ behavior: 'smooth', block: 'center' });
+	};
+
 	return (
 		<div
 			ref={menuRef}
 			className={`fixed z-[999] ${
 				selectedText ? 'block' : 'hidden'
-			} flex flex-col gap-1 rounded-md bg-[#363636] p-2 text-xs after:absolute after:left-[70%] after:top-[40px] after:-z-10 after:h-4 after:w-4 after:-translate-x-1/2 after:rotate-45 after:transform after:border-8 after:border-solid after:border-[#363636] after:content-['']`}
+			} flex flex-col gap-1 rounded-md bg-[#363636] p-2 text-xs text-white after:absolute after:left-[70%] after:top-[40px] after:-z-10 after:h-4 after:w-4 after:-translate-x-1/2 after:rotate-45 after:transform after:border-8 after:border-solid after:border-[#363636] after:content-['']`}
 			style={{ left: menuPosition.left, top: menuPosition.top - 10 }}
 		>
-			<div className='flex h-4 cursor-pointer gap-1'>
+			<div
+				className='flex h-4 cursor-pointer items-center gap-1'
+				onClick={handleQuote}
+			>
 				<QuoteIcon className='w-3' />
 				Quote
 			</div>
 			<div
-				className='flex h-4 cursor-pointer gap-1'
+				className='flex h-4 cursor-pointer items-center gap-1'
 				onClick={shareSelection}
 			>
 				<TwitterIcon className='w-3 fill-white' />

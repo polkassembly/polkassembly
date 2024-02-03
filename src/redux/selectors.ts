6530895269a4518fa5f99ea427ddcea7732c442f
Jasmeet Singh <jasmeetsonu990@gmail.com>
2024-02-03 14:22:31 +0530

    removing redux

--- src/redux/selectors.ts
+++ src/redux/selectors.ts
@@ -12,7 +12,6 @@ import { ICurvesInformationStore } from './curvesInformation/@types';
 import { ITippingStore } from './Tipping/@types';
 import { ITreasuryProposalStore } from './treasuryProposal/@types';
 import { IVoteDataStore } from './voteData/@types';
-import { IDelegationDashboard } from './delegationDashboard/@types';
 
 const useNetworkSelector = () => {
 	return useSelector<TAppState, INetworkStore>((state) => state?.network);
@@ -40,9 +39,6 @@ const useTreasuryProposalSelector = () => {
 const useVoteDataSelector = () => {
 	return useSelector<TAppState, IVoteDataStore>((state) => state.voteData);
 };
-const useDelegationDashboardSelector = () => {
-	return useSelector<TAppState, IDelegationDashboard>((state) => state.delegationDashboard);
-};
 
 export {
 	useNetworkSelector,
@@ -52,6 +48,5 @@ export {
 	useCurvesInformationSelector,
 	useTippingDataSelector,
 	useTreasuryProposalSelector,
-	useVoteDataSelector,
-	useDelegationDashboardSelector
+	useVoteDataSelector
 };

649161e6c13c27392ed70a42a7ce97e01ff96321
VAIBHAV-25 <vaibhavsinghvi254@gmail.com>
2024-02-03 11:09:15 +0530

    fix: passed theme attribute

--- src/ui-components/LatestActivityStates.tsx
+++ src/ui-components/LatestActivityStates.tsx
@@ -1,7 +1,7 @@
 // Copyright 2019-2025 @polkassembly/polkassembly authors & contributors
 // This software may be modified and distributed under the terms
 // of the Apache-2.0 license. See the LICENSE file for details.
-
+// import { Divider, Table } from 'antd';
 import { Divider } from 'antd';
 import Table from '~src/basic-components/Tables/Table';
 import { ColumnsType } from 'antd/lib/table';
@@ -51,11 +51,13 @@ interface IPopulatedLatestActivityProps {
 }
 
 export const PopulatedLatestActivity: FC<IPopulatedLatestActivityProps> = ({ columns, tableData, onClick }) => {
+	const { resolvedTheme: theme } = useTheme();
 	return (
 		<Table
 			columns={columns}
 			dataSource={tableData}
 			pagination={false}
+			theme={theme}
 			scroll={{ x: 1000, y: 650 }}
 			onRow={(rowData) => {
 				return {

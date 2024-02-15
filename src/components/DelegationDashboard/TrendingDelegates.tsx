f6050f1c4e66406158e69b9d2389a04e8c143533
Jasmeet Singh <jasmeetsonu990@gmail.com>
2024-02-16 01:28:04 +0530

    search func in trending delegates

--- src/components/DelegationDashboard/TrendingDelegates.tsx
+++ src/components/DelegationDashboard/TrendingDelegates.tsx
@@ -8,19 +8,49 @@ import DelegateCard from './DelegateCard';
 import ImageIcon from '~src/ui-components/ImageIcon';
 import { Pagination } from '~src/ui-components/Pagination';
 import { useTheme } from 'next-themes';
-import { Spin } from 'antd';
+import { Alert, Spin } from 'antd';
 import getSubstrateAddress from '~src/util/getSubstrateAddress';
+import Input from '~src/basic-components/Input';
+import CustomButton from '~src/basic-components/buttons/CustomButton';
+import getEncodedAddress from '~src/util/getEncodedAddress';
+import DelegatesProfileIcon from '~assets/icons/white-delegated-profile.svg';
+import { useNetworkSelector, useUserDetailsSelector } from '~src/redux/selectors';
+import Web3 from 'web3';
+import DelegateModal from '../Listing/Tracks/DelegateModal';
+import { useApiContext } from '~src/context';
 
 const TrendingDelegates = () => {
+	const { network } = useNetworkSelector();
+	const { api, apiReady } = useApiContext();
+	const { delegationDashboardAddress } = useUserDetailsSelector();
 	const [loading, setLoading] = useState<boolean>(false);
 	const [delegatesData, setDelegatesData] = useState<IDelegate[]>([]);
 	const [currentPage, setCurrentPage] = useState<number>(1);
 	const [showMore, setShowMore] = useState<boolean>(false);
+	const [addressAlert, setAddressAlert] = useState<boolean>(false);
+	const [open, setOpen] = useState<boolean>(false);
 	const { resolvedTheme: theme } = useTheme();
+	const [address, setAddress] = useState<string>('');
+
+	useEffect(() => {
+		if (!address) return;
+		if ((getEncodedAddress(address, network) || Web3.utils.isAddress(address)) && address !== getEncodedAddress(address, network)) {
+			setAddressAlert(true);
+		}
+		setTimeout(() => {
+			setAddressAlert(false);
+		}, 5000);
+	}, [network, address]);
 
 	const getData = async () => {
+		if (!api || !apiReady) return;
+
+		if (!(getEncodedAddress(address, network) || Web3.utils.isAddress(address)) && address.length > 0) return;
 		setLoading(true);
-		const { data, error } = await nextApiClientFetch<IDelegate[]>('api/v1/delegations/delegates');
+
+		const { data, error } = await nextApiClientFetch<IDelegate[]>('api/v1/delegations/delegates', {
+			address: address
+		});
 		if (data) {
 			setDelegatesData(data);
 		} else {
@@ -32,7 +62,7 @@ const TrendingDelegates = () => {
 	useEffect(() => {
 		getData();
 		// eslint-disable-next-line react-hooks/exhaustive-deps
-	}, []);
+	}, [address, delegationDashboardAddress]);
 
 	const itemsPerPage = showMore ? delegatesData.length : 6;
 	const totalPages = Math.ceil(delegatesData.length / itemsPerPage);
@@ -108,6 +138,64 @@ const TrendingDelegates = () => {
 				</span>
 			</div>
 
+			{/* {disabled && (
+				<Alert
+					className='text-sm font-normal text-bodyBlue dark:border-separatorDark dark:bg-[#05263F]'
+					showIcon
+					message={<span className='dark:text-blue-dark-high'>You have already delegated for this track.</span>}
+				/>
+			)} */}
+			{/* <h4 className={`mb-4 mt-4 text-sm font-normal text-bodyBlue dark:text-white ${disabled && 'opacity-50'}`}> */}
+			<h4 className={'mb-4 mt-4 text-sm font-normal text-bodyBlue dark:text-white '}>Enter an address or Select from the list below to delegate your voting power</h4>
+
+			<div className='flex items-center gap-4'>
+				<div className='dark:placeholder:white flex h-[48px] w-full items-center justify-between rounded-md border-[1px] border-solid border-[#D2D8E0] text-[14px] font-normal text-[#576D8BCC] dark:border-[#3B444F] dark:border-separatorDark dark:text-white'>
+					{/* Input Component */}
+					<Input
+						// disabled={disabled}
+						placeholder='Enter address to Delegate vote'
+						onChange={(e) => setAddress(e.target.value)}
+						value={address}
+						className='h-[44px] dark:border-separatorDark dark:bg-transparent dark:text-blue-dark-high dark:focus:border-[#91054F]'
+					/>
+
+					<CustomButton
+						variant='primary'
+						className={'ml-1 mr-1 justify-around gap-2 px-4 py-1'}
+						// className={`ml-1 mr-1 justify-around gap-2 px-4 py-1 ${disabled && 'opacity-50'}`}
+						height={40}
+						onClick={() => {
+							setOpen(true);
+							setAddress(address);
+						}}
+						disabled={
+							!address ||
+							!(getEncodedAddress(address, network) || Web3.utils.isAddress(address)) ||
+							address === delegationDashboardAddress ||
+							getEncodedAddress(address, network) === delegationDashboardAddress
+							// disabled
+						}
+					>
+						<DelegatesProfileIcon />
+						<span className='text-sm font-medium text-white'>Delegate</span>
+					</CustomButton>
+				</div>
+			</div>
+
+			{getEncodedAddress(address, network) === delegationDashboardAddress && (
+				<label className='mt-1 text-sm font-normal text-red-500'>You cannot delegate to your own address. Please enter a different wallet address.</label>
+			)}
+
+			{!address || (!(getEncodedAddress(address, network) || Web3.utils.isAddress(address)) && <label className='mt-1 text-sm font-normal text-red-500 '>Invalid Address.</label>)}
+			{addressAlert && (
+				<Alert
+					className='mb-4 mt-4 dark:border-infoAlertBorderDark dark:bg-infoAlertBgDark'
+					showIcon
+					type='info'
+					message={<span className='dark:text-blue-dark-high'>The substrate address has been changed to Kusama address.</span>}
+				/>
+			)}
+
 			<Spin spinning={loading}>
 				<div>
 					<div className='mt-6 grid grid-cols-2 gap-6 max-lg:grid-cols-1'>
@@ -143,6 +231,11 @@ const TrendingDelegates = () => {
 					)}
 				</div>
 			</Spin>
+			<DelegateModal
+				defaultTarget={address}
+				open={open}
+				setOpen={setOpen}
+			/>
 		</div>
 	);
 };

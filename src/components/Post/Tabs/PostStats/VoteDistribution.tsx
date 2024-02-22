fb52e6ec08f423a2cf55c3bebc6ef15055c0301f
Adetoye Adewoye <adetoyeadewoye@gmail.com>
2024-02-22 07:43:23 +0100

    add votes distribution legend

--- src/components/Post/Tabs/PostStats/VoteDistribution.tsx
+++ src/components/Post/Tabs/PostStats/VoteDistribution.tsx
@@ -2,16 +2,19 @@
 // This software may be modified and distributed under the terms
 // of the Apache-2.0 license. See the LICENSE file for details.
 import { Card } from 'antd';
+import { useTheme } from 'next-themes';
 
 const VoteDistribution = ({ votesDistribution }: { votesDistribution: { ayes: any[]; nays: any[]; abstain: any[] } }) => {
+	const { resolvedTheme: theme } = useTheme();
+
 	const sortedAyes = votesDistribution.ayes.sort((a, b) => b.balance - a.balance);
 	const sortedNays = votesDistribution.nays.sort((a, b) => b.balance - a.balance);
 	const sortedAbstain = votesDistribution.abstain.sort((a, b) => b.balance - a.balance);
 
 	const colors: { [key: string]: string } = {
-		abstain: '#407BFF',
-		aye: '#6DE1A2',
-		nay: '#FF778F'
+		abstain: theme === 'dark' ? '#407BFF' : '#407BFF',
+		aye: theme === 'dark' ? '#64A057' : '#2ED47A',
+		nay: theme === 'dark' ? '#BD2020' : '#E84865'
 	};
 
 	const gridStyle = {
@@ -26,7 +29,23 @@ const VoteDistribution = ({ votesDistribution }: { votesDistribution: { ayes: an
 
 	return (
 		<Card className='mx-auto h-fit max-h-[500px] w-full flex-1 rounded-xxl border-[#D2D8E0] bg-white p-0 text-blue-light-high dark:bg-section-dark-overlay dark:text-white'>
-			<h2 className='text-xl font-semibold'>Vote Distribution</h2>
+			<div className='flex flex-col items-center justify-between gap-5 md:flex-row'>
+				<h2 className='text-xl font-semibold'>Vote Distribution</h2>
+				<div className='flex items-center gap-5 text-xs'>
+					<div className='flex items-center gap-2'>
+						<div className='h-1.5 w-1.5 rounded-full bg-ayeGreenColor'></div>
+						<span>Aye</span>
+					</div>
+					<div className='flex items-center gap-2'>
+						<div className='h-1.5 w-1.5 rounded-full bg-nayRedColor'></div>
+						<span>Nay</span>
+					</div>
+					<div className='flex items-center gap-2'>
+						<div className='h-1.5 w-1.5 rounded-full bg-abstainBlueColor'></div>
+						<span>Abstain</span>
+					</div>
+				</div>
+			</div>
 			<div className='flex w-full flex-col gap-2 overflow-hidden lg:flex-row'>
 				{sortedAyes.length ? (
 					<div

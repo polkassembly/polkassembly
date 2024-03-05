af3e292a302c9720b9414c7ddc75d4cfdb308759
Jasmeet Singh <jasmeetsonu990@gmail.com>
2024-03-05 17:37:05 +0530

    feat: polkadex set identity feat

--- src/components/AppLayout/index.tsx
+++ src/components/AppLayout/index.tsx
@@ -104,7 +104,7 @@ function getSiderMenuItem(label: React.ReactNode, key: React.Key, icon?: React.R
 	} as MenuItem;
 }
 
-export const onchainIdentitySupportedNetwork: Array<string> = [AllNetworks.POLKADOT, AllNetworks.KUSAMA];
+export const onchainIdentitySupportedNetwork: Array<string> = [AllNetworks.POLKADOT, AllNetworks.KUSAMA, AllNetworks.POLKADEX];
 
 const getUserDropDown = (
 	handleSetIdentityClick: any,

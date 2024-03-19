7b55c364aa033fd1030a1226eb62b31ba18a2a55
Jasmeet Singh <jasmeetsonu990@gmail.com>
2024-03-19 18:22:27 +0530

    fixes

--- src/components/Subscription/Subscriptions.tsx
+++ src/components/Subscription/Subscriptions.tsx
@@ -3,17 +3,28 @@
 // of the Apache-2.0 license. See the LICENSE file for details.
 
 import React, { useEffect, useState } from 'react';
+import { Post } from '~src/types';
+import getRelativeCreatedAt from '~src/util/getRelativeCreatedAt';
 import nextApiClientFetch from '~src/util/nextApiClientFetch';
+import { ClockCircleOutlined } from '@ant-design/icons';
+import Link from 'next/link';
+import Image from 'next/image';
+import { getSinglePostLinkFromProposalType } from '~src/global/proposalType';
+
+interface IUserData {
+	subscribedPosts?: Post[];
+}
 
 const Subscriptions = () => {
-	const [userData, setUserData] = useState({});
+	const [userData, setUserData] = useState<IUserData>({});
+
 	const fetchSubscriptions = async () => {
 		const { data, error } = await nextApiClientFetch('/api/v1/users/user-subscriptions', { proposalType: 'referendums_v2' });
 
 		if (data) {
 			setUserData(data);
 		}
-		console.log(error);
+		if (error) console.log(error);
 	};
 
 	useEffect(() => {
@@ -23,7 +34,48 @@ const Subscriptions = () => {
 
 	return (
 		<div className={''}>
-			<div>Hello world</div>
+			<p className='mb-4 w-full rounded-md bg-white p-4 text-sm font-medium text-sidebarBlue shadow-md dark:bg-section-dark-overlay dark:text-white md:p-8 md:text-base'>
+				This is a place to keep track of Subscribed posts.
+			</p>
+			<div className='mt-2 flex flex-col pb-6'>
+				<p className='text-xl font-medium'>Posts</p>
+				{userData.subscribedPosts &&
+					userData.subscribedPosts.map((post) => {
+						const createdAt = post?.created_at?._seconds;
+						const createdAtDate = createdAt ? new Date(createdAt * 1000) : null;
+						return (
+							<div
+								className='my-3 flex'
+								key={post.id}
+							>
+								<div className='w-[10%] font-semibold'>#{post.id}</div>
+								<div className='flex w-[90%] space-x-1'>
+									<span>{post.title}</span>
+									<span>
+										<Link
+											href={`/referenda/${post?.id}`}
+											target='_blank'
+											className='flex items-center gap-1 text-sm font-medium'
+										>
+											<Image
+												src='/assets/icons/redirect.svg'
+												alt=''
+												height={16}
+												width={16}
+											/>
+										</Link>
+									</span>
+								</div>
+								{post?.created_at && (
+									<span className='flex flex-shrink-0 items-center gap-1 text-xs text-lightBlue dark:text-blue-dark-medium'>
+										<ClockCircleOutlined />
+										{getRelativeCreatedAt(createdAtDate!)}
+									</span>
+								)}
+							</div>
+						);
+					})}
+			</div>
 		</div>
 	);
 };

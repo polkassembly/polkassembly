43f2cfa03a08fc68c6f8258d74487bbff0afc20e
Adetoye Adewoye <adetoyeadewoye@gmail.com>
2024-03-20 14:32:26 +0100

    show empty state for time split curve

--- src/components/Post/Tabs/PostStats/TimeSplit.tsx
+++ src/components/Post/Tabs/PostStats/TimeSplit.tsx
@@ -42,94 +42,92 @@ const TimeSplit: FC<ITimeSplitProps> = ({ className, axisLabel, votesByTimeSplit
 		}
 	];
 
-	if (elapsedPeriod < 1) {
-		return (
-			<div className='flex flex-col items-center justify-center gap-5 p-10'>
-				<NoVotesIcon />
-				<p className='text-sm'>No votes have been casted yet</p>
-			</div>
-		);
-	}
-
 	return (
 		<Card className='mx-auto h-fit max-h-[500px] w-full flex-1 rounded-xxl border-[#D2D8E0] bg-white p-0 text-blue-light-high dark:border-[#3B444F] dark:bg-section-dark-overlay dark:text-white'>
 			<h2 className='text-xl font-semibold'>Time Split</h2>
-			<div className={`${className} relative -mt-7 flex h-[200px] items-center justify-center gap-x-2`}>
-				<ResponsiveLine
-					data={chartData}
-					margin={{ bottom: 20, left: 50, right: 10, top: 40 }}
-					xScale={{ type: 'point' }}
-					yScale={{
-						max: 'auto',
-						min: 'auto',
-						reverse: false,
-						stacked: true,
-						type: 'linear'
-					}}
-					yFormat=' >-.2f'
-					enablePoints={false}
-					enableGridX={false}
-					colors={['#4064FF']}
-					axisTop={null}
-					axisRight={null}
-					axisLeft={{
-						format: (value) => formatUSDWithUnits(value, 1)
-					}}
-					axisBottom={{
-						tickValues: elapsedPeriod >= 14 ? Array.from({ length: elapsedPeriod + 1 }, (_, i) => (i % 7 === 0 ? i : null)).filter((value) => value !== null) : undefined
-					}}
-					tooltip={({ point }) => {
-						return (
-							<div className={`flex gap-2 rounded-md bg-white capitalize dark:bg-[#1E2126] ${theme === 'dark' ? 'text-white' : 'text-[#576D8B]'} p-2 text-[11px] shadow-md`}>
-								<span className='text-xs font-semibold'>Day: {point.data.xFormatted}</span>
-								<span className='text-xs font-semibold'>
-									{axisLabel ? `${axisLabel}: ` : 'votes: '}
-									{formatUSDWithUnits(point.data.yFormatted.toString(), 1)} {isUsedInAccounts ? 'voters' : chainProperties[network]?.tokenSymbol}
-								</span>
-							</div>
-						);
-					}}
-					pointSize={5}
-					pointColor={{ theme: 'background' }}
-					pointBorderWidth={2}
-					pointBorderColor={{ from: 'serieColor' }}
-					pointLabelYOffset={-12}
-					useMesh={true}
-					theme={{
-						axis: {
-							domain: {
+			{elapsedPeriod < 1 ? (
+				<div className='flex flex-col items-center justify-center gap-5'>
+					<NoVotesIcon />
+					<p className='text-sm'>Not enough data available, pls check back after 1 day.</p>
+				</div>
+			) : (
+				<div className={`${className} relative -mt-7 flex h-[200px] items-center justify-center gap-x-2`}>
+					<ResponsiveLine
+						data={chartData}
+						margin={{ bottom: 20, left: 50, right: 10, top: 40 }}
+						xScale={{ type: 'point' }}
+						yScale={{
+							max: 'auto',
+							min: 'auto',
+							reverse: false,
+							stacked: true,
+							type: 'linear'
+						}}
+						yFormat=' >-.2f'
+						enablePoints={false}
+						enableGridX={false}
+						colors={['#4064FF']}
+						axisTop={null}
+						axisRight={null}
+						axisLeft={{
+							format: (value) => formatUSDWithUnits(value, 1)
+						}}
+						axisBottom={{
+							tickValues: elapsedPeriod >= 14 ? Array.from({ length: elapsedPeriod + 1 }, (_, i) => (i % 7 === 0 ? i : null)).filter((value) => value !== null) : undefined
+						}}
+						tooltip={({ point }) => {
+							return (
+								<div className={`flex gap-2 rounded-md bg-white capitalize dark:bg-[#1E2126] ${theme === 'dark' ? 'text-white' : 'text-[#576D8B]'} p-2 text-[11px] shadow-md`}>
+									<span className='text-xs font-semibold'>Day: {point.data.xFormatted}</span>
+									<span className='text-xs font-semibold'>
+										{axisLabel ? `${axisLabel}: ` : 'votes: '}
+										{formatUSDWithUnits(point.data.yFormatted.toString(), 1)} {isUsedInAccounts ? 'voters' : chainProperties[network]?.tokenSymbol}
+									</span>
+								</div>
+							);
+						}}
+						pointSize={5}
+						pointColor={{ theme: 'background' }}
+						pointBorderWidth={2}
+						pointBorderColor={{ from: 'serieColor' }}
+						pointLabelYOffset={-12}
+						useMesh={true}
+						theme={{
+							axis: {
+								domain: {
+									line: {
+										stroke: theme === 'dark' ? '#3B444F' : '#D2D8E0',
+										strokeWidth: 1
+									}
+								},
+								ticks: {
+									text: {
+										fill: theme === 'dark' ? '#fff' : '#576D8B',
+										fontSize: 11,
+										outlineColor: 'transparent',
+										outlineWidth: 0
+									}
+								}
+							},
+							grid: {
 								line: {
 									stroke: theme === 'dark' ? '#3B444F' : '#D2D8E0',
+									strokeDasharray: '2 2',
 									strokeWidth: 1
 								}
 							},
-							ticks: {
-								text: {
-									fill: theme === 'dark' ? '#fff' : '#576D8B',
+							tooltip: {
+								container: {
+									background: theme === 'dark' ? '#1E2126' : '#fff',
+									color: theme === 'dark' ? '#fff' : '#576D8B',
 									fontSize: 11,
-									outlineColor: 'transparent',
-									outlineWidth: 0
+									textTransform: 'capitalize'
 								}
 							}
-						},
-						grid: {
-							line: {
-								stroke: theme === 'dark' ? '#3B444F' : '#D2D8E0',
-								strokeDasharray: '2 2',
-								strokeWidth: 1
-							}
-						},
-						tooltip: {
-							container: {
-								background: theme === 'dark' ? '#1E2126' : '#fff',
-								color: theme === 'dark' ? '#fff' : '#576D8B',
-								fontSize: 11,
-								textTransform: 'capitalize'
-							}
-						}
-					}}
-				/>
-			</div>
+						}}
+					/>
+				</div>
+			)}
 		</Card>
 	);
 };

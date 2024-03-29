5e01e8b3843e9a7f40776a074bcf0bb799afdd96
Jasmeet Singh <jasmeetsonu990@gmail.com>
2024-03-30 03:21:48 +0530

    fix: Create referendum form ui issue

--- src/components/Forms/CreateReferendaForm.tsx
+++ src/components/Forms/CreateReferendaForm.tsx
@@ -728,8 +728,8 @@ export default function CreateReferendaForm({
 								value={EEnactment.At_Block_No}
 								className='text-sm font-normal text-bodyBlue dark:text-blue-dark-high'
 							>
-								<div className='flex h-10 items-center gap-2'>
-									<span className='w-[150px]'>
+								<div className='flex h-10 items-center gap-4'>
+									<span>
 										At Block no.
 										<HelperTooltip
 											className='ml-1'

ea99ce0981fb4d79d4b123c38df28c47972c703a
alphainfinitus <kumar.mridul000@gmail.com>
2024-03-30 13:31:24 +0530

    fix: :sparkles: fetch from subsquare when subsquid fails

--- pages/api/v1/posts/on-chain-post.ts
+++ pages/api/v1/posts/on-chain-post.ts
@@ -737,6 +737,12 @@ export async function getOnChainPost(params: IGetOnChainPostParams): Promise<IAp
 				query: postQuery,
 				variables: postVariables
 			});
+
+			if (!subsquidRes?.data?.proposals?.length) {
+				console.log('Failed to fetch from subsquid, fetching from subsquare instead');
+				// this will make the control flow to the catch block to fetch from subsquare
+				throw apiErrorWithStatusCode(`The Post with index "${postId}" is not found.`, 404);
+			}
 		} catch (error) {
 			const data = await fetchSubsquare(network, strPostId);
 			if (data) {

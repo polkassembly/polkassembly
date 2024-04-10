ae309229e9c13a45241543e6aafc9982b4e20ae0
KanishkaRajputd <kanishka6393rajput@gmail.com>
2024-04-10 12:55:31 +0530

    hot fixes

--- src/util/getIdentityRegistrarIndex.ts
+++ src/util/getIdentityRegistrarIndex.ts
@@ -1,16 +1,17 @@
 // Copyright 2019-2025 @polkassembly/polkassembly authors & contributors
 // This software may be modified and distributed under the terms
 // of the Apache-2.0 license. See the LICENSE file for details.
+import { network as AllNetworks } from '~src/global/networkConstants';
 interface Args {
 	network: string;
 }
 const getIdentityRegistrarIndex = ({ network }: Args) => {
 	switch (network) {
-		case 'polkadot':
+		case AllNetworks.POLKADOT:
 			return 3;
-		case 'kusama':
+		case AllNetworks.KUSAMA:
 			return 5;
-		case 'polkadex':
+		case AllNetworks.POLKADEX:
 			return 4;
 		default:
 			return null;

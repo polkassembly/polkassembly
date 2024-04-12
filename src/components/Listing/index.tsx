f902a1a3f88e42adb1394dea4958394d828aacd5
KanishkaRajputd <kanishka6393rajput@gmail.com>
2024-04-12 14:04:38 +0530

    hot fixes

--- src/components/Listing/index.tsx
+++ src/components/Listing/index.tsx
@@ -60,6 +60,7 @@ const Listing: FC<IListingProps> = (props) => {
 	return (
 		<div className={`${className} proposals__list`}>
 			{posts.map((post, index) => {
+				console.log(post);
 				const {
 					cid,
 					curator,

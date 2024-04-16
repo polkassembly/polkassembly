507c5c9326c221070f98aa43414fc8003138ddff
Jasmeet Singh <jasmeetsonu990@gmail.com>
2024-04-16 14:01:03 +0530

    minor fixes

--- src/components/Analytics/utils/CustomTooltip.tsx
+++ src/components/Analytics/utils/CustomTooltip.tsx
@@ -2,7 +2,6 @@
 // This software may be modified and distributed under the terms
 // of the Apache-2.0 license. See the LICENSE file for details.
 
-// of the Apache-2.0 license. See the LICENSE file for details.
 import { PointTooltipProps } from '@nivo/line';
 export const CustomTooltip = ({ point }: PointTooltipProps) => {
 	return (

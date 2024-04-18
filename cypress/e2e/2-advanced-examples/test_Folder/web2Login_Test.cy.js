0f9b68487cf43d1eb13eda30e7d04e400873643c
paras raghav <parasraghav@parass-MacBook-Air.local>
2024-04-18 10:32:42 +0530

    first test Class

---
+++ cypress/e2e/2-advanced-examples/test_Folder/web2Login_Test.cy.js
@@ -0,0 +1,9 @@
+describe('This Test Case Are Used for Web 2 Login', function() {
+  
+    it('Verify For Visits the polkAssembly Web page ', function() {
+
+      cy.visit('https://polkadot.polkassembly.io/opengov');
+
+    });
+  });
+  

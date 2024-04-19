4a7f21f96bc6ffb24fbe0412bce81c74e4dec562
paras raghav <parasraghav@parass-MacBook-Air.local>
2024-04-19 15:57:52 +0530

    fixes

--- cypress/e2e/2-advanced-examples/test_Folder/web2Login_Test.cy.js
+++ cypress/e2e/2-advanced-examples/test_Folder/web2Login_Test.cy.js
@@ -1,9 +1,25 @@
-describe('This Test Case Are Used for Web 2 Login', function() {
-  
-    it('Verify For Visits the polkAssembly Web page ', function() {
 
-      cy.visit('https://polkadot.polkassembly.io/opengov');
+describe('Login functionality', () => {
+  it('Logs in to the website', () => {
+    // Visit the website
+    cy.visit('https://polkadot.polkassembly.io')
+    cy.wait(2000);
 
-    });
-  });
-  
+    // Click on the login button
+    cy.get('button[text="Login"]').click()
+    cy.wait(1000);
+
+    // Wait for the login popup to appear
+    cy.get('span.font-semibold').contains('Login with Username/Email').click()
+    cy.wait(1000);
+
+    // Fill in the username/email
+    cy.get('input#username').type('paras@polkassembly.io')
+
+    // Fill in the password
+    cy.get('input#password').type('Gjmptw%4567890')
+
+    // Submit the login form
+    cy.get('button').contains('Login').click()
+  })
+})

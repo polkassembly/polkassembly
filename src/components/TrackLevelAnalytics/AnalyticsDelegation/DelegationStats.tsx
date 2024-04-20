bf1aa2ea6d1b25677776ce5e721ea234344dfd4c
Jasmeet Singh <jasmeetsonu990@gmail.com>
2024-04-20 14:32:06 +0530

    fixes

--- src/components/TrackLevelAnalytics/AnalyticsDelegation/DelegationStats.tsx
+++ src/components/TrackLevelAnalytics/AnalyticsDelegation/DelegationStats.tsx
@@ -4,24 +4,29 @@
 import { Divider } from 'antd';
 import React from 'react';
 import { parseBalance } from '~src/components/Post/GovernanceSideBar/Modal/VoteData/utils/parseBalaceToReadable';
+import { chainProperties } from '~src/global/networkConstants';
 import { useNetworkSelector, useTrackLevelAnalytics } from '~src/redux/selectors';
 
 const DelegationStats = () => {
 	const { network } = useNetworkSelector();
+	const tokenSymbol = `${chainProperties[network]?.tokenSymbol}`;
 	const { totalCapital, totalDelegates, totalDelegators, totalVotesBalance } = useTrackLevelAnalytics();
 	const stats = [
 		{ title: 'Delegatee', value: totalDelegates },
 		{ title: 'Delegator', value: totalDelegators },
-		{ title: 'Total Capital', value: parseBalance(totalCapital, 1, true, network) },
-		{ title: 'Total Votes', value: parseBalance(totalVotesBalance, 1, true, network) }
+		{ title: 'Total Capital', value: parseBalance(totalCapital, 1, false, network) },
+		{ title: 'Total Votes', value: parseBalance(totalVotesBalance, 1, false, network) }
 	];
 	return (
 		<section className='grid grid-cols-2 gap-2 sm:flex sm:justify-between sm:px-4'>
 			{stats.map((stat, index) => (
 				<React.Fragment key={index}>
-					<div className='flex flex-col items-center rounded-xl bg-[#D2D8E04F] pb-1 pt-2 dark:bg-section-dark-container sm:bg-transparent dark:sm:bg-transparent'>
+					<div className='flex flex-col items-center rounded-xl bg-[#D2D8E04F] pb-1 pt-2 dark:bg-[#1F1F20] sm:bg-transparent dark:sm:bg-transparent'>
 						<div className='text-xs font-normal text-blue-light-medium dark:text-blue-dark-medium'>{stat.title}</div>
-						<div className=' text-center text-[28px] font-semibold text-blue-light-high dark:text-blue-dark-high'>{stat.value}</div>
+						<div className=' text-center text-[28px] font-semibold text-blue-light-high dark:text-blue-dark-high'>
+							{stat.value}
+							<span className='ml-[2px] text-lg font-semibold text-blue-light-medium dark:text-blue-dark-medium'>{tokenSymbol}</span>
+						</div>
 					</div>
 					{index < stats.length - 1 && (
 						<Divider

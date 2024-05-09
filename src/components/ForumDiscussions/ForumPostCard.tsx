30d214212ad716c2028bc9a2415526acb6e68f79
Jasmeet Singh <jasmeetsonu990@gmail.com>
2024-05-10 04:08:23 +0530

    Adding Username and UserPicture

--- src/components/ForumDiscussions/ForumPostCard.tsx
+++ src/components/ForumDiscussions/ForumPostCard.tsx
@@ -1,7 +1,7 @@
 // Copyright 2019-2025 @polkassembly/polkassembly authors & contributors
 // This software may be modified and distributed under the terms
 // of the Apache-2.0 license. See the LICENSE file for details.
-import React, { FC } from 'react';
+import React, { FC, useEffect, useState } from 'react';
 import { Topic } from './types';
 import { Divider } from 'antd';
 import { ClockCircleOutlined, LikeOutlined, EyeFilled } from '@ant-design/icons';
@@ -9,6 +9,9 @@ import { CommentsIcon } from '~src/ui-components/CustomIcons';
 import getRelativeCreatedAt from '~src/util/getRelativeCreatedAt';
 import Link from 'next/link';
 import getCategoryName from './utils/getCategoryName';
+import nextApiClientFetch from '~src/util/nextApiClientFetch';
+import ImageIcon from '~src/ui-components/ImageIcon';
+import formatAvatarUrl from './utils/FormatAvatarUrl';
 
 interface ForumPostCardProps {
 	topics: Topic[];
@@ -16,11 +19,52 @@ interface ForumPostCardProps {
 
 const ForumPostCard: FC<ForumPostCardProps> = ({ topics }) => {
 	// const [tagsModal, setTagsModal] = useState<boolean>(false);
+	const [usernames, setUsernames] = useState<{ [key: number]: string }>({});
+	const [userImg, setUserImg] = useState<{ [key: number]: string }>({});
+
+	const fetchUsernames = async () => {
+		const userMap: { [key: number]: string } = {};
+		const userImgMap: { [key: number]: string } = {};
+		for (const topic of topics) {
+			try {
+				const { data } = await nextApiClientFetch<any>(`/api/v1/discourse/getTopicById?id=${topic.id}`);
+				const { posts } = data?.data?.post_stream || {};
+
+				if (posts?.length > 0) {
+					const username = posts[0].username || posts[0].name || 'Unknown';
+					userMap[topic.id] = username;
+
+					const avatarTemplate = posts[0].avatar_template || '/path/to/default/avatar.png';
+					userImgMap[topic.id] = avatarTemplate;
+				} else {
+					console.log(`No posts found for topic ID ${topic.id}`);
+					userMap[topic.id] = 'Unknown';
+					userImgMap[topic.id] = '/path/to/default/avatar.png';
+				}
+			} catch (error) {
+				console.error(`Failed to fetch data for topic ID ${topic.id}:`, error);
+				userMap[topic.id] = 'Failed to load';
+				userImgMap[topic.id] = '/path/to/default/avatar.png';
+			}
+		}
+		setUsernames(userMap);
+		setUserImg(userImgMap);
+	};
+
+	useEffect(() => {
+		if (topics.length > 0) {
+			fetchUsernames();
+		}
+		// eslint-disable-next-line react-hooks/exhaustive-deps
+	}, [topics]);
+
 	return (
 		<div className='mt-6'>
 			{topics.map((topic, index) => {
 				const { title, id, created_at, reply_count, like_count, tags, views, slug, category_id } = topic;
 				const date = new Date(created_at);
+				const username = usernames[id] || 'Loading...';
+				const user_avatar = userImg[id];
 				return (
 					<div
 						key={topic.id}
@@ -29,9 +73,19 @@ const ForumPostCard: FC<ForumPostCardProps> = ({ topics }) => {
 						<Link href={`/forum/t/${slug}/${id}`}>
 							<div className='min-h-[100px] cursor-pointer border-2 border-[#DCDFE350] p-3 transition-all duration-200 hover:border-pink_primary hover:shadow-xl dark:border-[1px] dark:border-separatorDark xs:hidden sm:flex md:p-4'>
 								<span className='flex-none text-center font-medium text-bodyBlue dark:text-blue-dark-high sm:mt-2 sm:w-[120px]'>#{id}</span>
-								<div className='flex-1 flex-col sm:mt-1 sm:flex sm:justify-between'>
-									<span className='text-sm font-medium text-[#334D6E] dark:text-blue-dark-high'>{title}</span>
 
+								<div className='flex-1 flex-col gap-1 sm:mt-1 sm:flex sm:justify-between'>
+									<div className='mb-1 flex items-center gap-2'>
+										{user_avatar && (
+											<ImageIcon
+												src={formatAvatarUrl(user_avatar, '22')}
+												alt='user image'
+												imgClassName='rounded-full w-[18px]'
+											/>
+										)}
+										<span className='text-xs font-medium text-blue-light-high dark:text-blue-dark-high'>{username}</span>
+									</div>
+									<span className='text-sm font-medium text-[#334D6E] dark:text-blue-dark-high'>{title}</span>
 									<div className='flex-col items-start text-xs font-medium text-bodyBlue dark:text-blue-dark-high xs:hidden sm:flex lg:flex-row lg:items-center'>
 										<div className='flex items-center gap-x-2'>
 											<div className='items-center justify-center gap-x-1.5 xs:hidden sm:flex'>
@@ -80,7 +134,7 @@ const ForumPostCard: FC<ForumPostCardProps> = ({ topics }) => {
 												<>
 													<Divider
 														type='vertical'
-														className='border-l-1 border-lightBlue dark:border-icon-dark-inactive max-sm:hidden'
+														className='border-l-1 border-lightBlue dark:border-icon-dark-inactive max-sm:hidden sm:mt-1'
 													/>
 													{tags?.slice(0, 2).map((tag, index) => (
 														<div
@@ -145,10 +199,20 @@ const ForumPostCard: FC<ForumPostCardProps> = ({ topics }) => {
 										#{id} {title}
 									</div>
 									<div className='flex-col items-start text-xs font-medium text-bodyBlue dark:text-blue-dark-high xs:ml-2 xs:mt-1 xs:flex xs:gap-0 sm:ml-0 sm:hidden sm:gap-2.5 lg:flex-row lg:items-center'>
-										<div className='xs:flex xs:justify-start sm:hidden'>
+										<div className='items-center xs:flex xs:justify-start sm:hidden'>
+											<div className='mb-1 flex items-center gap-2'>
+												{user_avatar && (
+													<ImageIcon
+														src={formatAvatarUrl(user_avatar, '22')}
+														alt='user image'
+														imgClassName='rounded-full w-[18px]'
+													/>
+												)}
+												<span className='text-xs font-medium text-blue-light-high dark:text-blue-dark-high'>{username}</span>
+											</div>
 											<Divider
 												type='vertical'
-												className='border-l-1 border-lightBlue dark:border-icon-dark-inactive max-lg:hidden xs:mt-0.5 xs:inline-block'
+												className='border-l-1 border-lightBlue dark:border-icon-dark-inactive max-lg:hidden xs:inline-block sm:mt-0.5'
 											/>
 											{created_at && (
 												<>
@@ -163,7 +227,7 @@ const ForumPostCard: FC<ForumPostCardProps> = ({ topics }) => {
 											{tags && tags.length > 0 && (
 												<Divider
 													type='vertical'
-													className='border-l-1 my-4 border-lightBlue dark:border-icon-dark-inactive max-lg:hidden'
+													className='border-l-1 border-lightBlue dark:border-icon-dark-inactive max-lg:hidden'
 												/>
 											)}
 											{tags && tags.length > 0 && (

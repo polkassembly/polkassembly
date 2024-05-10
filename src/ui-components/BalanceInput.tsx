c1b92bdd00b19da7a6bef627dcd3cc28302bdeab
Jasmeet Singh <jasmeetsonu990@gmail.com>
2024-05-10 14:28:00 +0530

    fix: resolving conflicts

--- src/ui-components/BalanceInput.tsx
+++ src/ui-components/BalanceInput.tsx
@@ -129,7 +129,7 @@ const BalanceInput = ({
 												(Number(value) > 0 && value?.split('.')?.[1]?.length && chainProperties[network]?.tokenDecimals < (value?.split('.')?.[1].length || 0)) ||
 												(value.length && Number(value) <= 0))
 										) {
-											setIsBalanceSet && setIsBalanceSet(false);
+											setIsBalanceSet?.(false);
 											callback(rule?.message?.toString());
 										} else {
 											callback();

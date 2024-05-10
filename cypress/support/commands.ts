c5907fabb5983dff2fd9985d93bfb3ed0c42f854
paras raghav <parasraghav@parass-MacBook-Air.local>
2024-05-10 12:41:57 +0530

    web3 Automation

--- cypress/support/commands.ts
+++ cypress/support/commands.ts
@@ -1,4 +1,6 @@
-/// <reference types="cypress" />
+export {};
+
+// <reference types="cypress" />
 // ***********************************************
 // This example commands.ts shows you how to
 // create various custom commands and overwrite

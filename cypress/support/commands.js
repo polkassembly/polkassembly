6e0ee44652b82ccfccc89cd3ee1cb9b58f966898
paras raghav <parasraghav@parass-MacBook-Air.local>
2024-05-27 10:28:06 +0530

    fixes

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -1,8 +1,9 @@
-const loadEnvVars = require('./loadEnvVars');
+// cypress/support/commands.js
 
 // Custom command to handle login
 Cypress.Commands.add('login', () => {
-  const { username, password } = loadEnvVars();
+  const username = Cypress.env('username');
+  const password = Cypress.env('password');
 
   // Handle uncaught exceptions
   Cypress.on('uncaught:exception', (err, runnable) => {
@@ -11,15 +12,15 @@ Cypress.Commands.add('login', () => {
   });
 
   // Visit the website
-  cy.visit("https://polkadot.polkassembly.io/");
+  cy.visit("https://polkadot.polkassembly.io/")
   cy.wait(2000);
 
   // Click on the login button
-  cy.get('button[text="Login"]').click();
+  cy.get('button[text="Login"]').click()
   cy.wait(1000);
 
   // Wait for the login popup to appear
-  cy.get('span.font-semibold').contains('Login with Username/Email').click();
+  cy.get('span.font-semibold').contains('Login with Username/Email').click()
   cy.wait(1000);
 
   // Fill in the username/email
@@ -33,6 +34,7 @@ Cypress.Commands.add('login', () => {
   cy.wait(1000);
 });
 
+
 // Custom command to handle logout
 Cypress.Commands.add('logout', () => {
   // Handle uncaught exceptions
@@ -41,10 +43,10 @@ Cypress.Commands.add('logout', () => {
     return false;
   });
 
-  // Wait for the logout button to appear
-  cy.get('.navbar-user-dropdown').should('be.visible').click();
-  cy.wait(1000);
+  // Wait for the user dropdown to appear and click it
+  cy.get('.navbar-user-dropdown').click();
 
-  // Click on the "Logout" option from the dropdown menu
+
+  // Wait for the dropdown menu to become visible
   cy.get('.ant-dropdown-menu-title-content > .mt-1').click();
 });

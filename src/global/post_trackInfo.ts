dc0511b80509f35598cb3aac8e0292bcfe29e614
Jasmeet Singh <jasmeetsonu990@gmail.com>
2024-05-29 20:39:44 +0530

    fix: adjusting track

--- src/global/post_trackInfo.ts
+++ src/global/post_trackInfo.ts
@@ -1804,6 +1804,32 @@ export const networkTrackInfo: INetworkTrackInfo = {
 				}
 			}
 		},
+		[PostOrigin.FAST_GENERAL_ADMIN]: {
+			trackId: 5,
+			description: 'Fast origin for managing the registrar. ',
+			group: 'Governance',
+			name: 'fast_general_admin',
+			maxDeciding: 10,
+			decisionDeposit: 10000000000000000000000,
+			preparePeriod: 300,
+			decisionPeriod: 100800,
+			confirmPeriod: 900,
+			minEnactmentPeriod: 50,
+			minApproval: {
+				reciprocal: {
+					factor: 999999999,
+					xOffset: 999999999,
+					yOffset: 0
+				}
+			},
+			minSupport: {
+				Reciprocal: {
+					factor: 5799702,
+					xOffset: 11467891,
+					yOffset: -5733946
+				}
+			}
+		},
 		[PostOrigin.REFERENDUM_CANCELLER]: {
 			trackId: 3,
 			description: 'Origin able to cancel referenda.',
@@ -1853,32 +1879,6 @@ export const networkTrackInfo: INetworkTrackInfo = {
 					factor: 869501,
 					xOffset: 8620680,
 					yOffset: -862069
-				}
-			}
-		},
-		[PostOrigin.FAST_GENERAL_ADMIN]: {
-			trackId: 5,
-			description: 'Fast origin for managing the registrar. ',
-			group: 'Governance',
-			name: 'fast_general_admin',
-			maxDeciding: 10,
-			decisionDeposit: 10000000000000000000000,
-			preparePeriod: 300,
-			decisionPeriod: 100800,
-			confirmPeriod: 900,
-			minEnactmentPeriod: 50,
-			minApproval: {
-				reciprocal: {
-					factor: 999999999,
-					xOffset: 999999999,
-					yOffset: 0
-				}
-			},
-			minSupport: {
-				Reciprocal: {
-					factor: 5799702,
-					xOffset: 11467891,
-					yOffset: -5733946
 				}
 			}
 		}

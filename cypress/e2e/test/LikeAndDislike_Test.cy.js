81022b550561683d01ae3d7138253c11c75003f8
paras raghav <parasraghav@parass-MacBook-Air.local>
2024-06-14 15:43:27 +0530

    Like and Comment feature test

--- cypress/e2e/test/LikeAndDislike_Test.cy.js
+++ cypress/e2e/test/LikeAndDislike_Test.cy.js
@@ -8,11 +8,11 @@ describe('Like and Dislike Feature Test', () => {
       cy.wait(3000);
   
       // Click on the like button to like the post
-      cy.get(':nth-child(1) > :nth-child(1) > .ant-btn > .flex').click();
+      cy.get(':nth-child(1) > :nth-child(1) > .ant-btn > .flex').first().click();
       cy.wait(3000);
       
       // Click on the like button again to reset the like
-      cy.get(':nth-child(2) > :nth-child(1) > .ant-btn > .flex').click();
+      cy.get(':nth-child(2) > :nth-child(1) > .ant-btn > .flex').first().click();
       cy.wait(3000);
   
       // Add a comment
@@ -23,10 +23,10 @@ describe('Like and Dislike Feature Test', () => {
       cy.wait(3000);
   
       // Assuming there are modal elements to select sentiment and confirm
-      cy.get('.ant-slider-step > [style="left: 75%; transform: translateX(-50%);"]').click();
+      cy.get('.ant-slider-step > [style="left: 75%; transform: translateX(-50%);"]').first().click();
       cy.wait(1000);
   
-      cy.get('.justify-center > .ant-btn > :nth-child(1)').click();
+      cy.get('.justify-center > .ant-btn > :nth-child(1)').first().click();
       cy.wait(3000);
   
       // Logout using the custom logout command

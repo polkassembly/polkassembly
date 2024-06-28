3ad6cb3425c7a37511821e17e5aaea950627fe0a
Jasmeet Singh <jasmeetsonu990@gmail.com>
2024-06-28 18:29:26 +0530

    fix: search issue

--- src/components/Leaderboard/LeaderBoardTable.tsx
+++ src/components/Leaderboard/LeaderBoardTable.tsx
@@ -1,29 +1,26 @@
 // Copyright 2019-2025 @polkassembly/polkassembly authors & contributors
 // This software may be modified and distributed under the terms
 // of the Apache-2.0 license. See the LICENSE file for details.
-import React, { FC, useEffect, useState } from 'react';
+import React, { FC, useState } from 'react';
 import LeaderboardData from './LeaderboardData';
 import { Input } from 'antd';
 import styled from 'styled-components';
 import { useTheme } from 'next-themes';
 import { ILeaderboardTable } from './types';
 import { poppins } from 'pages/_app';
-import debounce from 'lodash/debounce';
 
 const LeaderBoardTable: FC<ILeaderboardTable> = ({ className }) => {
 	const { resolvedTheme: theme } = useTheme();
 	const [searchedUsername, setSearchedUsername] = useState<string | undefined>();
 	const [inputValue, setInputValue] = useState<string>('');
 
-	useEffect(() => {
-		const debouncedSearch = debounce((value: string) => {
+	const handleSearch = (value: string) => {
+		if (value.length >= 3) {
 			setSearchedUsername(value);
-		}, 300);
-		debouncedSearch(inputValue);
-		return () => {
-			debouncedSearch.cancel();
-		};
-	}, [inputValue]);
+		} else {
+			setSearchedUsername(undefined);
+		}
+	};
 
 	return (
 		<section className={`${className}`}>
@@ -34,9 +31,9 @@ const LeaderBoardTable: FC<ILeaderboardTable> = ({ className }) => {
 						<Input.Search
 							placeholder='Enter username to search'
 							className='m-0 rounded-[4px] p-0 px-3.5 py-2.5 text-[#7788a0] dark:bg-transparent dark:text-blue-dark-high dark:focus:border-[#91054F]'
-							onChange={(e) => {
-								setInputValue(e.target.value);
-							}}
+							onSearch={handleSearch}
+							onChange={(e) => setInputValue(e.target.value)}
+							value={inputValue}
 						/>
 					</div>
 				</div>

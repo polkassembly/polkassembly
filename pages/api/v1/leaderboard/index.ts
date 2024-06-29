bb24c38022f8a7a1b309e54c80a95328c301f60d
Jasmeet Singh <jasmeetsonu990@gmail.com>
2024-06-29 16:29:11 +0530

    fix conflicts

--- pages/api/v1/leaderboard/index.ts
+++ pages/api/v1/leaderboard/index.ts
@@ -36,10 +36,10 @@ export const getLeaderboard = async ({ page, username = '' }: { page: number; us
 				addresses: [],
 				created_at: userData?.created_at?.toDate?.() || new Date(),
 				image: userData?.profile?.image,
-				profile_score: userData.profile_score,
-				user_id: userData.id,
-				username: userData.username,
-				...userData.profile,
+				profile_score: userData?.profile_score,
+				user_id: userData?.id,
+				username: userData?.username,
+				...userData?.profile,
 				rank
 			} as LeaderboardEntry;
 		});

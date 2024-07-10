9dafb6c61b0c979964c9ff5406bc5cf2a8fffc30
Jasmeet Singh <jasmeetsonu990@gmail.com>
2024-07-10 05:58:46 +0530

    fix: footer issue and activities animation

--- src/components/Bounties/BountyActivities.tsx
+++ src/components/Bounties/BountyActivities.tsx
@@ -24,7 +24,6 @@ const BountyActivities = () => {
 		isLoading: true,
 		value: ''
 	});
-	const [startIndex, setStartIndex] = useState(0);
 
 	const getData = useCallback(async () => {
 		setLoading(true);
@@ -49,15 +48,6 @@ const BountyActivities = () => {
 		if (!network) return;
 		GetCurrentTokenPrice(network, setCurrentTokenPrice);
 	}, [network]);
-
-	useEffect(() => {
-		if (!loading && userActivities.length > 0) {
-			const interval = setInterval(() => {
-				setStartIndex((prevIndex) => (prevIndex + 1) % userActivities.length);
-			}, 3000);
-			return () => clearInterval(interval);
-		}
-	}, [loading, userActivities.length]);
 
 	const formatNumberWithSuffix = (value: number) => {
 		if (value >= 1e6) {
@@ -88,10 +78,8 @@ const BountyActivities = () => {
 		return `$${getFormattedValue(value)}`;
 	};
 
-	const activitiesToShow = userActivities.slice(startIndex, startIndex + 7);
-
 	return (
-		<div className='mt-1 flex max-h-[400px] w-full flex-col gap-[18px]'>
+		<div className=' w-full'>
 			{loading ? (
 				<>
 					<Skeleton />
@@ -103,32 +91,26 @@ const BountyActivities = () => {
 					autoplay
 					autoplaySpeed={3000}
 					dots={false}
-					infinite={false}
-					className='max-h-[400px]'
+					infinite
+					className=' flex items-center'
 					easing='linear'
+					slidesToShow={7}
 				>
-					{chunkArray(activitiesToShow, 7).map((chunk, index) => (
+					{userActivities.map((activity, index) => (
 						<div
 							key={index}
-							className='flex max-w-[500px] flex-col gap-2'
+							className='my-1 flex items-center gap-1 rounded-[14px] border bg-white px-3 py-2 dark:bg-section-light-overlay'
 						>
-							{chunk.map((activity, idx) => (
-								<div
-									key={idx}
-									className='flex items-center gap-1 rounded-[14px] border bg-white px-3  py-2 dark:bg-section-light-overlay'
-								>
-									<NameLabel
-										truncateUsername={true}
-										defaultAddress={activity.address}
-										usernameMaxLength={10}
-									/>
-									<span className='text-sm font-normal text-blue-light-medium dark:text-blue-dark-medium'>claimed</span>
-									<span className='text-[20px] font-normal text-pink_primary'>{getDisplayValue(activity?.amount)}</span>
-									<span className='text-sm font-normal text-blue-light-medium dark:text-blue-dark-medium'>bounty</span>
-									<div className='mx-2 h-[5px] w-[5px] rounded-full bg-[#485F7DB2] dark:bg-[#909090B2]'></div>
-									<span className='rounded-full text-xs text-[#485F7DB2] dark:text-blue-dark-medium'>{dayjs(activity?.created_at).format("DD[th] MMM 'YY")}</span>
-								</div>
-							))}
+							<NameLabel
+								truncateUsername={true}
+								defaultAddress={activity.address}
+								usernameMaxLength={10}
+							/>
+							<span className='text-sm font-normal text-blue-light-medium dark:text-blue-dark-medium'>claimed</span>
+							<span className='text-[20px] font-normal text-pink_primary'>{getDisplayValue(activity?.amount)}</span>
+							<span className='text-sm font-normal text-blue-light-medium dark:text-blue-dark-medium'>bounty</span>
+							<div className='mx-2 h-[5px] w-[5px] rounded-full bg-[#485F7DB2] dark:bg-[#909090B2]'></div>
+							<span className='rounded-full text-xs text-[#485F7DB2] dark:text-blue-dark-medium'>{dayjs(activity?.created_at).format("DD[th] MMM 'YY")}</span>
 						</div>
 					))}
 				</Carousel>

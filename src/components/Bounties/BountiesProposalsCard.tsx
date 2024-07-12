cff6c11c60ebbcc391dbe47dcdb60f0d7d3c6e33
Jasmeet Singh <jasmeetsonu990@gmail.com>
2024-07-12 19:56:08 +0530

    fix: bounties issue

--- src/components/Bounties/BountiesProposalsCard.tsx
+++ src/components/Bounties/BountiesProposalsCard.tsx
@@ -26,6 +26,8 @@ import { getTrackData } from '../Listing/Tracks/AboutTrackCard';
 import { getStatusBlock } from '~src/util/getStatusBlock';
 import { GetCurrentTokenPrice } from '~src/util/getCurrentTokenPrice';
 import { formatTrackName, getFormattedValue } from './utils/formatBalanceUsd';
+import { IDelegationProfileType } from '~src/auth/types';
+import { IGetProfileWithAddressResponse } from 'pages/api/v1/auth/data/profileWithAddress';
 export interface BountiesProposalsCardProps {
 	activeData: any;
 }
@@ -71,7 +73,7 @@ const BountiesProposalsCard: React.FC<BountiesProposalsCardProps> = ({ activeDat
 	const { network } = useNetworkSelector();
 	const { resolvedTheme: theme } = useTheme();
 	const unit = `${chainProperties[network]?.tokenSymbol}`;
-	const { track_no, tags, title, content, reward, user_id, post_id, tally, created_at, timeline } = activeData;
+	const { track_no, tags, title, content, reward, user_id, post_id, tally, created_at, timeline, proposer } = activeData;
 	const [decision, setDecision] = useState<IPeriod>();
 	const decidingStatusBlock = getStatusBlock(timeline || [], ['ReferendumV2', 'FellowshipReferendum'], 'Deciding');
 	const [userImageData, setUserImageData] = useState<UserProfileImage[]>([]);
@@ -81,12 +83,35 @@ const BountiesProposalsCard: React.FC<BountiesProposalsCardProps> = ({ activeDat
 		isLoading: true,
 		value: ''
 	});
+	const [profileDetails, setProfileDetails] = useState<IDelegationProfileType>({
+		bio: '',
+		image: '',
+		social_links: [],
+		user_id: 0,
+		username: ''
+	});
+
+	const getData = async () => {
+		try {
+			const { data, error } = await nextApiClientFetch<IGetProfileWithAddressResponse>(`api/v1/auth/data/profileWithAddress?address=${proposer}`, undefined, 'GET');
+			if (error || !data || !data.username || !data.user_id) {
+				return;
+			}
+			setProfileDetails({
+				bio: data?.profile?.bio || '',
+				image: data?.profile?.image || '',
+				social_links: data?.profile?.social_links || [],
+				user_id: data?.user_id,
+				username: data?.username
+			});
+		} catch (error) {
+			console.log(error);
+		}
+	};
 
 	useEffect(() => {
 		const fetchData = async () => {
 			setLoading(true);
-			await getUserProfile([user_id]);
-
 			if (network && post_id) {
 				const votesResponse = await getReferendumVotes(network, post_id);
 				if (votesResponse.data) {
@@ -116,25 +141,12 @@ const BountiesProposalsCard: React.FC<BountiesProposalsCardProps> = ({ activeDat
 		// eslint-disable-next-line react-hooks/exhaustive-deps
 	}, [track_no, created_at, network]);
 
-	const getUserProfile = async (userIds: string[]) => {
-		if (userIds?.length) {
-			const { data, error } = await nextApiClientFetch<UserProfileImage[]>('api/v1/auth/data/getUsersProfileImages', { userIds });
-			if (data) {
-				setUserImageData(data);
-			} else {
-				console.log('There is error in fetching data', error);
-			}
-		} else {
-			setUserImageData([]);
-		}
-	};
-
 	useEffect(() => {
 		if (!network) return;
 		GetCurrentTokenPrice(network, setCurrentTokenPrice);
 		const fetchData = async () => {
 			setLoading(true);
-			await getUserProfile([user_id]);
+			await getData();
 			setLoading(false);
 		};
 		fetchData();
@@ -146,127 +158,133 @@ const BountiesProposalsCard: React.FC<BountiesProposalsCardProps> = ({ activeDat
 			{loading ? (
 				<Skeleton />
 			) : (
-				<div className='w-full md:w-[383px]'>
-					<div className='flex'>
-						<CardHeader
-							theme={theme as any}
-							className='relative flex h-[56px] w-full items-center justify-start gap-4 rounded-t-3xl border-b-0 border-l border-r border-t border-solid border-section-light-container bg-white px-3 pt-5 dark:border-section-dark-container dark:bg-section-light-overlay'
-						>
-							<div className='flex items-baseline gap-x-2'>
-								<h2 className='mt-4 font-pixeboy text-[35px] font-normal text-pink_primary'>
-									{currentTokenPrice.isLoading || isNaN(Number(currentTokenPrice.value)) ? '' : '$'}
-									{getFormattedValue(String(reward), network, currentTokenPrice)}
-								</h2>
-								<span className=' font-pixeboy text-[24px] font-normal text-pink_primary'>
-									{currentTokenPrice.isLoading || isNaN(Number(currentTokenPrice.value)) ? `${unit}` : ''}
-								</span>
-							</div>
-							<Divider
-								type='vertical'
-								className='h-[30px] bg-section-light-container dark:bg-section-dark-container'
-							/>
-							{decision && decidingStatusBlock && <h2 className='mt-3 font-pixeboy text-[22px] font-normal dark:text-white'>{decision.periodPercent || 0}%</h2>}
-							<div className='mr-1'>
-								{votesData && (
-									<VotesProgressInListing
-										index={post_id}
-										proposalType={'ReferendumV2'}
-										votesData={votesData}
-										onchainId={post_id}
-										tally={tally}
-									/>
-								)}
-							</div>
-						</CardHeader>
-						<Link
-							href={`/referenda/${post_id}`}
-							target='_blank'
-						>
-							<div
-								// theme={theme as any}
-								className='relative ml-2 flex w-full items-center'
-							>
-								<Image
-									src={theme === 'light' ? '/assets/bounty-icons/redirect-icon.svg' : '/assets/bounty-icons/redirect-icon-black.svg'}
-									width={45}
-									height={45}
-									alt='redirect link'
-									className='-mr-[2px] h-[36px] w-[36px] cursor-pointer rounded-full bg-black dark:bg-white md:h-auto md:w-auto'
-								/>
-								<BtnLine
-									theme={theme as any}
-									className='relative h-2 w-[10px] bg-black dark:bg-white'
-								></BtnLine>
-								<button
-									className={`${spaceGrotesk.className} ${spaceGrotesk.variable} -ml-[2px] h-[36px] w-[75px] cursor-pointer rounded-3xl border-none bg-black text-base font-bold text-white dark:bg-white dark:text-black md:h-[44px] md:w-[100px] md:text-lg`}
-								>
-									Vote
-								</button>
-							</div>
-						</Link>
-					</div>
-					<div
-						className={`rounded-b-3xl rounded-tr-2xl border-b border-l border-r border-t-0 
-                     border-solid border-section-light-container bg-white px-3 py-1 dark:border-section-dark-container dark:bg-section-light-overlay`}
+				<div className='w-full cursor-pointer md:w-[383px]'>
+					<Link
+						key={post_id}
+						href={`/referenda/${post_id}`}
+						target='_blank'
 					>
-						<Link
-							href={`/referenda/${post_id}`}
-							target='_blank'
-						>
-							<ImageIcon
-								src='/assets/bounty-icons/bounty-image.svg'
-								alt='bounty icon'
-								imgClassName='mt-5 mb-3 w-full md:w-auto'
-								imgWrapperClassName=''
-							/>
-						</Link>
-						<div className={`${spaceGrotesk.className} ${spaceGrotesk.variable}`}>
-							<span className='mr-1 text-base font-medium text-blue-light-medium dark:text-blue-dark-medium'>#{post_id}</span>
-							<span className='text-lg font-bold text-blue-light-high dark:text-blue-dark-high'>{title}</span>
-						</div>
-						<p className={`${spaceGrotesk.className} ${spaceGrotesk.variable} text-sm font-normal text-blue-light-medium dark:text-blue-dark-medium`}>{content.slice(0, 140)}</p>
-						{tags && tags.length > 0 && (
-							<div className='flex gap-x-1'>
-								{tags?.slice(0, 3).map((tag: string, index: number) => (
-									<div
-										key={index}
-										style={{ fontSize: '10px' }}
-										className=' w-min rounded-xl border-[1px] border-solid border-section-light-container px-[14px] py-[4px] font-medium text-lightBlue dark:border-[#3B444F] dark:border-separatorDark dark:text-blue-dark-high'
-									>
-										{tag}
-									</div>
-								))}
-								{tags.length > 3 && (
-									<span
-										className='text-bodyBlue dark:text-blue-dark-high'
-										style={{ background: '#D2D8E050', borderRadius: '20px', fontSize: '10px', padding: '4px 8px' }}
-									>
-										+{tags.length - 3}
-									</span>
-								)}
-							</div>
-						)}
-
-						<Link
-							href={`/user/${userImageData[0]?.username}`}
-							target='_blank '
-						>
-							<span className={`${poppins.variable} ${poppins.className} mr-1 text-xs font-normal text-blue-light-medium dark:text-blue-dark-medium`}>Proposer:</span>
-							<ImageComponent
-								alt='user img'
-								src={userImageData[0]?.image}
-								className='-mt-[1px] mr-[1px] h-[16px] w-[16px]'
-							/>
-							<span className={`${poppins.variable} ${poppins.className} text-xs font-medium text-blue-light-high dark:text-blue-dark-high`}>{userImageData[0]?.username}</span>
-						</Link>
-						<div className='mb-3 mt-2'>
-							<TrackTag
+						<div className='flex'>
+							<CardHeader
 								theme={theme as any}
-								className='sm:mt-0'
-								track={formatTrackName(getTrackNameFromId(network, track_no))}
-							/>
+								className='relative flex h-[56px] w-full items-center justify-start gap-4 rounded-t-3xl border-b-0 border-l border-r border-t border-solid border-section-light-container bg-white px-3 pt-5 dark:border-section-dark-container dark:bg-section-light-overlay'
+							>
+								<div className='flex items-baseline gap-x-2'>
+									<h2 className='mt-4 font-pixeboy text-[35px] font-normal text-pink_primary'>
+										{currentTokenPrice.isLoading || isNaN(Number(currentTokenPrice.value)) ? '' : '$'}
+										{getFormattedValue(String(reward), network, currentTokenPrice)}
+									</h2>
+									<span className=' font-pixeboy text-[24px] font-normal text-pink_primary'>
+										{currentTokenPrice.isLoading || isNaN(Number(currentTokenPrice.value)) ? `${unit}` : ''}
+									</span>
+								</div>
+								<Divider
+									type='vertical'
+									className='h-[30px] bg-section-light-container dark:bg-section-dark-container'
+								/>
+								{decision && decidingStatusBlock && <h2 className='mt-3 font-pixeboy text-[22px] font-normal dark:text-white'>{decision.periodPercent || 0}%</h2>}
+								<div className='mr-1'>
+									{votesData && (
+										<VotesProgressInListing
+											index={post_id}
+											proposalType={'ReferendumV2'}
+											votesData={votesData}
+											onchainId={post_id}
+											tally={tally}
+										/>
+									)}
+								</div>
+							</CardHeader>
+							<Link
+								href={`/referenda/${post_id}`}
+								target='_blank'
+							>
+								<div
+									// theme={theme as any}
+									className='relative ml-2 flex w-full items-center'
+								>
+									<Image
+										src={theme === 'light' ? '/assets/bounty-icons/redirect-icon.svg' : '/assets/bounty-icons/redirect-icon-black.svg'}
+										width={45}
+										height={45}
+										alt='redirect link'
+										className='-mr-[2px] h-[36px] w-[36px] cursor-pointer rounded-full bg-black dark:bg-white md:h-auto md:w-auto'
+									/>
+									<BtnLine
+										theme={theme as any}
+										className='relative h-2 w-[10px] bg-black dark:bg-white'
+									></BtnLine>
+									<button
+										className={`${spaceGrotesk.className} ${spaceGrotesk.variable} -ml-[2px] h-[36px] w-[75px] cursor-pointer rounded-3xl border-none bg-black text-base font-bold text-white dark:bg-white dark:text-black md:h-[44px] md:w-[100px] md:text-lg`}
+									>
+										Vote
+									</button>
+								</div>
+							</Link>
 						</div>
-					</div>
+						<div
+							className={`rounded-b-3xl rounded-tr-2xl border-b border-l border-r border-t-0 
+                     border-solid border-section-light-container bg-white px-3 py-1 dark:border-section-dark-container dark:bg-section-light-overlay`}
+						>
+							<Link
+								href={`/referenda/${post_id}`}
+								target='_blank'
+							>
+								<ImageIcon
+									src='/assets/bounty-icons/bounty-image.svg'
+									alt='bounty icon'
+									imgClassName='mt-5 mb-3 w-full md:w-auto'
+									imgWrapperClassName=''
+								/>
+							</Link>
+							<div className={`${spaceGrotesk.className} ${spaceGrotesk.variable}`}>
+								<span className='mr-1 text-base font-medium text-blue-light-medium dark:text-blue-dark-medium'>#{post_id}</span>
+								<span className='text-lg font-bold text-blue-light-high dark:text-blue-dark-high'>{title}</span>
+							</div>
+							<p className={`${spaceGrotesk.className} ${spaceGrotesk.variable} text-sm font-normal text-blue-light-medium dark:text-blue-dark-medium`}>{content.slice(0, 140)}</p>
+							{tags && tags.length > 0 && (
+								<div className='flex gap-x-1'>
+									{tags?.slice(0, 3).map((tag: string, index: number) => (
+										<div
+											key={index}
+											style={{ fontSize: '10px' }}
+											className=' w-min rounded-xl border-[1px] border-solid border-section-light-container px-[14px] py-[4px] font-medium text-lightBlue dark:border-[#3B444F] dark:border-separatorDark dark:text-blue-dark-high'
+										>
+											{tag}
+										</div>
+									))}
+									{tags.length > 3 && (
+										<span
+											className='text-bodyBlue dark:text-blue-dark-high'
+											style={{ background: '#D2D8E050', borderRadius: '20px', fontSize: '10px', padding: '4px 8px' }}
+										>
+											+{tags.length - 3}
+										</span>
+									)}
+								</div>
+							)}
+
+							<Link
+								href={`/address/${proposer}`}
+								target='_blank'
+							>
+								<span className={`${poppins.variable} ${poppins.className} mr-1 text-xs font-normal text-blue-light-medium dark:text-blue-dark-medium`}>Proposer:</span>
+								<ImageComponent
+									alt='user img'
+									src={profileDetails.image}
+									className='-mt-[1px] mr-[1px] h-[16px] w-[16px]'
+								/>
+								<span className={`${poppins.variable} ${poppins.className} text-xs font-medium text-blue-light-high dark:text-blue-dark-high`}>{profileDetails.username}</span>
+							</Link>
+							<div className='mb-3 mt-2'>
+								<TrackTag
+									theme={theme as any}
+									className='sm:mt-0'
+									track={formatTrackName(getTrackNameFromId(network, track_no))}
+								/>
+							</div>
+						</div>
+					</Link>
 				</div>
 			)}
 		</section>

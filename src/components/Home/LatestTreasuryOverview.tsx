f7c90732ae9f13b0450338e534ae2d1e62e05cf8
Jasmeet Singh <jasmeetsonu990@gmail.com>
2024-08-02 14:19:46 +0530

    refactor code and setting up APIs

--- src/components/Home/LatestTreasuryOverview.tsx
+++ src/components/Home/LatestTreasuryOverview.tsx
@@ -1,3 +1,6 @@
+// Copyright 2019-2025 @polkassembly/polkassembly authors & contributors
+// This software may be modified and distributed under the terms
+// of the Apache-2.0 license. See the LICENSE file for details.
 import { Divider } from 'antd';
 import React, { useEffect, useState } from 'react';
 import { CaretDownOutlined, CaretUpOutlined, LoadingOutlined } from '@ant-design/icons';

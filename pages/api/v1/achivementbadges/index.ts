32f1c76d4706fba585912177e4fc4d986761b6c3
vmmuthu31 <mvairamuthu2003@gmail.com>
2024-08-05 11:52:01 +0530

    updated the apache lisence to the header of the file

--- pages/api/v1/achivementbadges/index.ts
+++ pages/api/v1/achivementbadges/index.ts
@@ -1,3 +1,7 @@
+// Copyright 2019-2025 @polkassembly/polkassembly authors & contributors
+// This software may be modified and distributed under the terms
+// of the Apache-2.0 license. See the LICENSE file for details.
+
 import type { NextApiHandler } from 'next';
 import { firestore_db } from '~src/services/firebaseInit';
 import { Badge, ProfileDetailsResponse, BadgeName, MessageType } from '~src/auth/types';

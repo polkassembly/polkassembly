e7bbd6a3da672e9903b547e4f53b4464ce9ddcbd
vmmuthu31 <mvairamuthu2003@gmail.com>
2024-08-05 12:36:50 +0530

    fixed the prod issue for the constant

--- src/global/achievementbadges.ts
+++ src/global/achievementbadges.ts
@@ -1,76 +1,80 @@
+// Copyright 2019-2025 @polkassembly/polkassembly authors & contributors
+// This software may be modified and distributed under the terms
+// of the Apache-2.0 license. See the LICENSE file for details.
+
 import { BadgeName } from '~src/auth/types';
 
 export interface BadgeDetails {
-	id: string;
-	name: BadgeName;
 	description: string;
-	requirements: string;
+	id: string;
 	img: string;
+	name: BadgeName;
+	requirements: string;
 }
 
 export const badgeDetails: BadgeDetails[] = [
 	{
-		id: '1',
-		name: BadgeName.DecentralisedVoice_polkodot,
 		description: 'Awarded to Polkadot delegates who have significant influence.',
-		requirements: 'Must be a delegate on the Polkadot network.',
-		img: ''
+		id: '1',
+		img: '',
+		name: BadgeName.DecentralisedVoice_polkodot,
+		requirements: 'Must be a delegate on the Polkadot network.'
 	},
 	{
-		id: '2',
-		name: BadgeName.DecentralisedVoice_kusama,
 		description: 'Awarded to Kusama delegates who have significant influence.',
-		requirements: 'Must be a delegate on the Kusama network.',
-		img: ''
+		id: '2',
+		img: '',
+		name: BadgeName.DecentralisedVoice_kusama,
+		requirements: 'Must be a delegate on the Kusama network.'
 	},
 	{
-		id: '3',
-		name: BadgeName.Fellow,
 		description: 'Rank 1 and above Fellow.',
-		requirements: 'Must achieve a rank of 1 or higher.',
-		img: ''
+		id: '3',
+		img: '',
+		name: BadgeName.Fellow,
+		requirements: 'Must achieve a rank of 1 or higher.'
 	},
 	{
-		id: '4',
-		name: BadgeName.Council,
 		description: 'Member of the governance council.',
-		requirements: 'Must be a member of the governance council.',
-		img: ''
+		id: '4',
+		img: '',
+		name: BadgeName.Council,
+		requirements: 'Must be a member of the governance council.'
 	},
 	{
-		id: '5',
-		name: BadgeName.ActiveVoter,
 		description: 'Actively participates in voting on proposals.',
-		requirements: 'Must vote on at least 15% of proposals with a minimum of 5 proposals.',
-		img: ''
+		id: '5',
+		img: '',
+		name: BadgeName.ActiveVoter,
+		requirements: 'Must vote on at least 15% of proposals with a minimum of 5 proposals.'
 	},
 	{
-		id: '6',
-		name: BadgeName.Whale,
 		description: 'Holds a significant amount of voting power.',
-		requirements: 'Must have voting power equal to or greater than 0.05% of the total supply.',
-		img: ''
+		id: '6',
+		img: '',
+		name: BadgeName.Whale,
+		requirements: 'Must have voting power equal to or greater than 0.05% of the total supply.'
 	},
 	{
-		id: '7',
-		name: BadgeName.SteadfastCommentor,
 		description: 'Regularly contributes comments.',
-		requirements: 'Must have more than 50 comments.',
-		img: ''
+		id: '7',
+		img: '',
+		name: BadgeName.SteadfastCommentor,
+		requirements: 'Must have more than 50 comments.'
 	},
 	{
-		id: '8',
-		name: BadgeName.GMVoter,
 		description: 'Regularly votes on proposals.',
-		requirements: 'Must have voted on more than 50 proposals.',
-		img: ''
+		id: '8',
+		img: '',
+		name: BadgeName.GMVoter,
+		requirements: 'Must have voted on more than 50 proposals.'
 	},
 	{
-		id: '9',
-		name: BadgeName.PopularDelegate,
 		description: 'Received significant delegated tokens.',
-		requirements: 'Must have received delegated tokens equal to or greater than 0.01% of the total supply.',
-		img: ''
+		id: '9',
+		img: '',
+		name: BadgeName.PopularDelegate,
+		requirements: 'Must have received delegated tokens equal to or greater than 0.01% of the total supply.'
 	}
 ];
 

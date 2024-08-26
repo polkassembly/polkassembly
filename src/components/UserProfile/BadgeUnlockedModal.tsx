9d291602cbc9627dd33050b191c2fed8d9b7da12
vmmuthu31 <mvairamuthu2003@gmail.com>
2024-08-27 00:59:19 +0530

    updated the badge unlocked state model

--- src/components/UserProfile/BadgeUnlockedModal.tsx
+++ src/components/UserProfile/BadgeUnlockedModal.tsx
@@ -12,6 +12,7 @@ import Link from 'next/link';
 import { useNetworkSelector, useUserDetailsSelector } from '~src/redux/selectors';
 import { Badge } from '~src/auth/types';
 import { badgeDetails } from '~src/global/achievementbadges';
+import ImageIcon from '~src/ui-components/ImageIcon';
 
 interface Props {
 	className?: string;
@@ -45,24 +46,19 @@ const BadgeUnlockedModal = ({ className, open, setOpen, badge, badges }: Props) 
 			}}
 			footer={
 				<>
-					<Divider
-						type='horizontal'
-						className='mb-6 mt-4 bg-section-light-container dark:bg-separatorDark'
-					/>
-					<Link
-						href={`https://${network}.polkassembly.io/user/${username}`}
-						className='-mt-2 flex items-center pb-2'
-						target='_blank'
-						rel='noopener noreferrer'
+					<button
+						onClick={() => setOpen(false)}
+						className='w-full rounded-lg border-none bg-[#F7F8F9] p-5 py-3 text-left  text-[#485F7D]'
 					>
-						<CustomButton
-							onClick={() => setOpen(false)}
-							variant='primary'
-							text='View Badge'
-							height={32}
-							className='w-full p-5'
-						/>
-					</Link>
+						<span className='flex items-center gap-1'>
+							<ImageIcon
+								src='/assets/badges/hourglass_move_dark.svg'
+								alt='hourglass'
+								className='text-[#485F7D]'
+							/>
+							<span>{`Unlocked on ${badge.unlockedAt.split('T')[0]}`}</span>
+						</span>
+					</button>
 				</>
 			}
 			closable
@@ -79,6 +75,7 @@ const BadgeUnlockedModal = ({ className, open, setOpen, badge, badges }: Props) 
 				<h2 className='mt-2 text-[20px] tracking-[0.0015em] dark:text-white'>
 					You Earned <span className='font-semibold text-pink_primary'>{badge.name}</span> Badge
 				</h2>
+				<p className='mt-2 text-center text-[16px] font-light dark:text-white'>{matchingBadge?.description}</p>
 			</div>
 		</Modal>
 	);
--- src/global/achievementbadges.ts
+++ src/global/achievementbadges.ts
@@ -15,37 +15,37 @@ export const badgeNames = ['Decentralised Voice', 'Fellow', 'Council Member', 'A
 
 export const badgeDetails: BadgeDetails[] = [
 	{
-		description: 'Awarded to Polkadot delegates who have significant influence.',
+		description: 'Receive delegation of 1,000,000 DOT at 6x conviction from Web3 Foundation to unlock this badge',
 		img: '/assets/badges/decentralised_voice.svg',
 		name: BadgeName.DECENTRALISED_VOICE_POLKADOT,
 		requirements: 'Must be a delegate on the Polkadot network.'
 	},
 	{
-		description: 'Awarded to Kusama delegates who have significant influence.',
+		description: 'Receive delegation of 1,000,000 KSM at 6x conviction from Web3 Foundation to unlock this badge.',
 		img: '/assets/badges/decentralised_voice.svg',
 		name: BadgeName.DECENTRALISED_VOICE_KUSAMA,
 		requirements: 'Must be a delegate on the Kusama network.'
 	},
 	{
-		description: 'Rank 1 and above Fellow.',
+		description: 'Achieve the prestigious rank of Fellow, starting from Rank 1.',
 		img: '/assets/badges/fellow.svg',
 		name: BadgeName.FELLOW,
 		requirements: 'Must achieve a rank of 1 or higher.'
 	},
 	{
-		description: 'Member of the governance council.',
+		description: 'Recognized as a member of the influential governance council.',
 		img: '/assets/badges/Council.svg',
 		name: BadgeName.COUNCIL,
 		requirements: 'Must be a member of the governance council.'
 	},
 	{
-		description: 'Actively participates in voting on proposals.',
+		description: 'Consistently engages in governance by voting on key proposals.',
 		img: '/assets/badges/active_voter.svg',
 		name: BadgeName.ACTIVE_VOTER,
 		requirements: 'Must vote on at least 15% of proposals with a minimum of 5 proposals.'
 	},
 	{
-		description: 'Holds a significant amount of voting power.',
+		description: 'A key stakeholder with significant voting power in the network.',
 		img: '/assets/badges/whale.svg',
 		name: BadgeName.WHALE,
 		requirements: 'Must have voting power equal to or greater than 0.05% of the total supply.'

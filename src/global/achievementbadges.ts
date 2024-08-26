2fe4d72c0631e28d3d6c79bdff26b4e07228f6b2
vmmuthu31 <mvairamuthu2003@gmail.com>
2024-08-26 18:02:19 +0530

    updated the badges path

--- src/global/achievementbadges.ts
+++ src/global/achievementbadges.ts
@@ -16,37 +16,37 @@ export const badgeNames = ['Decentralised Voice', 'Fellow', 'Council Member', 'A
 export const badgeDetails: BadgeDetails[] = [
 	{
 		description: 'Awarded to Polkadot delegates who have significant influence.',
-		img: '/assets/badges/decentralised_voice_polkadot.png',
+		img: '/assets/badges/decentralised_voice.svg',
 		name: BadgeName.DECENTRALISED_VOICE_POLKADOT,
 		requirements: 'Must be a delegate on the Polkadot network.'
 	},
 	{
 		description: 'Awarded to Kusama delegates who have significant influence.',
-		img: '',
+		img: '/assets/badges/decentralised_voice.svg',
 		name: BadgeName.DECENTRALISED_VOICE_KUSAMA,
 		requirements: 'Must be a delegate on the Kusama network.'
 	},
 	{
 		description: 'Rank 1 and above Fellow.',
-		img: '',
+		img: '/assets/badges/fellow.svg',
 		name: BadgeName.FELLOW,
 		requirements: 'Must achieve a rank of 1 or higher.'
 	},
 	{
 		description: 'Member of the governance council.',
-		img: '',
+		img: '/assets/badges/Council.svg',
 		name: BadgeName.COUNCIL,
 		requirements: 'Must be a member of the governance council.'
 	},
 	{
 		description: 'Actively participates in voting on proposals.',
-		img: '',
+		img: '/assets/badges/active_voter.svg',
 		name: BadgeName.ACTIVE_VOTER,
 		requirements: 'Must vote on at least 15% of proposals with a minimum of 5 proposals.'
 	},
 	{
 		description: 'Holds a significant amount of voting power.',
-		img: '',
+		img: '/assets/badges/whale.svg',
 		name: BadgeName.WHALE,
 		requirements: 'Must have voting power equal to or greater than 0.05% of the total supply.'
 	},

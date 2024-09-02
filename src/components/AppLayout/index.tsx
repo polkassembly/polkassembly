7a788045c2e4e9538ded7b6591fe01c54e44c0b7
vaibhav-25 <vaibhavsinghvi254@gmail.com>
2024-09-02 14:41:38 +0530

    fix: routing fixes

--- src/components/AppLayout/index.tsx
+++ src/components/AppLayout/index.tsx
@@ -932,7 +932,7 @@ const AppLayout = ({ className, Component, pageProps }: Props) => {
 				<span>Gov Analytics</span>
 				<div className={`${poppins.className} ${poppins.variable} rounded-[9px] bg-[#407bfe] px-[6px] text-[10px] font-semibold text-white md:-right-6 md:-top-2`}>NEW</div>
 			</div>,
-			'/gov-level-analytics',
+			'/analytics',
 			<div className={`relative ${!sidedrawer && 'mt-2'}`}>
 				<AnalyticsSVGIcon className='scale-90 font-medium text-lightBlue  dark:text-icon-dark-inactive' />
 				<div

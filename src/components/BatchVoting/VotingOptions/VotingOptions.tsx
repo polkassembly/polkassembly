e6b326f02e4d8c4a16b0f74a58083f7cb92c3d88
vaibhav-25 <vaibhavsinghvi254@gmail.com>
2024-09-04 17:54:38 +0530

    fix: ui changes

--- src/components/BatchVoting/VotingOptions/VotingOptions.tsx
+++ src/components/BatchVoting/VotingOptions/VotingOptions.tsx
@@ -1,7 +1,7 @@
 import { Skeleton } from 'antd';
 import dynamic from 'next/dynamic';
 import React from 'react'
-import VotingCards from '~src/components/TinderStyleVoting';
+import SwipableVotingCards from './SwipableVotingCards';
 const VoteCart = dynamic(() => import('~src/components/TinderStyleVoting/VoteCart'), {
 	loading: () => <Skeleton active />,
 	ssr: false
@@ -9,11 +9,15 @@ const VoteCart = dynamic(() => import('~src/components/TinderStyleVoting/VoteCar
 
 const VotingOptions = () => {
   return (
-    <section className='flex gap-x-4 w-full'>
-        <article className='h-full w-[70%] items-center justify-start gap-x-3 rounded-xl bg-white dark:bg-black'><VotingCards /></article>
+    <section className='flex gap-x-4 w-full mb-[200px]'>
+        <article className='h-[557px] w-[70%] items-center justify-start gap-x-3 rounded-xl bg-white dark:bg-black'>
+          <div className='p-5 h-[557px] bg-transparent w-full drop-shadow-lg'>
+            <SwipableVotingCards />
+          </div>
+        </article>
 
-        {/* add confirm batch vote CTA inside voteCard component */}
-        <article className='h-full w-[30%] items-center justify-start gap-x-3 rounded-xl bg-white dark:bg-black'><VoteCart isUsedInWebView={true}/></article>
+        {/* add confirm batch vote CTA inside voteCard component and fix max-h-[662px] to 557px */}
+        <article className='h-[557px] w-[30%] items-center justify-start gap-x-3 rounded-xl bg-white dark:bg-black'><VoteCart isUsedInWebView={true}/></article>
     </section>
   )
 }

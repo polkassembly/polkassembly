18e7b1f23e9dc0e6ffb6eb3f8df9caa63f6bc1bd
Jasmeet <jasmeetsonu990@gmail.com>
2024-10-05 12:37:55 +0530

    fix API issue

--- src/components/createProxy/CreateProxySuccessModal.tsx
+++ src/components/createProxy/CreateProxySuccessModal.tsx
@@ -1,28 +1,61 @@
 // Copyright 2019-2025 @polkassembly/polkassembly authors & contributors
 // This software may be modified and distributed under the terms
 // of the Apache-2.0 license. See the LICENSE file for details.
-/* eslint-disable sort-keys */
-import { Alert, Modal } from 'antd';
+import { Modal } from 'antd';
 import { poppins } from 'pages/_app';
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { styled } from 'styled-components';
 import { CloseIcon } from '~src/ui-components/CustomIcons';
 import Image from 'next/image';
 import Address from '~src/ui-components/Address';
+import { useNetworkSelector } from '~src/redux/selectors';
 import SkeletonInput from '~src/basic-components/Skeleton/SkeletonInput';
 import useImagePreloader from '~src/hooks/useImagePreloader';
-import { IProxyState } from '.';
+import nextApiClientFetch from '~src/util/nextApiClientFetch';
+import Alert from '~src/basic-components/Alert';
+import Link from 'next/link';
 
 interface Props {
 	openModal: boolean;
 	setOpenModal: (pre: boolean) => void;
 	className: string;
 	address: string;
-	proxyState: IProxyState;
 }
 
-const CreateProxySuccessModal = ({ openModal, setOpenModal, className, address, proxyState }: Props) => {
+const CreateProxySuccessModal = ({ openModal, setOpenModal, className, address }: Props) => {
+	const { network } = useNetworkSelector();
+	const [pureProxyAddress, setPureProxyAddress] = useState<string | null>(null);
+	const [loading, setLoading] = useState(false);
+	const [error, setError] = useState<string | null>(null);
+
 	const isGifLoaded = useImagePreloader('/assets/Gifs/voted.gif');
+
+	const fetchProxyAddress = async () => {
+		if (openModal) {
+			setLoading(true);
+			setError(null);
+			try {
+				const { data, error } = await nextApiClientFetch('/api/v1/accounts/proxyAddress');
+				if (error) {
+					setError(error);
+				}
+				if (data) {
+					setPureProxyAddress(data as any);
+				}
+			} catch (err) {
+				console.error('Error fetching proxy address:', err);
+				setError('Failed to fetch proxy address');
+			} finally {
+				setLoading(false);
+			}
+		}
+	};
+
+	useEffect(() => {
+		if (!address) return;
+		fetchProxyAddress();
+		// eslint-disable-next-line react-hooks/exhaustive-deps
+	}, [openModal, address, network]);
 
 	return (
 		<Modal
@@ -35,7 +68,7 @@ const CreateProxySuccessModal = ({ openModal, setOpenModal, className, address, 
 			onCancel={() => setOpenModal(false)}
 			closeIcon={<CloseIcon className=' text-lightBlue dark:text-icon-dark-inactive' />}
 		>
-			<div className={`${proxyState?.error && 'pb-10'} pb-2`}>
+			<div className={`${error && 'pb-10'} pb-2`}>
 				{' '}
 				<div className='flex items-center justify-center pb-10'>
 					<div>
@@ -48,8 +81,10 @@ const CreateProxySuccessModal = ({ openModal, setOpenModal, className, address, 
 								priority={true}
 							/>
 						</div>
-						<h2 className={`${poppins.variable} ${poppins.className} text-xl font-semibold text-blue-light-high dark:text-blue-dark-high`}>Proxy created successfully</h2>
-						<div className='mt-4 flex flex-col gap-1'>
+						<h2 className={`${poppins.variable} ${poppins.className} -mt-2 text-center text-xl font-semibold text-blue-light-high dark:text-blue-dark-high`}>
+							Proxy created successfully
+						</h2>
+						<div className='ml-5 mt-4 flex flex-col gap-1'>
 							<div className='flex items-center gap-2'>
 								<span className='w-[104px] text-blue-light-medium dark:text-blue-dark-medium'>With Address:</span>
 								<Address
@@ -62,16 +97,16 @@ const CreateProxySuccessModal = ({ openModal, setOpenModal, className, address, 
 							</div>
 							<div className='flex items-center gap-2'>
 								<span className='w-[104px] text-blue-light-medium dark:text-blue-dark-medium'>Proxy Address:</span>
-								{proxyState?.loading ? (
+								{loading ? (
 									<SkeletonInput active />
 								) : (
-									!proxyState?.loading &&
-									proxyState?.pureProxyAddress && (
+									!loading &&
+									pureProxyAddress && (
 										<Address
 											displayInline
 											iconSize={18}
 											isTruncateUsername={false}
-											address={proxyState?.pureProxyAddress}
+											address={pureProxyAddress}
 											destroyTooltipOnHide
 										/>
 									)
@@ -80,12 +115,31 @@ const CreateProxySuccessModal = ({ openModal, setOpenModal, className, address, 
 						</div>
 					</div>
 				</div>
-				{proxyState?.error && (
+				{!pureProxyAddress && (
+					<Alert
+						type='info'
+						className={`mx-8 mb-5 h-10 rounded-[4px] text-bodyBlue ${poppins.variable} ${poppins.className}`}
+						showIcon
+						message={
+							<span className='text-sm dark:text-blue-dark-high'>
+								Visit{' '}
+								<Link
+									href='/accounts'
+									className=' font-medium text-pink_primary'
+								>
+									Profile
+								</Link>{' '}
+								to view proxy address
+							</span>
+						}
+					/>
+				)}
+				{error && (
 					<Alert
 						type='error'
 						className={`mx-10 h-10 rounded-[4px] text-bodyBlue ${poppins.variable} ${poppins.className}`}
 						showIcon
-						message={<span className='dark:text-blue-dark-high'>{proxyState?.error}</span>}
+						message={<span className='dark:text-blue-dark-high'>{error}</span>}
 					/>
 				)}
 			</div>
--- src/components/createProxy/index.tsx
+++ src/components/createProxy/index.tsx
@@ -1,11 +1,9 @@
 // Copyright 2019-2025 @polkassembly/polkassembly authors & contributors
 // This software may be modified and distributed under the terms
 // of the Apache-2.0 license. See the LICENSE file for details.
-import React, { useEffect, useState } from 'react';
+import React, { useState } from 'react';
 import dynamic from 'next/dynamic';
-import { useNetworkSelector, useUserDetailsSelector } from '~src/redux/selectors';
-import { onChainProxy } from 'pages/api/v1/accounts/proxyAddress';
-/* eslint-disable sort-keys */
+import { useUserDetailsSelector } from '~src/redux/selectors';
 
 const CreateProxyModal = dynamic(() => import('~src/components/createProxy/CreateProxyModal'), {
 	ssr: false
@@ -16,12 +14,6 @@ const CreateProxyMainModal = dynamic(() => import('~src/components/createProxy/C
 const CreateProxySuccessModal = dynamic(() => import('~src/components/createProxy/CreateProxySuccessModal'), {
 	ssr: false
 });
-
-export interface IProxyState {
-	pureProxyAddress: string | null;
-	loading: boolean;
-	error: string | null;
-}
 
 interface Props {
 	openProxyModal: boolean;
@@ -34,39 +26,6 @@ const ProxyMain = ({ openProxyModal, setOpenProxyModal }: Props) => {
 	const [openProxyMainModal, setOpenProxyMainModal] = useState<boolean>(false);
 	const [openProxySuccessModal, setOpenProxySuccessModal] = useState<boolean>(false);
 	const [address, setAddress] = useState<string>(loginAddress);
-	const { network } = useNetworkSelector();
-	const [proxyState, setProxyState] = useState<IProxyState>({
-		pureProxyAddress: null as string | null,
-		loading: false,
-		error: null as string | null
-	});
-
-	const fetchProxyAddress = async () => {
-		if (openProxySuccessModal) {
-			setProxyState((prevState) => ({ ...prevState, loading: true, error: null }));
-			try {
-				const fetchedProxyAddress = await onChainProxy(address, network);
-				setProxyState((prevState) => ({
-					...prevState,
-					pureProxyAddress: fetchedProxyAddress,
-					loading: false
-				}));
-			} catch (err) {
-				console.error('Error fetching proxy address:', err);
-				setProxyState((prevState) => ({
-					...prevState,
-					error: 'Failed to fetch proxy address',
-					loading: false
-				}));
-			}
-		}
-	};
-
-	useEffect(() => {
-		if (!address && !openProxySuccessModal) return;
-		fetchProxyAddress();
-		// eslint-disable-next-line react-hooks/exhaustive-deps
-	}, [openProxySuccessModal, address, network]);
 
 	return (
 		<>
@@ -88,7 +47,6 @@ const ProxyMain = ({ openProxyModal, setOpenProxyModal }: Props) => {
 				openModal={openProxySuccessModal}
 				setOpenModal={setOpenProxySuccessModal}
 				address={address}
-				proxyState={proxyState}
 				className=''
 			/>
 		</>

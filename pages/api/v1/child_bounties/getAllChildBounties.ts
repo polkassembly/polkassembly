c89d93064c102fbc2feb0be70e7e6eddc5815766
KanishkaRajputd <kanishka6393rajput@gmail.com>
2024-10-10 19:10:46 +0530

    hot fixes

--- pages/api/v1/child_bounties/getAllChildBounties.ts
+++ pages/api/v1/child_bounties/getAllChildBounties.ts
@@ -50,7 +50,26 @@ export const getAllchildBountiesFromBountyIndex = async ({ parentBountyIndex, ne
 
 		const allChildBountiesIndexes = childBountiesProposals.map((childBounty: { index: number }) => childBounty?.index);
 
-		const childBountiesDocs = await postsByTypeRef(network, ProposalType.CHILD_BOUNTIES).where('id', 'in', allChildBountiesIndexes).get();
+		// const childBountiesDocs = await postsByTypeRef(network, ProposalType.CHILD_BOUNTIES).where('id', 'in', allChildBountiesIndexes).get();
+		const chunkArray = (arr: any[], chunkSize: number) => {
+			const chunks = [];
+			for (let i = 0; i < arr.length; i += chunkSize) {
+				chunks.push(arr.slice(i, i + chunkSize));
+			}
+			return chunks;
+		};
+
+		const chunks = chunkArray(allChildBountiesIndexes, 30);
+
+		// Perform Firestore queries for each chunk and merge results
+		const childBountiesDocsPromises = chunks.map((chunk) => postsByTypeRef(network, ProposalType.CHILD_BOUNTIES).where('id', 'in', chunk).get());
+
+		const childBountiesDocsSnapshots = await Promise.all(childBountiesDocsPromises);
+
+		// Combine the documents from all snapshots
+		const childBountiesDocs = childBountiesDocsSnapshots.flatMap((snapshot) => snapshot.docs);
+
+		console.log(childBountiesDocs);
 
 		const childBountiesPromises = childBountiesProposals?.map(async (subsquidChildBounty: any) => {
 			const payload = {
@@ -62,7 +81,7 @@ export const getAllchildBountiesFromBountyIndex = async ({ parentBountyIndex, ne
 				status: subsquidChildBounty?.status,
 				title: ''
 			};
-			childBountiesDocs?.docs?.map((childBounty) => {
+			childBountiesDocs?.map((childBounty) => {
 				if (childBounty.exists) {
 					const data = childBounty.data();
 					payload.title = data?.title || '';
@@ -70,7 +89,7 @@ export const getAllchildBountiesFromBountyIndex = async ({ parentBountyIndex, ne
 			});
 
 			if (!payload?.title.length) {
-				const subsqaureRes = await getSubSquareContentAndTitle(ProposalType.REFERENDUM_V2, network, subsquidChildBounty?.index);
+				const subsqaureRes = await getSubSquareContentAndTitle(ProposalType.CHILD_BOUNTIES, network, subsquidChildBounty?.index);
 				payload.title = subsqaureRes?.title || getProposalTypeTitle(ProposalType.CHILD_BOUNTIES) || '';
 			}
 			return payload;

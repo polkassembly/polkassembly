f7977a73ab672c7652b25228f60b0dd33b846419
vmmuthu31 <mvairamuthu2003@gmail.com>
2024-10-14 22:57:17 +0530

    updated the curator profile

--- src/components/CuratorDashboard/BountiesCuratorInfo/index.tsx
+++ src/components/CuratorDashboard/BountiesCuratorInfo/index.tsx
@@ -1,12 +1,14 @@
 // Copyright 2019-2025 @polkassembly/polkassembly authors & contributors
 // This software may be modified and distributed under the terms
 // of the Apache-2.0 license. See the LICENSE file for details.
+import { spaceGrotesk } from 'pages/_app';
 import React from 'react';
 
 function BountiesCuratorInfo() {
 	return (
 		<div className='rounded-lg border-[0.7px] border-solid border-[#D2D8E0] bg-white p-5'>
-			<p>Bounties Curated (2)</p>
+			<p className={`${spaceGrotesk.className} ${spaceGrotesk.variable} text-[24px] font-bold text-blue-light-high`}>Bounties Curated (2)</p>
+			<div></div>
 		</div>
 	);
 }

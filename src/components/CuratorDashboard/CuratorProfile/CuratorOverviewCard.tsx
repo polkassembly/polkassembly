4a2f0caae8bfcef08cc2302ce7e57efa8c0d4206
vmmuthu31 <mvairamuthu2003@gmail.com>
2024-10-15 21:11:50 +0530

    updated the calculation

--- src/components/CuratorDashboard/CuratorProfile/CuratorOverviewCard.tsx
+++ src/components/CuratorDashboard/CuratorProfile/CuratorOverviewCard.tsx
@@ -9,6 +9,8 @@ import { ResponsiveLine } from '@nivo/line';
 import dayjs from 'dayjs';
 import { parseBalance } from '~src/components/Post/GovernanceSideBar/Modal/VoteData/utils/parseBalaceToReadable';
 import { useNetworkSelector } from '~src/redux/selectors';
+import { DownOutlined, UpOutlined } from '@ant-design/icons';
+import { chainProperties } from '~src/global/networkConstants';
 
 const CuratorOverviewCard = ({ curatorData }: { curatorData: any }) => {
 	const { resolvedTheme: theme } = useTheme();
@@ -18,19 +20,53 @@ const CuratorOverviewCard = ({ curatorData }: { curatorData: any }) => {
 		for (let i = 5; i >= 0; i--) {
 			months.push(dayjs().subtract(i, 'month').format('MMM'));
 		}
-		return months;
+		return months.reverse();
 	};
+
+	const getLastSixMonthsData = (curatorData: any) => {
+		const lastSixMonths = getLastSixMonths();
+		const monthKeys = lastSixMonths
+			.map((month, idx) => {
+				return dayjs().subtract(idx, 'month').format('YYYY-MM');
+			})
+			.reverse();
+		const lastSixMonthData = monthKeys.map((month) => ({
+			x: dayjs(month).format('MMM'),
+			y: curatorData?.lastSixMonthGraphData?.[month] ? parseFloat(parseBalance(curatorData.lastSixMonthGraphData[month], 2, false, network)) : 0
+		}));
+
+		return lastSixMonthData;
+	};
+
+	const calculateHikePercentage = (data: { x: string; y: number }[]) => {
+		const firstMonthValue = data[0].y;
+		const lastMonthValue = data[data.length - 1].y;
+
+		if (firstMonthValue === 0) {
+			if (lastMonthValue > 0) {
+				return 100;
+			} else if (lastMonthValue < 0) {
+				return -100;
+			} else {
+				return 0;
+			}
+		}
+
+		return ((lastMonthValue - firstMonthValue) / firstMonthValue) * 100;
+	};
+
+	const lastSixMonthsData = getLastSixMonthsData(curatorData);
+	const totalDisbursed = lastSixMonthsData.reduce((acc, monthData) => acc + monthData.y, 0);
+	const hikePercentage = calculateHikePercentage(lastSixMonthsData);
 
 	const chartData = [
 		{
 			color: '#4064FF',
-			data: getLastSixMonths().map((month) => ({
-				x: month,
-				y: Math.floor(Math.random() * 10000 + 1000)
-			})),
+			data: lastSixMonthsData,
 			id: 'amount'
 		}
 	];
+
 	return (
 		<div className='mt-5 rounded-lg border-[0.7px] border-solid border-[#D2D8E0] bg-white px-5 pt-5 dark:border-[#494b4d] dark:bg-[#0d0d0d]'>
 			<p className={`${spaceGrotesk.className} ${spaceGrotesk.variable} text-[24px] font-bold text-blue-light-medium dark:text-lightWhite`}>Overview</p>
@@ -40,13 +76,27 @@ const CuratorOverviewCard = ({ curatorData }: { curatorData: any }) => {
 					<span className={`${spaceGrotesk.className} ${spaceGrotesk.variable}   p-1 text-[14px] font-medium text-blue-light-medium  text-opacity-[80%] `}>Last 6 months</span>
 				</div>
 			</div>
-			<p className=' -mt-5 font-pixeboy text-[60px] text-[#2D2D2D] dark:text-icon-dark-inactive'>
-				<span className='font-pixelify text-[46px] font-bold'>$</span>250
-			</p>
-			<div className='relative -mt-14 flex h-[200px] items-center justify-center gap-x-2'>
+			<div className='-mt-5 flex gap-3'>
+				<p className='font-pixeboy text-[60px] text-[#2D2D2D] dark:text-icon-dark-inactive'>{`${totalDisbursed.toFixed(2)} ${chainProperties[network]?.tokenSymbol}`}</p>
+				<div className='mt-5'>
+					{hikePercentage > 0 ? (
+						<div className='flex items-center gap-2 rounded-full bg-[#75c486] px-5 py-2 text-white'>
+							<UpOutlined />
+							<span className='font-pixeboy text-[20px] '>{hikePercentage.toFixed(2)}%</span>
+						</div>
+					) : (
+						<div className='flex items-center gap-2 rounded-full bg-[#FF5D7A] px-5 py-2 text-white'>
+							<DownOutlined />
+							<span className='font-pixeboy text-[20px] '>{hikePercentage.toFixed(2)}%</span>
+						</div>
+					)}
+				</div>
+			</div>
+
+			<div className='relative -mt-10 flex h-[200px] items-center justify-center gap-x-2'>
 				<ResponsiveLine
 					data={chartData}
-					margin={{ bottom: 40, left: 0, right: 40, top: 30 }}
+					margin={{ bottom: 40, left: 10, right: 40, top: 0 }}
 					xScale={{ type: 'point' }}
 					yScale={{
 						max: 'auto',
@@ -78,7 +128,7 @@ const CuratorOverviewCard = ({ curatorData }: { curatorData: any }) => {
 					tooltip={({ point }) => (
 						<div className={`flex gap-2 rounded-md bg-white capitalize dark:bg-[#1E2126] ${theme === 'dark' ? 'text-white' : 'text-[#576D8B]'} p-2 text-[11px] shadow-md`}>
 							<span className='text-xs font-semibold'>{point.data.xFormatted}</span>
-							<span className='text-xs font-semibold'>${point.data.yFormatted}</span>
+							<span className='text-xs font-semibold'>{`${parseFloat(String(point.data.yFormatted)).toFixed(2)} ${chainProperties[network]?.tokenSymbol}`}</span>
 						</div>
 					)}
 					pointSize={5}

d9e439fb27066803b87a49c11d8080205cf79d6d
vmmuthu31 <mvairamuthu2003@gmail.com>
2024-10-16 15:03:55 +0530

    updated the tabs

--- src/components/CuratorDashboard/PendingRequestManager/PendingRequestTabs/CuratorSubmission.tsx
+++ src/components/CuratorDashboard/PendingRequestManager/PendingRequestTabs/CuratorSubmission.tsx
@@ -1,6 +1,7 @@
 // Copyright 2019-2025 @polkassembly/polkassembly authors & contributors
 // This software may be modified and distributed under the terms
 // of the Apache-2.0 license. See the LICENSE file for details.
+import { poppins, spaceGrotesk } from 'pages/_app';
 import React, { useState } from 'react';
 
 function CuratorSubmission() {
@@ -14,16 +15,20 @@ function CuratorSubmission() {
 		<div className='curator-request-container'>
 			<div className='flex justify-between rounded-full border-[1px] border-solid border-[#D2D8E0]'>
 				<div
-					className={`w-1/2 cursor-pointer py-2 text-center ${activeTab === 'received' ? 'rounded-l-full bg-[#F6F8FA]' : ''}`}
+					className={`w-1/2 ${spaceGrotesk.className} ${spaceGrotesk.variable} cursor-pointer py-2 text-center text-[14px] ${
+						activeTab === 'received' ? 'rounded-l-full bg-[#F6F8FA] font-bold text-blue-light-high' : 'font-medium text-[#475569]'
+					}`}
 					onClick={() => handleTabClick('received')}
 				>
-					Received Requests
+					Received Requests <span className={`${poppins.className} ${poppins.variable} text-[12px] font-medium text-[#475569]`}>(0)</span>
 				</div>
 				<div
-					className={`w-1/2 cursor-pointer py-2 text-center ${activeTab === 'sent' ? 'rounded-r-full bg-[#F6F8FA]' : ''}`}
+					className={`w-1/2 ${spaceGrotesk.className} ${spaceGrotesk.variable} cursor-pointer py-2 text-center text-[14px] ${
+						activeTab === 'sent' ? 'rounded-r-full bg-[#F6F8FA] font-bold text-blue-light-high' : 'font-medium text-[#475569]'
+					}`}
 					onClick={() => handleTabClick('sent')}
 				>
-					Sent Requests
+					Sent Requests <span className={`${poppins.className} ${poppins.variable} text-[12px] font-medium text-[#475569]`}>(0)</span>
 				</div>
 			</div>
 			<div className='pt-5'>

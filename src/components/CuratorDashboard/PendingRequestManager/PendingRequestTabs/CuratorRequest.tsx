8e8655250ebc37c038b367be3ac6cf7d9e19c0c1
vmmuthu31 <mvairamuthu2003@gmail.com>
2024-10-16 21:09:09 +0530

    adding the tab for the reqeust

--- src/components/CuratorDashboard/PendingRequestManager/PendingRequestTabs/CuratorRequest.tsx
+++ src/components/CuratorDashboard/PendingRequestManager/PendingRequestTabs/CuratorRequest.tsx
@@ -1,8 +1,11 @@
 // Copyright 2019-2025 @polkassembly/polkassembly authors & contributors
 // This software may be modified and distributed under the terms
 // of the Apache-2.0 license. See the LICENSE file for details.
+import { Divider } from 'antd';
 import { poppins, spaceGrotesk } from 'pages/_app';
 import React, { useState } from 'react';
+import Alert from '~src/basic-components/Alert';
+import { DownOutlined } from '@ant-design/icons';
 
 function CuratorRequest() {
 	const [activeTab, setActiveTab] = useState('received');
@@ -39,7 +42,23 @@ function CuratorRequest() {
 }
 
 function ReceivedRequests() {
-	return <div>Content for Received Requests</div>;
+	return (
+		<div>
+			<div>
+				<Alert
+					type='info'
+					showIcon
+					closable
+					message='Requests to become a curator for bounties can be viewed here'
+				/>
+				<div className='mb-4 mt-6 flex justify-between pr-5'>
+					<span className={`text-[16px] ${poppins.className} ${poppins.variable} font-semibold text-blue-light-high`}>On-chain Bounty Requests</span>
+					<DownOutlined style={{ fontSize: '16px', fontWeight: 'bold' }} />
+				</div>
+				<Divider className='m-0 border-[1px] border-solid border-[#D2D8E0] dark:border-[#494b4d]' />
+			</div>
+		</div>
+	);
 }
 
 function SentRequests() {

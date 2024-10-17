f581becf7365f3416676e54465ebb090f434333e
KanishkaRajputd <kanishka6393rajput@gmail.com>
2024-10-17 17:44:51 +0530

    reject submission api created

---
+++ pages/api/v1/bounty/curator/submissions/rejectSubmission.ts
@@ -0,0 +1,71 @@
+// Copyright 2019-2025 @polkassembly/polkassembly authors & contributors
+// This software may be modified and distributed under the terms
+// of the Apache-2.0 license. See the LICENSE file for details.
+import { NextApiHandler } from 'next';
+import storeApiKeyUsage from '~src/api-middlewares/storeApiKeyUsage';
+import withErrorHandling from '~src/api-middlewares/withErrorHandling';
+import { isValidNetwork } from '~src/api-utils';
+import authServiceInstance from '~src/auth/auth';
+import { MessageType } from '~src/auth/types';
+import getTokenFromReq from '~src/auth/utils/getTokenFromReq';
+import messages from '~src/auth/utils/messages';
+import { firestore_db } from '~src/services/firebaseInit';
+import { getBountyInfo } from '../../getBountyInfoFromIndex';
+import getBountiesCustomStatuses from '~src/util/getBountiesCustomStatuses';
+import { EBountiesStatuses } from '~src/components/Bounties/BountiesListing/types/types';
+import { ESubmissionStatus } from '~src/types';
+import getEncodedAddress from '~src/util/getEncodedAddress';
+
+const handler: NextApiHandler<MessageType> = async (req, res) => {
+	storeApiKeyUsage(req);
+
+	try {
+		const network = String(req.headers['x-network']);
+		if (!network || !isValidNetwork(network)) return res.status(400).json({ message: messages.INVALID_NETWORK });
+
+		const { curatorAddress, proposerAddress, submissionId, parentBountyIndex, rejectionMesssage } = req.body;
+		if (!proposerAddress?.length || !curatorAddress?.length || !submissionId?.length) {
+			return res.status(400).json({ message: messages?.INVALID_PARAMS });
+		}
+
+		const token = getTokenFromReq(req);
+		if (!token) return res.status(400).json({ message: messages?.INVALID_JWT });
+
+		const user = await authServiceInstance.GetUser(token);
+		if (!user) return res.status(403).json({ message: messages.UNAUTHORISED });
+
+		const { data } = await getBountyInfo({
+			bountyIndex: parentBountyIndex,
+			network: network
+		});
+
+		if (!getBountiesCustomStatuses(EBountiesStatuses.ACTIVE).includes(data?.status || '')) {
+			return res.status(400).json({ message: messages?.PARENT_BOUNTY_IS_NOT_ACTIVE });
+		}
+
+		if (![getEncodedAddress(curatorAddress, network), curatorAddress].includes(data?.curator)) {
+			return res.status(400).json({ message: messages.UNAUTHORISED });
+		}
+
+		const submissionDocRef = firestore_db.collection('curator_submissions').doc(submissionId);
+		const submissionDoc = await submissionDocRef.get();
+
+		if (!submissionDoc?.exists) {
+			return res.status(403).json({ message: messages?.CHILD_BOUNTY_SUBMISSION_NOT_EXISTS });
+		}
+
+		const payload = {
+			rejection_msg: rejectionMesssage || '',
+			status: ESubmissionStatus.REJECTED,
+			updatedAt: new Date()
+		};
+
+		await submissionDocRef?.update(payload);
+
+		return res.status(200).json({ message: messages?.CHILD_BOUNTY_SUBMISSION_EDITED_SUCCESSFULLY });
+	} catch (err) {
+		return res.status(500).json({ message: err || messages.API_FETCH_ERROR });
+	}
+};
+
+export default withErrorHandling(handler);

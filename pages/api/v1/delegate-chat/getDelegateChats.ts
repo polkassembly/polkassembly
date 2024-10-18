b914b9b46c0c1c5189b9903023ce309bf8d725c0
Adetoye Adewoye <adetoyeadewoye@gmail.com>
2024-10-18 04:31:15 +0100

    fetch chats api

---
+++ pages/api/v1/delegate-chat/getDelegateChats.ts
@@ -0,0 +1,74 @@
+// Copyright 2019-2025 @polkassembly/polkassembly authors & contributors
+// This software may be modified and distributed under the terms
+// of the Apache-2.0 license. See the LICENSE file for details.
+import type { NextApiRequest, NextApiResponse } from 'next';
+
+import withErrorHandling from '~src/api-middlewares/withErrorHandling';
+import { isValidNetwork } from '~src/api-utils';
+import getEncodedAddress from '~src/util/getEncodedAddress';
+import { isAddress } from 'ethers';
+import { IChat, IChatsResponse } from '~src/types';
+import getTokenFromReq from '~src/auth/utils/getTokenFromReq';
+import { MessageType } from '~src/auth/types';
+import messages from '~src/auth/utils/messages';
+import authServiceInstance from '~src/auth/auth';
+import * as admin from 'firebase-admin';
+import storeApiKeyUsage from '~src/api-middlewares/storeApiKeyUsage';
+
+const firestore_db = admin.firestore();
+
+async function handler(req: NextApiRequest, res: NextApiResponse<IChatsResponse | MessageType>) {
+	storeApiKeyUsage(req);
+
+	const network = String(req.headers['x-network']);
+	if (!network || !isValidNetwork(network)) return res.status(400).json({ message: 'Invalid network in request header' });
+
+	const token = getTokenFromReq(req);
+	if (!token) return res.status(400).json({ message: 'Invalid token' });
+
+	const user = await authServiceInstance.GetUser(token);
+	if (!user) return res.status(403).json({ message: messages.UNAUTHORISED });
+
+	const { address } = req.body;
+
+	if (!address) return res.status(400).json({ message: messages.INVALID_PARAMS });
+	if (!(getEncodedAddress(String(address), network) || isAddress(String(address)))) return res.status(400).json({ message: 'Invalid address' });
+
+	const encodedAddress = getEncodedAddress(address, network);
+
+	const paDelegatesSnapshot = await firestore_db.collection('networks').doc(network).collection('pa_delegates').where('address', '==', encodedAddress).limit(1).get();
+
+	if (paDelegatesSnapshot.empty && !paDelegatesSnapshot?.docs?.[0]) {
+		return res.status(400).json({ message: `User with address ${address} is not a Polkassembly delegate` });
+	}
+
+	const chatSnapshot = firestore_db.collection('chats');
+	try {
+		const [sentChatsSnapshot, receivedChatsSnapshot] = await Promise.all([
+			chatSnapshot.where('senderAddress', '==', encodedAddress).get(),
+			chatSnapshot.where('receiverAddress', '==', encodedAddress).get()
+		]);
+
+		const mapChatData = (docs: FirebaseFirestore.QueryDocumentSnapshot[]): IChat[] =>
+			docs.map((doc) => {
+				const data = doc.data();
+				return {
+					chatId: data.chatId,
+					created_at: data.created_at.toDate ? data.created_at.toDate() : data.created_at,
+					latestMessage: data.latestMessage,
+					receiverAddress: data.receiverAddress,
+					senderAddress: data.senderAddress,
+					updated_at: data.updated_at.toDate ? data.updated_at.toDate() : data.updated_at
+				};
+			});
+
+		const sentChats: IChat[] = mapChatData(sentChatsSnapshot.docs);
+		const receivedChats: IChat[] = mapChatData(receivedChatsSnapshot.docs);
+
+		return res.status(200).json({ receivedChats, sentChats });
+	} catch (error) {
+		return res.status(500).json({ message: error.message || messages.API_FETCH_ERROR });
+	}
+}
+
+export default withErrorHandling(handler);

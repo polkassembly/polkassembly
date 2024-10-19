9e94177d4c52d25368d9a8c33cedbcc6fe63f098
vmmuthu31 <mvairamuthu2003@gmail.com>
2024-10-19 14:08:41 +0530

    updated the submission modal

--- src/components/Post/GovernanceSideBar/Bounty/Submission.tsx
+++ src/components/Post/GovernanceSideBar/Bounty/Submission.tsx
@@ -21,7 +21,7 @@ import Link from 'next/link';
 import { usePostDataContext } from '~src/context';
 import getEncodedAddress from '~src/util/getEncodedAddress';
 import Address from '~src/ui-components/Address';
-import classNames from 'classnames';
+import Image from 'next/image';
 
 const { Option } = Select;
 
@@ -41,7 +41,6 @@ const Submission: FC<IBountyChildBountiesProps> = (props) => {
 	const { network } = useNetworkSelector();
 	const [loading, setLoading] = useState<boolean>(false);
 	const [activeTab, setActiveTab] = useState<ESubmissionStatus | null>(null);
-
 	const showModal = () => {
 		setIsModalVisible(true);
 	};
@@ -77,57 +76,89 @@ const Submission: FC<IBountyChildBountiesProps> = (props) => {
 		// eslint-disable-next-line react-hooks/exhaustive-deps
 	}, [bountyId]);
 
-	const getFilteredSumbissions = () => {
+	const getFilteredSubmissions = () => {
 		switch (activeTab) {
 			case ESubmissionStatus.PENDING:
 				return bountySubmission?.filter((item: IChildBountySubmission) => item?.status == ESubmissionStatus.PENDING);
+			case ESubmissionStatus.REJECTED:
+				return bountySubmission?.filter((item: IChildBountySubmission) => item?.status == ESubmissionStatus.REJECTED);
 			default:
 				return bountySubmission || [];
 		}
 	};
+	const canViewAll = bountySubmission.some((submission) =>
+		[getEncodedAddress(curator, network), getEncodedAddress(submission?.proposer, network)].includes(getEncodedAddress(loginAddress, network))
+	);
 
 	return (
 		<GovSidebarCard>
 			<div className='flex items-center justify-between'>
 				<div className='flex items-center gap-1'>
-					<ImageIcon
+					<Image
 						alt='document'
 						src='/assets/icons/curator-dashboard/Document.svg'
 						className='-mt-3'
+						width={20}
+						height={20}
+						style={{
+							filter: theme === 'dark' ? 'invert(100%) brightness(200%)' : 'none'
+						}}
 					/>
-					<h4 className='text-[20px] font-semibold text-[#334D6E]  dark:text-white'>
-						Submissions <span className='text-[16px] font-normal'>({bountySubmission?.length || 0})</span>{' '}
+
+					<h4 className='text-xl font-semibold text-sidebarBlue  dark:text-white'>
+						Submissions <span className='text-base font-normal'>({bountySubmission?.length || 0})</span>{' '}
 					</h4>
 				</div>
-				{getEncodedAddress(curator, network) == getEncodedAddress(loginAddress, network) && (
+				{canViewAll && (
 					<Link href='/curator-dashboard'>
 						<p className='text-sm font-medium text-pink_primary'>View All</p>
 					</Link>
 				)}
 			</div>
-			<div className='dark:text- mb-4 flex items-center gap-6 rounded-lg bg-[#F5F6F8] px-3 py-2 text-center text-sm text-bodyBlue dark:bg-section-dark-garyBackground dark:text-white'>
-				<span
-					onClick={() => setActiveTab(null)}
-					className={classNames(activeTab == null ? 'text-pink_primary' : '', 'border-pink_primary px-4 py-1 font-medium shadow-sm')}
-				>
-					All
-				</span>
-				<span
-					onClick={() => setActiveTab(ESubmissionStatus.PENDING)}
-					className={activeTab == ESubmissionStatus.PENDING ? 'text-pink_primary' : ''}
-				>
-					Pending
-				</span>
-			</div>
+			{bountySubmission?.length > 0 && (
+				<div className='mb-2 flex items-center justify-between gap-3 rounded-lg bg-section-light-background px-2 py-2 text-center text-sm text-bodyBlue dark:bg-section-dark-garyBackground dark:text-white'>
+					<Button
+						onClick={() => setActiveTab(null)}
+						className={` w-1/3 cursor-pointer rounded-md border-none p-0 py-1 text-sm font-semibold ${
+							activeTab === null
+								? 'bg-white text-pink_primary dark:bg-section-dark-overlay'
+								: 'bg-section-light-background text-blue-light-medium shadow-none dark:bg-section-dark-garyBackground dark:text-[#DADADA]'
+						}`}
+					>
+						All
+					</Button>
+					<Button
+						onClick={() => setActiveTab(ESubmissionStatus.PENDING)}
+						className={` w-1/3 cursor-pointer rounded-md border-none p-0 py-1 text-sm font-semibold ${
+							activeTab === ESubmissionStatus.PENDING
+								? 'bg-white text-pink_primary dark:bg-section-dark-overlay'
+								: 'bg-section-light-background text-blue-light-medium shadow-none dark:bg-section-dark-garyBackground dark:text-[#DADADA]'
+						}`}
+					>
+						Pending
+					</Button>
+					<Button
+						onClick={() => setActiveTab(ESubmissionStatus.REJECTED)}
+						className={` w-1/3 cursor-pointer rounded-md border-none p-0 py-1 text-sm font-semibold ${
+							activeTab === ESubmissionStatus.REJECTED
+								? 'bg-white text-pink_primary dark:bg-section-dark-overlay'
+								: 'bg-section-light-background text-blue-light-medium shadow-none dark:bg-section-dark-garyBackground dark:text-[#DADADA]'
+						}`}
+					>
+						Rejected
+					</Button>
+				</div>
+			)}
+
 			{loading ? (
 				<Skeleton active />
 			) : (
 				<>
-					{getFilteredSumbissions()?.map((submission: any, index: number) => {
-						return (
+					{getFilteredSubmissions()?.length > 0 ? (
+						getFilteredSubmissions()?.map((submission: any, index: number) => (
 							<div
 								key={submission?.id}
-								className='rounded-lg border-[1px] border-solid border-[#D2D8E0] p-3'
+								className='mb-3 rounded-lg border-[1px] border-solid border-section-light-container p-3 dark:border-[#4B4B4B]'
 							>
 								<div>
 									<div className='flex items-center gap-1'>
@@ -135,25 +166,29 @@ const Submission: FC<IBountyChildBountiesProps> = (props) => {
 											address={submission?.proposer}
 											displayInline
 											isTruncateUsername
+											className='text-xs'
 										/>
 										<Divider
 											type='vertical'
-											className='border-l-1 border-lightBlue dark:border-icon-dark-inactive'
+											className='border-l-1 m-0 border-lightBlue p-0 dark:border-icon-dark-inactive'
 										/>
 										<div className='flex items-center gap-1'>
 											<ImageIcon
 												src={theme === 'dark' ? '/assets/activityfeed/darktimer.svg' : '/assets/icons/timer.svg'}
 												alt='timer'
-												className='h-4 text-blue-light-medium dark:text-[#9E9E9E]'
+												className='text-xs text-blue-light-medium dark:text-icon-dark-inactive'
 											/>
-											<p className=' whitespace-nowrap text-xs text-blue-light-medium dark:text-[#9E9E9E]'>{dayjs(submission?.createdAt)?.format('Do MMM YYYY')}</p>
+											<span className='whitespace-nowrap text-xs text-blue-light-medium dark:text-icon-dark-inactive'>{dayjs(submission?.createdAt)?.format('Do MMM YYYY')}</span>
 										</div>
-										<p className='text-blue-light-medium dark:text-[#9E9E9E]'>|</p>
-										<span className='ml-1 whitespace-nowrap text-sm font-medium text-pink_primary'>{parseBalance(String(submission?.reqAmount || '0'), 2, true, network)}</span>
+										<Divider
+											type='vertical'
+											className='border-l-1 m-0 border-lightBlue p-0 dark:border-icon-dark-inactive'
+										/>
+										<span className='ml-1 whitespace-nowrap text-xs font-semibold text-pink_primary'>{parseBalance(String(submission?.reqAmount || '0'), 2, true, network)}</span>
 									</div>
-									<div className='px-3 pb-3'>
+									<div className='mt-2 pb-2'>
 										<span className='text-sm font-medium text-blue-light-medium dark:text-icon-dark-inactive'>#{index + 1} </span>
-										<span className={'text-sm font-medium text-blue-light-high hover:underline dark:text-icon-dark-inactive'}>{submission?.title}</span>
+										<span className='text-sm font-medium text-blue-light-high hover:underline dark:text-white'>{submission?.title}</span>
 									</div>
 									<div className='flex w-full'>
 										{submission.status === ESubmissionStatus.APPROVED ? (
@@ -161,11 +196,9 @@ const Submission: FC<IBountyChildBountiesProps> = (props) => {
 												<CheckCircleOutlined /> Approved
 											</span>
 										) : submission.status === ESubmissionStatus.REJECTED ? (
-											<>
-												<span className='w-full cursor-default rounded-md bg-[#ffe3e7] py-2 text-center text-sm font-medium text-[#FB123C]'>
-													<CloseCircleOutlined /> Rejected
-												</span>
-											</>
+											<span className='w-full cursor-default rounded-md bg-[#ffe3e7] py-2 text-center text-sm font-medium text-[#FB123C]'>
+												<CloseCircleOutlined /> Rejected
+											</span>
 										) : (
 											submission.status === ESubmissionStatus.PENDING && (
 												<span className='w-full cursor-default rounded-md bg-[#fefced] py-2 text-center text-sm font-medium text-[#EDB10A]'>
@@ -176,12 +209,25 @@ const Submission: FC<IBountyChildBountiesProps> = (props) => {
 									</div>
 								</div>
 							</div>
-						);
-					})}
+						))
+					) : activeTab === ESubmissionStatus.PENDING || activeTab === ESubmissionStatus.REJECTED ? (
+						<div className='flex flex-col items-center text-center text-sm text-bodyBlue dark:text-white'>
+							<Image
+								src='/assets/Gifs/watering.gif'
+								alt='document'
+								className='-mt-16 h-80 w-80'
+								width={320}
+								height={320}
+							/>
+							<span className='-mt-20 mb-5 text-base font-medium text-blue-light-medium dark:text-white'>
+								{activeTab === ESubmissionStatus.PENDING ? 'No pending submissions' : 'No rejected submissions'}
+							</span>
+						</div>
+					) : null}
 				</>
 			)}
 			<div
-				className='flex cursor-pointer items-center justify-center rounded-md border-[0.7px] border-solid border-[#D2D8E0] bg-[#E5007A] px-2 py-1 dark:border-separatorDark'
+				className='flex cursor-pointer items-center justify-center rounded-md border-[0.7px] border-solid border-section-light-container bg-[#E5007A] px-2 py-1 dark:border-separatorDark'
 				onClick={showModal}
 			>
 				<ImageIcon
@@ -227,9 +273,8 @@ const Submission: FC<IBountyChildBountiesProps> = (props) => {
 						label={'Request Amount'}
 						helpText={'Enter an amount for your request'}
 						placeholder={'Enter an amount for your request '}
-						// onChange={onBalanceChange}
-						className='-mt-3 border-section-light-container text-sm font-medium dark:border-[#3B444F]'
-						// theme={theme}
+						className='-mt-3 border-section-light-container text-sm font-medium dark:border-section-dark-container'
+						theme={theme}
 					/>
 
 					<Form.Item

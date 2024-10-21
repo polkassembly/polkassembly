73e41e4eef84d481d60ba50da116c43ee947b337
Jasmeet <jasmeetsonu990@gmail.com>
2024-10-21 22:04:37 +0530

    added empty state

--- src/components/UserProfile/ProfileFollows.tsx
+++ src/components/UserProfile/ProfileFollows.tsx
@@ -14,6 +14,8 @@ import FollowButton from './Follow/FollowButton';
 import getRelativeCreatedAt from '~src/util/getRelativeCreatedAt';
 import { ProfileFollowIcon } from '~src/ui-components/CustomIcons';
 import Link from 'next/link';
+import Image from 'next/image';
+import useImagePreloader from '~src/hooks/useImagePreloader';
 
 interface FollowerData {
 	follower_user_id: number;
@@ -34,6 +36,7 @@ const ProfileFollows = ({ className }: { className: string }) => {
 		loading: false,
 		error: null
 	});
+	const isGifLoaded = useImagePreloader('/assets/Gifs/search.gif');
 
 	const fetchFollowers = async () => {
 		setIsLoading({ loading: true, error: null });
@@ -74,47 +77,72 @@ const ProfileFollows = ({ className }: { className: string }) => {
 
 				<div className='mt-6 px-6'>
 					{isLoading.error && <div className='text-red-500'>{isLoading.error}</div>}
-					{!isLoading.loading &&
-						!isLoading.error &&
-						followers.map((follower, index) => (
-							<div key={follower.follower_user_id}>
-								<div className={`${poppins.variable} ${poppins.className} mb-4 mt-3 flex items-start gap-3`}>
-									<ImageComponent
-										src={follower.image || '/assets/icons/user-profile.png'}
-										alt={follower.username}
-										className='h-9 w-9 rounded-full'
-										iconClassName='h-9 w-9'
-									/>
-									<div className='flex flex-col gap-2'>
-										<div className='flex items-center gap-[6px]'>
-											<Link
-												href={`/user/${follower.username}`}
-												className='cursor-pointer'
-											>
-												<span className='text-sm font-semibold text-blue-light-high dark:text-blue-dark-high'>{follower.username}</span>
-											</Link>
-											<span className='text-xs text-blue-light-medium dark:text-blue-dark-medium'>followed you</span>
+					{isLoading.loading ? (
+						<div className='flex items-center justify-center'>
+							<Image
+								src={!isGifLoaded ? '/assets/Gifs/search.svg' : '/assets/Gifs/search.gif'}
+								alt='search-icon'
+								width={274}
+								height={274}
+								className='-my-[40px]'
+								priority={true}
+							/>
+						</div>
+					) : followers.length == 0 ? (
+						<div className='flex flex-col items-center pb-10'>
+							<Image
+								src={!isGifLoaded ? '/assets/Gifs/search.svg' : '/assets/Gifs/search.gif'}
+								alt='search-icon'
+								width={274}
+								height={274}
+								className='-my-[40px]'
+								priority={true}
+							/>
+							<span className='font-medium text-blue-light-medium dark:text-blue-dark-medium'>Oops! Nothing to show please come back later</span>
+						</div>
+					) : (
+						<>
+							{followers.map((follower, index) => (
+								<div key={follower.follower_user_id}>
+									<div className={`${poppins.variable} ${poppins.className} mb-4 mt-3 flex items-start gap-3`}>
+										<ImageComponent
+											src={follower.image || '/assets/icons/user-profile.png'}
+											alt={follower.username}
+											className='h-9 w-9 rounded-full'
+											iconClassName='h-9 w-9'
+										/>
+										<div className='flex flex-col gap-2'>
+											<div className='flex items-center gap-[6px]'>
+												<Link
+													href={`/user/${follower.username}`}
+													className='cursor-pointer'
+												>
+													<span className='text-sm font-semibold text-blue-light-high dark:text-blue-dark-high'>{follower.username}</span>
+												</Link>
+												<span className='text-xs text-blue-light-medium dark:text-blue-dark-medium'>followed you</span>
+											</div>
+
+											<span className='text-xs text-blue-light-medium dark:text-blue-dark-medium'>
+												<ClockCircleOutlined className='mr-[6px]' />
+												{getRelativeCreatedAt(new Date(follower.created_at))}
+											</span>
 										</div>
-
-										<span className='text-xs text-blue-light-medium dark:text-blue-dark-medium'>
-											<ClockCircleOutlined className='mr-[6px]' />
-											{getRelativeCreatedAt(new Date(follower.created_at))}
-										</span>
+										<FollowButton
+											userId={follower.follower_user_id}
+											isUsedInProfileTab={true}
+										/>
 									</div>
-									<FollowButton
-										userId={follower.follower_user_id}
-										isUsedInProfileTab={true}
-									/>
-								</div>
 
-								{index !== followers.length - 1 && (
-									<Divider
-										type='horizontal'
-										className='my-0 bg-[#D2D8E0B2] dark:bg-separatorDark'
-									/>
-								)}
-							</div>
-						))}
+									{index !== followers.length - 1 && (
+										<Divider
+											type='horizontal'
+											className='my-0 bg-[#D2D8E0B2] dark:bg-separatorDark'
+										/>
+									)}
+								</div>
+							))}
+						</>
+					)}
 				</div>
 			</Spin>
 		</div>

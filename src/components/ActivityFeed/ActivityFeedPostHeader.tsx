bc9ab03b60570ca03097956c3259796fedcc2ddb
AleemAlam <aleem.alam@outlook.com>
2024-11-01 05:12:16 +0530

    feat: :memo: Remove halloween Theme

--- src/components/ActivityFeed/ActivityFeedPostHeader.tsx
+++ src/components/ActivityFeed/ActivityFeedPostHeader.tsx
@@ -381,7 +381,7 @@ export const ActivityFeedPostHeader: React.FC<IPostHeaderProps> = ({
 				theme={theme}
 				modalOpen={modalOpen}
 				setModalOpen={setModalOpen}
-				image='/assets/Gifs/halloween-login.gif'
+				image='/assets/Gifs/login-vote.gif'
 				title={'Join Polkassembly to Vote on this proposal.'}
 				subtitle='Discuss, contribute and get regular updates from Polkassembly.'
 			/>

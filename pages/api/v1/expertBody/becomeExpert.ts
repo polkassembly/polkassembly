a7339a63fd3f28019e41bc89db9c23c8405bd89a
KanishkaRajputd <kanishka6393rajput@gmail.com>
2024-11-11 15:10:00 +0530

    hot fixed

--- pages/api/v1/expertBody/becomeExpert.ts
+++ pages/api/v1/expertBody/becomeExpert.ts
@@ -39,6 +39,16 @@ const handler: NextApiHandler<MessageType> = async (req, res) => {
 		const user = await authServiceInstance.GetUser(token);
 		if (!user) return res.status(401).json({ message: messages.UNAUTHORISED });
 
+		const userAddressesRefs = await firestore_db
+			.collection('addresses')
+			.where('address', '==', encodedUserAddress)
+			.where('user_id', '==', user?.id)
+			.get();
+
+		if (userAddressesRefs?.empty) {
+			return res.status(400).json({ message: messages.UNAUTHORISED });
+		}
+
 		const expertReqSnapshot = firestore_db.collection('expert_requests');
 
 		const expertReqDocs = await expertReqSnapshot

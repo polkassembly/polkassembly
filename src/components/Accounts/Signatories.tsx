a49394e94414a0d6096e68857ba809d9375e772d
Jasmeet <jasmeetsonu990@gmail.com>
2024-11-28 13:03:58 +0530

    fix lint issues

--- src/components/Accounts/Signatories.tsx
+++ src/components/Accounts/Signatories.tsx
@@ -3,8 +3,8 @@
 // of the Apache-2.0 license. See the LICENSE file for details.
 import React from 'react';
 import { IAccountData } from '~src/types';
-import { poppins } from 'pages/_app';
 import AddressComponent from './utils/AddressComponent';
+import { dmSans } from 'pages/_app';
 
 interface Props {
 	accountData: IAccountData;
@@ -15,7 +15,7 @@ const Signatories: React.FC<Props> = ({ accountData }) => {
 
 	return (
 		<div
-			className={`${poppins.className} ${poppins.variable} mt-5 w-full rounded-[14px] border border-solid border-[#F6F8FA] bg-[#F6F8FA] p-2 dark:border-separatorDark dark:bg-section-dark-background lg:p-4`}
+			className={`${dmSans.className} ${dmSans.variable} mt-5 w-full rounded-[14px] border border-solid border-[#F6F8FA] bg-[#F6F8FA] p-2 dark:border-separatorDark dark:bg-section-dark-background lg:p-4`}
 		>
 			<h3 className='text-xl font-semibold text-blue-light-high dark:text-blue-dark-high'>Signatories</h3>
 			{accountData?.multisig?.multi_account_member.map((signatories, index) => (

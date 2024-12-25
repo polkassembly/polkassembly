305e931a64586a8dccb5241c3c03f47b7e8e2afe
Jasmeet <jasmeetsonu990@gmail.com>
2024-12-25 15:10:04 +0530

    fix ui

--- src/components/UserCreatedBounties/UserBountyPage/BountySubmission/SubmissionComponent/SubmissionReactionButton.tsx
+++ src/components/UserCreatedBounties/UserBountyPage/BountySubmission/SubmissionComponent/SubmissionReactionButton.tsx
@@ -3,16 +3,24 @@ import React from 'react';
 
 const SubmissionReactionButton = ({ isUsedinModal }: { isUsedinModal?: boolean }) => {
 	return (
-		<div className={`${isUsedinModal ? '' : 'mt-2 flex gap-4'}`}>
+		<div>
 			{isUsedinModal && <Divider className='border-l-1 my-4 border-[#D2D8E0B2] dark:border-separatorDark md:inline-block' />}
-			<button
-				className={`${isUsedinModal ? 'w-[156px]' : 'w-full'} rounded-[4px] border border-solid border-[#E5007A] bg-transparent px-4 py-2 text-sm font-medium text-[#E5007A]`}
-			>
-				Reject
-			</button>
-			<button className={`${isUsedinModal ? 'w-[156px]' : 'w-full'} rounded-[4px] border border-solid border-[#E5007A] bg-[#E5007A] px-4 py-2 text-sm font-medium text-white`}>
-				Approve
-			</button>
+			<div className={`${isUsedinModal ? 'flex items-center justify-end gap-2' : ''}`}>
+				<button
+					className={`${
+						isUsedinModal ? 'w-[156px]' : 'w-full'
+					} cursor-pointer rounded-[4px] border border-solid border-[#E5007A] bg-transparent px-4 py-2 text-sm font-medium text-[#E5007A]`}
+				>
+					Reject
+				</button>
+				<button
+					className={`${
+						isUsedinModal ? 'w-[156px]' : 'w-full'
+					} cursor-pointer rounded-[4px] border border-solid border-[#E5007A] bg-[#E5007A] px-4 py-2 text-sm font-medium text-white`}
+				>
+					Approve
+				</button>
+			</div>
 		</div>
 	);
 };

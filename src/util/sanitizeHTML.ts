6ceeedeea4c466d140c330f44d19364cc858c876
alphainfinitus <kumar.mridul000@gmail.com>
2025-02-11 02:56:07 +0530

    feat: Implement robust HTML sanitization using xss library

--- src/util/sanitizeHTML.ts
+++ src/util/sanitizeHTML.ts
@@ -1,43 +1,38 @@
 // Copyright 2019-2025 @polkassembly/polkassembly authors & contributors
 // This software may be modified and distributed under the terms
 // of the Apache-2.0 license. See the LICENSE file for details.
+import xss from 'xss';
 
 export const sanitizeHTML = (html: string): string => {
-	// Remove potentially dangerous tags and their contents
-	const dangerousTags = [
-		'script',
-		'style',
-		'iframe',
-		'object',
-		'embed',
-		'form',
-		'input',
-		'button',
-		'textarea',
-		'select',
-		'meta',
-		'link',
-		'base',
-		'applet',
-		'audio',
-		'video',
-		'canvas',
-		'math'
-	];
-
-	let sanitized = html;
-
-	// Remove dangerous tags and their contents
-	dangerousTags.forEach((tag) => {
-		const regex = new RegExp(`<${tag}[^>]*>.*?</${tag}>|<${tag}[^>]*/>`, 'gis');
-		sanitized = sanitized.replace(regex, '');
+	return xss(html, {
+		whiteList: {
+			a: ['href', 'title', 'target'],
+			blockquote: [],
+			br: [],
+			code: [],
+			div: ['class', 'style'],
+			em: [],
+			h1: [],
+			h2: [],
+			h3: [],
+			h4: [],
+			h5: [],
+			h6: [],
+			hr: [],
+			img: ['src', 'alt', 'title', 'width', 'height'],
+			li: [],
+			ol: [],
+			p: ['style'],
+			pre: [],
+			span: ['class', 'style'],
+			strong: [],
+			table: ['style', 'border', 'cellpadding', 'cellspacing'],
+			tbody: [],
+			td: ['style', 'colspan', 'rowspan'],
+			th: ['style', 'colspan', 'rowspan'],
+			thead: [],
+			tr: [],
+			ul: []
+		}
 	});
-
-	// Remove dangerous attributes
-	sanitized = sanitized.replace(/(<[^>]*\s)(on\w+|javascript:|data:|vbscript:)=[^>]*>/gi, '$1>');
-
-	// Remove comments that might contain exploits
-	sanitized = sanitized.replace(/<!--[\s\S]*?-->/g, '');
-
-	return sanitized;
 };

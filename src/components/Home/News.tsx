858d955406e826d8d9977a702d3c6825f6f49c20
Jasmeet <jasmeetsonu990@gmail.com>
2025-03-03 17:45:06 +0530

    fixes

--- src/components/Home/News.tsx
+++ src/components/Home/News.tsx
@@ -1,7 +1,6 @@
 // Copyright 2019-2025 @polkassembly/polkassembly authors & contributors
 // This software may be modified and distributed under the terms
 // of the Apache-2.0 license. See the LICENSE file for details.
-
 import { useTheme } from 'next-themes';
 import React, { FC, useEffect, useState } from 'react';
 import { TwitterTimelineEmbed } from 'react-twitter-embed';
@@ -11,16 +10,21 @@ interface INewsProps {
 	twitter: string;
 }
 
-const News: FC<INewsProps> = (props) => {
-	const { twitter } = props;
+const News: FC<INewsProps> = ({ twitter }) => {
 	const { resolvedTheme: theme } = useTheme();
 	const [isLoading, setIsLoading] = useState<boolean>(true);
 	const [windowWidth, setWindowWidth] = useState<number>(window.innerWidth);
+	const [isIOS, setIsIOS] = useState<boolean>(false);
 
 	let profile = 'polkadot';
 	if (twitter) {
 		profile = twitter.split('/')[3];
 	}
+
+	useEffect(() => {
+		const userAgent = navigator.userAgent || navigator.vendor;
+		setIsIOS(/iPad|iPhone|iPod/.test(userAgent));
+	}, []);
 
 	useEffect(() => {
 		const handleResize = () => {
@@ -32,24 +36,35 @@ const News: FC<INewsProps> = (props) => {
 
 	useEffect(() => {
 		setIsLoading(true);
-	}, [theme, windowWidth]);
+	}, [theme, windowWidth, isIOS]);
 
 	return (
 		<div className='h-[520px] rounded-xxl bg-white p-4 drop-shadow-md dark:bg-section-dark-overlay lg:h-[550px] lg:p-6'>
 			<h2 className='mb-6 text-xl font-semibold leading-8 tracking-tight text-blue-light-high dark:text-blue-dark-high'>News</h2>
 			<div className='overflow-hidden rounded-[10px]'>
 				{isLoading && <Loader iconClassName={'text-7xl mt-32'} />}
-				<TwitterTimelineEmbed
-					key={`${theme}-${windowWidth}`}
-					onLoad={() => setIsLoading(false)}
-					sourceType='profile'
-					screenName={profile}
-					options={{ height: 450 }}
-					noHeader={true}
-					noFooter={true}
-					theme={theme === 'dark' ? 'dark' : 'light'}
-					noBorders
-				/>
+				{!isIOS ? (
+					<TwitterTimelineEmbed
+						key={`${theme}-${windowWidth}`}
+						onLoad={() => setIsLoading(false)}
+						sourceType='profile'
+						screenName={profile}
+						options={{ height: 450, width: '100%' }}
+						noHeader={true}
+						noFooter={true}
+						theme={theme === 'dark' ? 'dark' : 'light'}
+						noBorders
+					/>
+				) : (
+					<iframe
+						src={`https://twitframe.com/show?url=https://twitter.com/${profile}`}
+						width='100%'
+						height='450'
+						style={{ border: 'none', overflow: 'hidden' }}
+						allow='autoplay; clipboard-write; encrypted-media; picture-in-picture'
+						onLoad={() => setIsLoading(false)}
+					></iframe>
+				)}
 			</div>
 		</div>
 	);

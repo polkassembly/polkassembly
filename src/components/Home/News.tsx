e6de86cee5358a84369ba23e51c9cd77945be2d5
Jasmeet <jasmeetsonu990@gmail.com>
2025-03-04 16:57:29 +0530

    fixes

--- src/components/Home/News.tsx
+++ src/components/Home/News.tsx
@@ -1,3 +1,6 @@
+// Copyright 2019-2025 @polkassembly/polkassembly authors & contributors
+// This software may be modified and distributed under the terms
+// of the Apache-2.0 license. See the LICENSE file for details.
 import { useTheme } from 'next-themes';
 import React, { FC, useEffect, useState } from 'react';
 import Loader from '~src/ui-components/Loader';

b73a7f158c479236619815ffcd8d274ffac5a2e3
KanishkaRajputd <kanishka6393rajput@gmail.com>
2025-04-07 12:10:04 +0530

    spam post ui fixes

--- pages/post/[id].tsx
+++ pages/post/[id].tsx
@@ -3,6 +3,8 @@
 // of the Apache-2.0 license. See the LICENSE file for details.
 
 import type { GetServerSideProps } from 'next';
+import Image from 'next/image';
+import Link from 'next/link';
 import { getOffChainPost } from 'pages/api/v1/posts/off-chain-post';
 import { IPostResponse } from 'pages/api/v1/posts/on-chain-post';
 import React, { FC, useEffect, useState } from 'react';
@@ -11,6 +13,7 @@ import { PostCategory } from 'src/global/post_categories';
 import BackToListingView from 'src/ui-components/BackToListingView';
 import { ErrorState } from 'src/ui-components/UIStates';
 import { getNetworkFromReqHeaders } from '~src/api-utils';
+import CustomButton from '~src/basic-components/buttons/CustomButton';
 import LoadingState from '~src/basic-components/Loading/LoadingState';
 import Post from '~src/components/Post/Post';
 import { noTitle } from '~src/global/noTitle';
@@ -60,13 +63,51 @@ const DiscussionPost: FC<IDiscussionPostProps> = (props) => {
 		// eslint-disable-next-line react-hooks/exhaustive-deps
 	}, [post]);
 
-	if (error || (!!post?.spam_users_count && post?.spam_users_count > 0) || post?.isSpamDetected)
+	if (error)
 		return (
 			<ErrorState
-				errorMessage={error || 'This post is spam and has been deleted .'}
+				errorMessage={error}
 				isRefreshBtnVisible={!error?.includes('not found') && !post?.isSpamDetected}
 				showMoreDiscussions
 			/>
+		);
+
+	if (post?.isSpamDetected || post?.spam_users_count)
+		return (
+			<div className='mt-16'>
+				<div className='flex flex-col items-center justify-center'>
+					<Image
+						src='/assets/Gifs/spam-gif.gif'
+						alt='spam post'
+						width={200}
+						height={200}
+					/>
+					<h1 className='mt-8 text-lg font-bold text-bodyBlue dark:text-blue-dark-high'>This post has been deleted.</h1>
+					<p className='text-center text-sm text-lightBlue dark:text-blue-dark-medium'>
+						This post was flagged as spam by users, if you think there is an error,{' '}
+						<Link
+							href='https://polkassembly.featureos.app/'
+							className='text-pink_primary underline'
+							target='_blank'
+						>
+							raise a ticket
+						</Link>
+					</p>
+					<CustomButton
+						variant='primary'
+						text='Back to Feed'
+						size='large'
+						className='mt-4 w-[556px] transition-colors duration-300 max-md:w-full'
+					>
+						<Link
+							href='/discussions'
+							className='hover:text-white'
+						>
+							Back to Feed
+						</Link>
+					</CustomButton>
+				</div>
+			</div>
 		);
 
 	if (post && !post?.spam_users_count && !post?.isSpamDetected)

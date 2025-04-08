08d296937f596d8f9324b08d79722d7a89b4c0db
KanishkaRajputd <kanishka6393rajput@gmail.com>
2025-04-08 15:43:00 +0530

    user id 25687 blocked

--- src/global/userIdBlacklist.ts
+++ src/global/userIdBlacklist.ts
@@ -2,4 +2,4 @@
 // This software may be modified and distributed under the terms
 // of the Apache-2.0 license. See the LICENSE file for details.
 
-export const BLACKLISTED_USER_IDS = [24737, 24977, 23575, 23539, 23523, 24569, 23363, 25133];
+export const BLACKLISTED_USER_IDS = [24737, 24977, 23575, 23539, 23523, 24569, 23363, 25133, 25687];

702ee38d3c4d674acfea7f14584ec27dd4d16ee7
Adetoye Adewoye <adetoyeadewoye@gmail.com>
2025-05-31 09:50:54 +0100

    use network literals

--- src/global/revampedNetworks.ts
+++ src/global/revampedNetworks.ts
@@ -2,4 +2,6 @@
 // This software may be modified and distributed under the terms
 // of the Apache-2.0 license. See the LICENSE file for details.
 
-export const REVAMPED_NETWORKS = ['westend', 'polkadot', 'kusama', 'picasso'];
+import { network } from 'src/global/networkConstants';
+
+export const REVAMPED_NETWORKS = [network.WESTEND, network.POLKADOT, network.KUSAMA, network.PICASSO];

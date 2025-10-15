7cec2c8194926628f3d9fc450b08b5900bad1c8b
aadarsh012 <aadarsh012@gmail.com>
2025-10-15 14:45:05 +0530

    Refactor ServiceDiscontinued components to utilize structured service data
    - Updated ServiceDiscontinuedBanner and ServiceDiscontinuedModal to use an array of objects for DISCONTINUED_SERVICES, allowing for more flexible service management.
    - Replaced direct inclusion checks with a search for the corresponding service object, improving clarity and maintainability of the code.

--- src/global/discontinuedServices.ts
+++ src/global/discontinuedServices.ts
@@ -2,6 +2,29 @@
 // This software may be modified and distributed under the terms
 // of the Apache-2.0 license. See the LICENSE file for details.
 
-export const DISCONTINUED_SERVICE_DATE = '2025-06-05';
-
-export const DISCONTINUED_SERVICES = ['basilisk', 'hydradx', 'xx', 'equilibrium', 'picasso'];
+export const DISCONTINUED_SERVICES = [
+	{
+		date: '2025-06-05',
+		network: 'basilisk'
+	},
+	{
+		date: '2025-06-05',
+		network: 'hydradx'
+	},
+	{
+		date: '2025-06-05',
+		network: 'xx'
+	},
+	{
+		date: '2025-06-05',
+		network: 'equilibrium'
+	},
+	{
+		date: '2025-06-05',
+		network: 'picasso'
+	},
+	{
+		date: '2025-10-15',
+		network: 'zeitgeist'
+	}
+];

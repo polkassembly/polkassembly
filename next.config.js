d5fbf15395585e066452db01257449fce3871f2a
Nikhil Ranjan <niklabh811@gmail.com>
2026-02-14 11:21:15 +0100

    Revert "next version"
    This reverts commit d98457f05fc6e5ecb13bb4b2f7869aca90b7bb21.

--- next.config.js
+++ next.config.js
@@ -67,53 +67,9 @@ const nextConfig = {
 			]
 		};
 	},
-	transpilePackages: [
-		'@mdxeditor/editor',
-		'antd',
-		'rc-cascader',
-		'rc-checkbox',
-		'rc-collapse',
-		'rc-dialog',
-		'rc-drawer',
-		'rc-dropdown',
-		'rc-field-form',
-		'rc-image',
-		'rc-input',
-		'rc-input-number',
-		'rc-mentions',
-		'rc-menu',
-		'rc-motion',
-		'rc-notification',
-		'rc-overflow',
-		'rc-pagination',
-		'rc-picker',
-		'rc-progress',
-		'rc-rate',
-		'rc-resize-observer',
-		'rc-segmented',
-		'rc-select',
-		'rc-slider',
-		'rc-steps',
-		'rc-switch',
-		'rc-table',
-		'rc-tabs',
-		'rc-textarea',
-		'rc-tooltip',
-		'rc-tree',
-		'rc-tree-select',
-		'rc-upload',
-		'rc-util',
-		'rc-virtual-list',
-		'@ant-design/icons',
-		'@ant-design/cssinjs'
-	],
+	transpilePackages: ['@mdxeditor/editor'],
 	images: {
-		remotePatterns: [
-			{
-				protocol: 'https',
-				hostname: 'parachains.info'
-			}
-		]
+		domains: ['parachains.info']
 	},
 	reactStrictMode: true,
 	compiler: {

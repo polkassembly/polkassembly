5eac843a09d7df2f06154050db826a6ee6a8abfb
Nikhil Ranjan <niklabh811@gmail.com>
2026-02-14 14:12:26 +0100

    removed sentry

--- pages/_error.jsx
+++ pages/_error.jsx
@@ -2,7 +2,6 @@
 // This software may be modified and distributed under the terms
 // of the Apache-2.0 license. See the LICENSE file for details.
 
-import * as Sentry from '@sentry/nextjs';
 import Error from 'next/error';
 
 const CustomErrorComponent = (props) => {
@@ -11,11 +10,6 @@ const CustomErrorComponent = (props) => {
 };
 
 CustomErrorComponent.getInitialProps = async (contextData) => {
-	// In case this is running in a serverless function, await this in order to give Sentry
-	// time to send the error before the lambda exits
-	await Sentry.captureUnderscoreErrorException(contextData);
-
-	// This will contain the status code of the response
 	return Error.getInitialProps(contextData);
 };
 
